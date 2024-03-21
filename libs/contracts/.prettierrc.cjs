24c79f5592b4cad20cd96c55d2a9b294461907dc
Petar Kirov <petar.p.kirov@gmail.com>
2024-03-21 13:55:15 +0200

    build(yarn): Add `format:check` and `format:write` commands

---
+++ libs/contracts/.prettierrc.cjs
@@ -0,0 +1,13 @@
+const defaultConfig = require('../../.prettierrc.cjs');
+
+const config = {
+  ...defaultConfig,
+  overrides: [
+    {
+      files: '*.sol',
+      options: {},
+    },
+  ],
+};
+
+module.exports = config;
