f83ee80cad30f6112e970bda7e6068ae11f41a3a
Stefan Nikolov <consultsen.stefan@gmail.com>
2024-03-25 09:07:18 +0200

    feat(anomaly_detection): Add Extended Isolation Forest mock use

---
+++ libs/anomaly_detection/bin/extended_isolation_forest.rs
@@ -0,0 +1,38 @@
+use rand::distributions::Uniform;
+use rand::Rng;
+use extended_isolation_forest::{Forest, ForestOptions};
+
+/*
+"Extended Isolation Forest" paper - https://ieeexplore.ieee.org/document/8888179
+Reference implementation - https://github.com/sahandha/eif
+*/
+
+fn make_f64_forest() -> Forest<f64, 3> {
+    let rng = &mut rand::thread_rng();
+    let distribution = Uniform::new(-4., 4.);
+    let distribution2 = Uniform::new(10., 50.);
+    let values: Vec<_> = (0..3000)
+        .map(|_| [rng.sample(distribution), rng.sample(distribution), rng.sample(distribution2)])
+        .collect();
+
+    let options = ForestOptions {
+        n_trees: 150,
+        sample_size: 200,
+        max_tree_depth: None,
+        extension_level: 1,
+    };
+    Forest::from_slice(values.as_slice(), &options).unwrap()
+}
+
+fn main() {
+    let forest = make_f64_forest();
+
+    // no anomaly
+    assert!(forest.score(&[1.0, 3.0, 25.0]) < 0.5);
+    assert!(forest.score(&[-1.0, 3.0, 25.0]) < 0.5);
+
+    // anomalies
+    assert!(forest.score(&[-12.0, 6.0, 25.0]) > 0.5);
+    assert!(forest.score(&[-1.0, 2.0, 60.0]) > 0.5);
+    assert!(forest.score(&[-1.0, 2.0, 0.0]) > 0.5);
+}
