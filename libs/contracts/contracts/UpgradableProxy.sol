0af93714bfcf063e814c527b9680a2a169c479f2
Aneta Tsvetkova <anetastsvetkova@gmail.com>
2024-04-02 18:07:28 +0300

    feat(contracts): Optimize upgradable proxy contract

--- libs/contracts/contracts/UpgradableProxy.sol
+++ libs/contracts/contracts/UpgradableProxy.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-import {ERC1967Proxy} from '@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol';
+import {Proxy} from '@openzeppelin/contracts/proxy/Proxy.sol';
 import {ERC1967Utils} from '@openzeppelin/contracts/proxy/ERC1967/ERC1967Utils.sol';
 import {IERC1967} from '@openzeppelin/contracts/interfaces/IERC1967.sol';
 
@@ -9,27 +9,21 @@ interface ITransparentUpgradeableProxy is IERC1967 {
   function upgradeToAndCall(address, bytes calldata) external payable;
 }
 
-contract UpgradableProxy is ERC1967Proxy {
+contract UpgradableProxy is Proxy {
   address private immutable _admin;
 
   error ProxyDeniedAdminAccess();
 
-  constructor(
-    address _logic,
-    address initialOwner
-  ) payable ERC1967Proxy(_logic, '') {
+  constructor(address _logic, address initialOwner) {
+    ERC1967Utils.upgradeToAndCall(_logic, '');
+
     _admin = address(initialOwner);
-    // Set the storage value and emit an event for ERC-1967 compatibility
-    ERC1967Utils.changeAdmin(initialOwner);
   }
 
-  function implementation() external view returns (address) {
+  function _implementation() internal view virtual override returns (address) {
     return ERC1967Utils.getImplementation();
   }
 
-  /**
-   * @dev If caller is the admin process the call internally, otherwise transparently fallback to the proxy behavior.
-   */
   function _fallback() internal virtual override {
     if (msg.sender == _admin) {
       if (msg.sig != ITransparentUpgradeableProxy.upgradeToAndCall.selector) {
@@ -42,15 +36,8 @@ contract UpgradableProxy is ERC1967Proxy {
     }
   }
 
-  /**
-   * @dev Upgrade the implementation of the proxy. See {ERC1967Utils-upgradeToAndCall}.
-   *
-   * Requirements:
-   *
-   * - If `data` is empty, `msg.value` must be zero.
-   */
   function _dispatchUpgradeToAndCall() private {
-    address newImplementation = address(bytes20(msg.data[4:24]));
+    address newImplementation = address(bytes20(msg.data[4:]));
     ERC1967Utils.upgradeToAndCall(newImplementation, '');
   }
 }
