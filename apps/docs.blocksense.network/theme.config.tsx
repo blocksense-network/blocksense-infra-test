e92547d6c5ec2fcd4cd66b3264b1ef54905dd9c4
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2024-05-27 16:42:52 +0300

    config(docs.ws): Initial `nextra` configuration

---
+++ apps/docs.blocksense.network/theme.config.tsx
@@ -0,0 +1,10 @@
+export default {
+  logo: <span>Blocksense Network Documentation</span>,
+  project: {
+    link: 'https://github.com/blocksense-network/blocksense',
+  },
+  docsRepositoryBase: 'https://github.com/blocksense-network/blocksense',
+  editLink: '',
+
+  // ... other theme options
+};
