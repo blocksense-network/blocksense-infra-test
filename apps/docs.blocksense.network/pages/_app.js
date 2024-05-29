4d81c41fb1d790e06beacad6162f0bea227fe0b7
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2024-05-29 17:02:48 +0300

    feat(docs.ws): Load css from theme package

---
+++ apps/docs.blocksense.network/pages/_app.js
@@ -0,0 +1,6 @@
+import '@blocksense/docs-theme/style.css';
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
