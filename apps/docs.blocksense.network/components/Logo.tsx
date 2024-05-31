a3e2a30c320a544d41d63b52281b5c3dc3e5a83d
Vanya Pavlova <vanya.pavlova@blocksense.network>
2024-05-31 12:20:24 +0300

    feat(docs.ws): Update header part inside nav

--- apps/docs.blocksense.network/components/Logo.tsx
+++ apps/docs.blocksense.network/components/Logo.tsx
@@ -2,15 +2,33 @@ import React from 'react';
 
 export const Logo = () => {
   return (
-    <p
-      style={{
-        fontSize: '22px',
-        fontWeight: 700,
-        fontFamily: '"Space Mono", monospace',
-        color: 'rgb(30, 30, 30)',
-      }}
-    >
-      blocksense
-    </p>
+    <aside className="blocksense-header" style={styles.container}>
+      <img
+        className="blocksense-header__graphic"
+        src="/images/blocksense-header__graphic.jpg"
+        style={styles.header__graphic}
+        alt="Blocksense Logo"
+      />
+      <p className="blocksense-header__text" style={styles.text}>
+        Blocksense
+      </p>
+    </aside>
   );
+};
+
+const styles = {
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  header__graphic: {
+    height: '70px',
+    marginRight: '5px',
+  },
+  text: {
+    fontSize: '22px',
+    fontWeight: 700,
+    fontFamily: '"Space Mono", monospace',
+    color: 'rgb(30, 30, 30)',
+  },
 };
