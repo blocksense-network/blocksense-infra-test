04d4d8a5a54696de259542f4e80771011beb8633
Vanya Pavlova <vanya.pavlova@blocksense.network>
2024-05-31 13:14:50 +0300

    fix(docs.ws): Alter getting started section on the home page

--- apps/docs.blocksense.network/components/Button.tsx
+++ apps/docs.blocksense.network/components/Button.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import Link from 'next/link';
 
-export const Button = () => {
+export const Button = ({ label, link }) => {
   return (
-    <button
-      type="button"
-      className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-    >
-      <Link href="/getting-started">
-        Get started <span>→</span>
-      </Link>
-    </button>
+    <Link legacyBehavior href={link}>
+      <a className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+        {label}
+      </a>
+    </Link>
   );
 };
