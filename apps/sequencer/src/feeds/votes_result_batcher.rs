4a336aa28e27812c0791f826b19b5d18dce764b4
Hristo Staykov <izo8611@gmail.com>
2024-06-11 18:08:02 +0300

    fix(sequencer/updates_batching): Fix for time interval to wait before posting to the smart contracts

--- apps/sequencer/src/feeds/votes_result_batcher.rs
+++ apps/sequencer/src/feeds/votes_result_batcher.rs
@@ -4,7 +4,9 @@ use std::env;
 use std::fmt::Debug;
 use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
 use tokio::time::Duration;
-use tracing::{debug, info};
+use tracing::{debug, error, info, info_span};
+
+use crate::utils::time_utils::{get_ms_since_epoch, SlotTimeTracker};
 
 pub struct VotesResultBatcher {}
 
@@ -23,6 +25,8 @@ impl VotesResultBatcher {
         batched_votes_send: UnboundedSender<HashMap<K, V>>,
     ) -> VotesResultBatcher {
         spawn(async move {
+            let span = info_span!("VotesResultBatcher");
+            let _guard = span.enter();
             let batch_size_env = "SEQUENCER_MAX_KEYS_TO_BATCH";
             let max_keys_to_batch = match env::var(batch_size_env) {
                 Ok(batch_size) => batch_size
@@ -30,10 +34,7 @@ impl VotesResultBatcher {
                     .expect(format!("{} should be an unsigned integer.", batch_size_env).as_str()),
                 Err(_) => 1,
             };
-            info!(
-                "VotesResultBatcher: max_keys_to_batch set to {}",
-                max_keys_to_batch
-            );
+            info!("max_keys_to_batch set to {}", max_keys_to_batch);
             let duration_env = "SEQUENCER_KEYS_BATCH_DURATION";
             let timeout_duration = match env::var(duration_env) {
                 Ok(duration_env) => duration_env.parse::<u64>().expect(
@@ -45,9 +46,11 @@ impl VotesResultBatcher {
                 ),
                 Err(_) => 500,
             };
-            info!(
-                "VotesResultBatcher: timeout_duration set to {}",
-                timeout_duration
+            info!("timeout_duration set to {}", timeout_duration);
+
+            let mut stt = SlotTimeTracker::new(
+                Duration::from_millis(timeout_duration),
+                get_ms_since_epoch(),
             );
 
             loop {
@@ -56,7 +59,7 @@ impl VotesResultBatcher {
                 while !send_to_contract {
                     let var: Result<Option<(K, V)>, tokio::time::error::Elapsed> =
                         actix_web::rt::time::timeout(
-                            Duration::from_millis(timeout_duration),
+                            stt.get_duration_until_end_of_current_slot(),
                             vote_recv.recv(),
                         )
                         .await;
@@ -84,8 +87,14 @@ impl VotesResultBatcher {
                     };
                 }
                 if updates.keys().len() > 0 {
-                    let _ = batched_votes_send.send(updates); //TODO: handle when integrating anyhow::Result
+                    if let Err(e) = batched_votes_send.send(updates) {
+                        error!(
+                            "Channel for propagating batched updates to sender failed: {}",
+                            e.to_string()
+                        );
+                    }
                 }
+                stt.reset_report_start_time();
             }
         });
         VotesResultBatcher {}
