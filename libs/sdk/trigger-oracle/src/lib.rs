ebd689f4be937d623afda7e30e3f429a81e3edbb
stefan-nikolov96 <consultsen.stefan@gmail.com>
2024-06-28 19:15:29 +0300

    fix(sdk): Replace process_payload Payload with new type

--- libs/sdk/trigger-oracle/src/lib.rs
+++ libs/sdk/trigger-oracle/src/lib.rs
@@ -1,15 +1,11 @@
 use clap::Args;
-use data_feeds::types::DataFeedPayload;
-use serde::{Deserialize, Serialize};
+use data_feeds::types::{ DataFeedPayload, FeedResult, FeedType, PayloadMetaData };
+use serde::{ Deserialize, Serialize };
 use spin_app::MetadataKey;
-use spin_core::{async_trait, InstancePre};
-use spin_trigger::{TriggerAppEngine, TriggerExecutor};
-use std::{
-    collections::HashMap,
-    sync::Arc,
-    time::{SystemTime, UNIX_EPOCH},
-};
-use tokio::sync::mpsc::{channel, Receiver, Sender};
+use spin_core::{ async_trait, InstancePre };
+use spin_trigger::{ TriggerAppEngine, TriggerExecutor };
+use std::{ collections::HashMap, sync::Arc, time::{ SystemTime, UNIX_EPOCH } };
+use tokio::sync::mpsc::{ channel, Receiver, Sender };
 
 wasmtime::component::bindgen!({
     path: "../wit",
@@ -24,49 +20,49 @@ pub(crate) type _Store = spin_core::Store<RuntimeData>;
 
 #[derive(Args)]
 pub struct CliArgs {
-    /// If true, run each component once and exit
-    #[clap(long)]
-    pub test: bool,
-    ///// IP address of the sequencer
-    // #[clap(long = "sequencer-address", default_value = "127.0.0.1:3000", value_parser = parse_listen_addr)]
-    // pub sequencer_address: SocketAddr,
+  /// If true, run each component once and exit
+  #[clap(long)]
+  pub test: bool,
+  ///// IP address of the sequencer
+  // #[clap(long = "sequencer-address", default_value = "127.0.0.1:3000", value_parser = parse_listen_addr)]
+  // pub sequencer_address: SocketAddr,
 }
 
 // The trigger structure with all values processed and ready
 pub struct OracleTrigger {
-    engine: TriggerAppEngine<Self>,
-    sequencer: String,
-    queue_components: HashMap<String, Component>,
+  engine: TriggerAppEngine<Self>,
+  sequencer: String,
+  queue_components: HashMap<String, Component>,
 }
 
 // Picks out the timer entry from the application-level trigger settings
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
 struct TriggerMetadataParent {
-    settings: Option<TriggerMetadata>,
+  settings: Option<TriggerMetadata>,
 }
 
 // Application-level settings (raw serialization format)
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
 #[serde(deny_unknown_fields)]
 struct TriggerMetadata {
-    sequencer: Option<String>,
+  sequencer: Option<String>,
 }
 
 // Per-component settings (raw serialization format)
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
 #[serde(deny_unknown_fields)]
 pub struct OracleTriggerConfig {
-    component: String,
-    interval_secs: u64,
-    data_feed: String,
+  component: String,
+  interval_secs: u64,
+  data_feed: String,
 }
 
 #[derive(Clone, Debug)]
 struct Component {
-    pub id: String,
-    pub data_feed: String,
-    pub idle_wait: tokio::time::Duration,
-    pub oracle_settings: oracle::Settings,
+  pub id: String,
+  pub data_feed: String,
+  pub idle_wait: tokio::time::Duration,
+  pub oracle_settings: oracle::Settings,
 }
 
 // This is a placeholder - we don't yet detect any situations that would require
@@ -77,225 +73,216 @@ struct Component {
 #[allow(dead_code)]
 #[derive(Debug)]
 enum TerminationReason {
-    ExitRequested,
-    SequencerExitRequested,
-    Other(String),
+  ExitRequested,
+  SequencerExitRequested,
+  Other(String),
 }
 
 const TRIGGER_METADATA_KEY: MetadataKey<TriggerMetadataParent> = MetadataKey::new("triggers");
 
 #[async_trait]
 impl TriggerExecutor for OracleTrigger {
-    const TRIGGER_TYPE: &'static str = "oracle";
+  const TRIGGER_TYPE: &'static str = "oracle";
 
-    type RuntimeData = RuntimeData;
+  type RuntimeData = RuntimeData;
 
-    type TriggerConfig = OracleTriggerConfig;
+  type TriggerConfig = OracleTriggerConfig;
 
-    type RunConfig = CliArgs;
+  type RunConfig = CliArgs;
 
-    type InstancePre = InstancePre<RuntimeData>;
+  type InstancePre = InstancePre<RuntimeData>;
 
-    async fn new(engine: spin_trigger::TriggerAppEngine<Self>) -> anyhow::Result<Self> {
-        let sequencer = engine
-            .app()
-            .require_metadata(TRIGGER_METADATA_KEY)?
-            .settings
-            .unwrap_or_default()
-            .sequencer
-            .expect("Sequencer URL is not provided");
+  async fn new(engine: spin_trigger::TriggerAppEngine<Self>) -> anyhow::Result<Self> {
+    let sequencer = engine
+      .app()
+      .require_metadata(TRIGGER_METADATA_KEY)?
+      .settings.unwrap_or_default()
+      .sequencer.expect("Sequencer URL is not provided");
 
-        // TODO(adikov) There is a specific case in which one reporter receives task to report multiple
-        // data feeds which are gathered from one wasm component. For example -
-        // USD/BTC and USD/ETH. In that case we need to optimize calling the component once and
-        // returning results for both data feeds.
-        let queue_components = engine
-            .trigger_configs()
-            .map(|(_, config)| {
-                (
-                    config.data_feed.clone(),
-                    Component {
-                        id: config.component.clone(),
-                        data_feed: config.data_feed.clone(),
-                        idle_wait: tokio::time::Duration::from_secs(config.interval_secs),
-                        oracle_settings: oracle::Settings {
-                            id: config.data_feed.clone(),
-                        },
-                    },
-                )
-            })
-            .collect();
+    // TODO(adikov) There is a specific case in which one reporter receives task to report multiple
+    // data feeds which are gathered from one wasm component. For example -
+    // USD/BTC and USD/ETH. In that case we need to optimize calling the component once and
+    // returning results for both data feeds.
+    let queue_components = engine
+      .trigger_configs()
+      .map(|(_, config)| {
+        (
+          config.data_feed.clone(),
+          Component {
+            id: config.component.clone(),
+            data_feed: config.data_feed.clone(),
+            idle_wait: tokio::time::Duration::from_secs(config.interval_secs),
+            oracle_settings: oracle::Settings {
+              id: config.data_feed.clone(),
+            },
+          },
+        )
+      })
+      .collect();
 
-        tracing::info!("Oracle Trigger initialized: {}", &engine.app_name);
+    tracing::info!("Oracle Trigger initialized: {}", &engine.app_name);
 
-        Ok(Self {
-            engine,
-            sequencer,
-            queue_components,
-        })
+    Ok(Self {
+      engine,
+      sequencer,
+      queue_components,
+    })
+  }
+
+  async fn run(self, config: Self::RunConfig) -> anyhow::Result<()> {
+    let engine = Arc::new(self.engine);
+    if config.test {
+      for component in self.queue_components.values() {
+        Self::execute_wasm(engine.clone(), component).await?;
+      }
+      return Ok(());
     }
+    // This trigger spawns threads, which Ctrl+C does not kill.  So
+    // for this case we need to detect Ctrl+C and shut those threads
+    // down.  For simplicity, we do this by terminating the process.
+    tokio::spawn(async move {
+      tokio::signal::ctrl_c().await.unwrap();
+      std::process::exit(0);
+    });
 
-    async fn run(self, config: Self::RunConfig) -> anyhow::Result<()> {
-        let engine = Arc::new(self.engine);
-        if config.test {
-            for component in self.queue_components.values() {
-                Self::execute_wasm(engine.clone(), component).await?;
-            }
-            return Ok(());
-        }
-        // This trigger spawns threads, which Ctrl+C does not kill.  So
-        // for this case we need to detect Ctrl+C and shut those threads
-        // down.  For simplicity, we do this by terminating the process.
-        tokio::spawn(async move {
-            tokio::signal::ctrl_c().await.unwrap();
-            std::process::exit(0);
-        });
+    tracing::info!("Sequencer URL provided: {}", &self.sequencer);
+    let (tx, rx) = channel(32);
+    //TODO(adikov): Move all the logic to a different struct and handle
+    //errors properly.
+    // For each component, run its own timer loop
+    let mut loops: Vec<_> = self.queue_components
+      .into_values()
+      .map(|component| Self::start_oracle_loop(engine.clone(), tx.clone(), &component))
+      .collect();
 
-        tracing::info!("Sequencer URL provided: {}", &self.sequencer);
-        let (tx, rx) = channel(32);
-        //TODO(adikov): Move all the logic to a different struct and handle
-        //errors properly.
-        // For each component, run its own timer loop
-        let mut loops: Vec<_> = self
-            .queue_components
-            .into_values()
-            .map(|component| Self::start_oracle_loop(engine.clone(), tx.clone(), &component))
-            .collect();
+    let manager = Self::start_manager(rx, &self.sequencer);
+    loops.push(manager);
 
-        let manager = Self::start_manager(rx, &self.sequencer);
-        loops.push(manager);
+    let (tr, _, rest) = futures::future::select_all(loops).await;
 
-        let (tr, _, rest) = futures::future::select_all(loops).await;
-
-        drop(rest);
-        match tr {
-            Ok(TerminationReason::ExitRequested) => {
-                tracing::trace!("Exiting");
-                Ok(())
-            }
-            _ => {
-                tracing::trace!("Fatal: {:?}", tr);
-                Err(anyhow::anyhow!("{tr:?}"))
-            }
-        }
+    drop(rest);
+    match tr {
+      Ok(TerminationReason::ExitRequested) => {
+        tracing::trace!("Exiting");
+        Ok(())
+      }
+      _ => {
+        tracing::trace!("Fatal: {:?}", tr);
+        Err(anyhow::anyhow!("{tr:?}"))
+      }
     }
+  }
 }
 
 impl OracleTrigger {
-    fn start_oracle_loop(
-        engine: Arc<TriggerAppEngine<Self>>,
-        thread_tx: Sender<(String, Payload)>,
-        component: &Component,
-    ) -> tokio::task::JoinHandle<TerminationReason> {
-        let future = Self::execute(engine, thread_tx, component.clone());
-        tokio::task::spawn(future)
-    }
+  fn start_oracle_loop(
+    engine: Arc<TriggerAppEngine<Self>>,
+    thread_tx: Sender<(String, Payload)>,
+    component: &Component
+  ) -> tokio::task::JoinHandle<TerminationReason> {
+    let future = Self::execute(engine, thread_tx, component.clone());
+    tokio::task::spawn(future)
+  }
 
-    async fn execute(
-        engine: Arc<TriggerAppEngine<Self>>,
-        thread_tx: Sender<(String, Payload)>,
-        component: Component,
-    ) -> TerminationReason {
-        loop {
-            tokio::time::sleep(component.idle_wait).await;
-            let payload = match Self::execute_wasm(engine.clone(), &component).await {
-                Ok(payload) => payload,
-                Err(_error) => {
-                    //TODO(adikov): We need to come up with proper way of handling errors in wasm
-                    //components.
-                    continue;
-                }
-            };
-            match thread_tx.send((component.data_feed.clone(), payload)).await {
-                Ok(_) => continue,
-                Err(_) => break,
-            };
+  async fn execute(
+    engine: Arc<TriggerAppEngine<Self>>,
+    thread_tx: Sender<(String, Payload)>,
+    component: Component
+  ) -> TerminationReason {
+    loop {
+      tokio::time::sleep(component.idle_wait).await;
+      let payload = match Self::execute_wasm(engine.clone(), &component).await {
+        Ok(payload) => payload,
+        Err(_error) => {
+          //TODO(adikov): We need to come up with proper way of handling errors in wasm
+          //components.
+          continue;
         }
-
-        TerminationReason::Other("Oracle execution loop terminated".to_string())
-    }
-
-    fn start_manager(
-        rx: Receiver<(String, Payload)>,
-        sequencer: &str,
-    ) -> tokio::task::JoinHandle<TerminationReason> {
-        let future = Self::process_payload(rx, sequencer.to_owned());
-
-        tokio::task::spawn(future)
-    }
-
-    async fn process_payload(
-        mut rx: Receiver<(String, Payload)>,
-        sequencer: String,
-    ) -> TerminationReason {
-        while let Some((feed_id, payload)) = rx.recv().await {
-            let payload_json = DataFeedPayload {
-                //TODO(snikolov): Replace with new struct.
-                reporter_id: 1,
-                feed_id,
-                timestamp: current_unix_time(),
-                result: payload.body.unwrap(),
-            };
-
-            let client = reqwest::Client::new();
-            match client
-                .post(sequencer.clone())
-                .json(&payload_json)
-                .send()
-                .await
-            {
-                Ok(res) => {
-                    let contents = res.text().await.unwrap();
-                    tracing::trace!("Sequencer responded with: {}", &contents);
-                }
-                Err(e) => {
-                    tracing::error!("Sequencer failed to respond with: {}", &e);
-                    break;
-                }
-            };
+      };
+      match thread_tx.send((component.data_feed.clone(), payload)).await {
+        Ok(_) => {
+          continue;
         }
-
-        TerminationReason::SequencerExitRequested
-    }
-
-    async fn execute_wasm(
-        engine: Arc<TriggerAppEngine<Self>>,
-        component: &Component,
-    ) -> anyhow::Result<Payload> {
-        // Load the guest...
-        let component_id = component.id.clone();
-        let (instance, mut store) = engine.prepare_instance(&component_id).await?;
-        let instance = BlocksenseOracle::new(&mut store, &instance)?;
-        // ...and call the entry point
-        tracing::trace!(
-            "Triggering application: {}; component_id: {}; data_feed: {}",
-            &engine.app_name,
-            component_id,
-            &component.data_feed
-        );
-        match instance
-            .call_handle_oracle_request(&mut store, &component.oracle_settings)
-            .await
-        {
-            Ok(Ok(payload)) => {
-                tracing::info!("Component {component_id} completed okay");
-                Ok(payload)
-            }
-            Ok(Err(e)) => {
-                tracing::warn!("Component {component_id} returned error {:?}", e);
-                Err(anyhow::anyhow!("Component {component_id} returned error")) // TODO: more details when WIT provides them
-            }
-            Err(e) => {
-                tracing::error!("error running component {component_id}: {:?}", e);
-                Err(anyhow::anyhow!("Error executing component {component_id}"))
-            }
+        Err(_) => {
+          break;
         }
+      };
     }
+
+    TerminationReason::Other("Oracle execution loop terminated".to_string())
+  }
+
+  fn start_manager(
+    rx: Receiver<(String, Payload)>,
+    sequencer: &str
+  ) -> tokio::task::JoinHandle<TerminationReason> {
+    let future = Self::process_payload(rx, sequencer.to_owned());
+
+    tokio::task::spawn(future)
+  }
+
+  async fn process_payload(
+    mut rx: Receiver<(String, Payload)>,
+    sequencer: String
+  ) -> TerminationReason {
+    while let Some((feed_id, payload)) = rx.recv().await {
+      let payload_json = DataFeedPayload {
+        payload_metadata: PayloadMetaData {
+          reporter_id: 1,
+          feed_id: feed_id,
+          timestamp: current_unix_time(),
+        },
+        result: FeedResult::Result { result: FeedType::Numerical(payload.body.unwrap() as f64) },
+      };
+
+      let client = reqwest::Client::new();
+      match client.post(sequencer.clone()).json(&payload_json).send().await {
+        Ok(res) => {
+          let contents = res.text().await.unwrap();
+          tracing::trace!("Sequencer responded with: {}", &contents);
+        }
+        Err(e) => {
+          tracing::error!("Sequencer failed to respond with: {}", &e);
+          break;
+        }
+      };
+    }
+
+    TerminationReason::SequencerExitRequested
+  }
+
+  async fn execute_wasm(
+    engine: Arc<TriggerAppEngine<Self>>,
+    component: &Component
+  ) -> anyhow::Result<Payload> {
+    // Load the guest...
+    let component_id = component.id.clone();
+    let (instance, mut store) = engine.prepare_instance(&component_id).await?;
+    let instance = BlocksenseOracle::new(&mut store, &instance)?;
+    // ...and call the entry point
+    tracing::trace!(
+      "Triggering application: {}; component_id: {}; data_feed: {}",
+      &engine.app_name,
+      component_id,
+      &component.data_feed
+    );
+    match instance.call_handle_oracle_request(&mut store, &component.oracle_settings).await {
+      Ok(Ok(payload)) => {
+        tracing::info!("Component {component_id} completed okay");
+        Ok(payload)
+      }
+      Ok(Err(e)) => {
+        tracing::warn!("Component {component_id} returned error {:?}", e);
+        Err(anyhow::anyhow!("Component {component_id} returned error")) // TODO: more details when WIT provides them
+      }
+      Err(e) => {
+        tracing::error!("error running component {component_id}: {:?}", e);
+        Err(anyhow::anyhow!("Error executing component {component_id}"))
+      }
+    }
+  }
 }
 
 pub fn current_unix_time() -> u64 {
-    SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .expect("SystemTime before UNIX EPOCH!")
-        .as_secs()
+  SystemTime::now().duration_since(UNIX_EPOCH).expect("SystemTime before UNIX EPOCH!").as_secs()
 }
