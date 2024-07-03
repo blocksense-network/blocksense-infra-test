52befd88f00ce47ec8113c62183978b0ca18cfdf
Vanya Pavlova <vanya.pavlova@blocksense.network>
2024-07-03 12:48:27 +0300

    update(docs.ws): Update tailwind.config.js

--- apps/docs.blocksense.network/tailwind.config.ts
+++ apps/docs.blocksense.network/tailwind.config.ts
@@ -3,10 +3,10 @@ import type { Config } from 'tailwindcss';
 const config = {
   darkMode: ['class'],
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './@/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     container: {
