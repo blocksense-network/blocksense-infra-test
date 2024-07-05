6c957d8dbcc86bf4a91cfd1e066522e4d9fadf86
Kalin Hristov <kalinsky.hristov@gmail.com>
2024-07-05 18:19:11 +0300

    refactor(docs.ws): refactor generate-contracts-mdx.ts

--- apps/docs.blocksense.network/src/generate-contracts-mdx.ts
+++ apps/docs.blocksense.network/src/generate-contracts-mdx.ts
@@ -1,18 +1,16 @@
 import * as path from 'path';
 import React from 'react';
 
-import { writeGeneratedMdx } from './write-generated-mdx';
-import { writeMetaJsonFile } from './write-meta-json-file';
+import { pagesContractRefDocFolder } from './constants';
 
 import { SourceUnitDocItem } from '@blocksense/sol-reflector';
 
 import { SourceUnit } from '@/sol-contracts-components/SourceUnit';
+import { createFsIOForDir } from './fs-IO-for-dir';
 import { createStaticComponent } from './utils';
 
 import SOL_REFLECTION_JSON from '@blocksense/contracts/docs/fine';
 const solReflection = SOL_REFLECTION_JSON as SourceUnitDocItem[];
-
-type MetaJSON = Record<string, string>;
 
 function generateMarkdownContent(sourceUnit: SourceUnitDocItem): string {
   const sourceUnitComponent = React.createElement(SourceUnit, { sourceUnit });
@@ -21,22 +19,27 @@ function generateMarkdownContent(sourceUnit: SourceUnitDocItem): string {
   return staticComponent;
 }
 
-function generateSolRefDocFiles() {
-  const metaJSON: MetaJSON = {};
+function generateSolRefDocFiles(): Promise<void[]> {
+  const mdxFiles = solReflection.map(sourceUnit => ({
+    name: path.parse(sourceUnit.absolutePath).name,
+    content: generateMarkdownContent(sourceUnit),
+  }));
 
-  solReflection.forEach((sourceUnit: SourceUnitDocItem) => {
-    const content = generateMarkdownContent(sourceUnit);
-    const fileName = path
-      .basename(sourceUnit.absolutePath)
-      .replace('.sol', '.mdx');
+  const metaJSON = mdxFiles.reduce(
+    (obj, { name }) => ({ [name]: name, ...obj }),
+    {},
+  );
 
-    writeGeneratedMdx(fileName, content);
+  const { write, writeJSON } = createFsIOForDir(pagesContractRefDocFolder);
 
-    const fileNameWithoutExtension = path.basename(fileName, '.mdx');
-    metaJSON[fileNameWithoutExtension] = fileNameWithoutExtension;
-  });
-
-  writeMetaJsonFile(metaJSON);
+  return Promise.all([
+    ...mdxFiles.map(args => write({ ext: '.mdx', ...args })),
+    writeJSON({ base: '_meta.json', content: metaJSON }),
+  ]);
 }
 
-generateSolRefDocFiles();
+generateSolRefDocFiles()
+  .then(() => console.log('Files generated!'))
+  .catch(err => {
+    console.log(err);
+  });
