49e673e997e3578b925989875a8779c9a32effd7
Radoslav Dimchev <radoslav.dimchev@blocksense.network>
2024-07-05 19:42:39 +0300

    feat(docs.ws): Integrate shiki code block to signature

--- apps/docs.blocksense.network/components/sol-contracts/Signature.tsx
+++ apps/docs.blocksense.network/components/sol-contracts/Signature.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { codeToHtml } from 'shiki';
 
 type SignatureProps = {
   signature?: string;
 };
 
 export const Signature = ({ signature }: SignatureProps) => {
-  return <div>{signature && signature}</div>;
+  const [html, setHtml] = React.useState('');
+
+  React.useEffect(() => {
+    codeToHtml(signature!, {
+      lang: 'solidity',
+      theme: 'catppuccin-mocha',
+    })
+      .then((htmlString: any) => setHtml(htmlString))
+      .finally(() => console.log(html));
+  }, []);
+
+  return <div dangerouslySetInnerHTML={{ __html: html }} />;
 };
