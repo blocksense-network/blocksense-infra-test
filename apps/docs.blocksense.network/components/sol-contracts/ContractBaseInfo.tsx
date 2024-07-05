86a36be0324102b5352665c939673a855a327e81
Radoslav Dimchev <radoslav.dimchev@blocksense.network>
2024-07-05 19:41:56 +0300

    fix(docs.ws): Hydration error of card description for contract

--- apps/docs.blocksense.network/components/sol-contracts/ContractBaseInfo.tsx
+++ apps/docs.blocksense.network/components/sol-contracts/ContractBaseInfo.tsx
@@ -16,32 +16,14 @@ export const ContractBaseInfo = ({ ...contract }: ContractDocItem) => {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>### {contract.name}</CardTitle>
+        <CardTitle>{contract.name}</CardTitle>
         <CardDescription>
-          <aside className="p-4 contract-details">
-            <section className="flex space-x-4 contract-details__content">
-              <section className="flex-shrink-0 contract-details__item">
-                <section className="flex items-center">
-                  <dt className="text-sm text-gray-600 contract-details__label">
-                    Kind:
-                  </dt>
-                  <dd className="ml-2 font-semibold text-black contract-details__value">
-                    {contract.contractKind}
-                  </dd>
-                </section>
-              </section>
-              <section className="flex-shrink-0 contract-details__item">
-                <section className="flex items-center">
-                  <dt className="text-sm text-gray-600 contract-details__label">
-                    Abstract:
-                  </dt>
-                  <dd className="ml-2 font-semibold text-black contract-details__value">
-                    {contract.abstract.toString()}
-                  </dd>
-                </section>
-              </section>
-            </section>
-          </aside>
+          <span className="text-sm text-gray-600 contract-details__label">
+            Kind: {contract.contractKind}
+          </span>
+          <span className="text-sm text-gray-600 contract-details__label">
+            Abstract: {contract.abstract.toString()}
+          </span>
         </CardDescription>
       </CardHeader>
       {contract._baseContracts.length > 0 && (
