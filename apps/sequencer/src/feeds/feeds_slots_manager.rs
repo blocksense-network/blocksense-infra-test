5d6f3e9bdc38740a03b603a67a8eb7b32c08cbbb
Stan Manilov <stan.manilov@metacraft-labs.com>
2024-07-19 16:20:46 +0300

    refactor(sequencer): Add unit test for feeds_slots_manager
    This combines unit tests from feeds_slots_processor and admin.rs.

--- apps/sequencer/src/feeds/feeds_slots_manager.rs
+++ apps/sequencer/src/feeds/feeds_slots_manager.rs
@@ -98,3 +98,82 @@ pub async fn feeds_slots_manager_loop<
         Ok({})
     })
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::config::config::init_sequencer_config;
+    use crate::feeds::feeds_registry::{new_feeds_meta_data_reg_from_config, AllFeedsReports};
+    use crate::plugin_registry;
+    use crate::providers::provider::init_shared_rpc_providers;
+    use crate::reporters::reporter::init_shared_reporters;
+    use crate::utils::logging::init_shared_logging_handle;
+    use actix_web::{http::header::ContentType, test, App};
+    use data_feeds::feeds_processing::naive_packing;
+    use data_feeds::types::FeedType;
+    use std::env;
+    use std::path::PathBuf;
+    use std::sync::{Arc, RwLock};
+    use std::time::{Duration, SystemTime, UNIX_EPOCH};
+    use tokio::sync::mpsc::unbounded_channel;
+    use utils::to_hex_string;
+
+    #[actix_web::test]
+    async fn test_feed_slots_manager_loop() {
+        let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+        let tests_dir_path = PathBuf::new().join(manifest_dir).join("tests");
+        env::set_var("SEQUENCER_CONFIG_DIR", tests_dir_path);
+        let log_handle = init_shared_logging_handle();
+        let sequencer_config = init_sequencer_config();
+        let all_feeds_reports = AllFeedsReports::new();
+        let all_feeds_reports_arc = Arc::new(RwLock::new(all_feeds_reports));
+        let providers = init_shared_rpc_providers(&sequencer_config).await;
+
+        let original_report_data = FeedType::Numerical(13.0);
+
+        // we are specifically sending only one report message as we don't want to test the average processor
+        let feed_id = 1;
+        let reporter_id = 42;
+        all_feeds_reports_arc.write().unwrap().push(
+            feed_id,
+            reporter_id,
+            original_report_data.clone(),
+        );
+
+        let app_state = web::Data::new(FeedsState {
+            registry: Arc::new(RwLock::new(new_feeds_meta_data_reg_from_config(
+                &sequencer_config,
+            ))),
+            reports: all_feeds_reports_arc,
+            plugin_registry: Arc::new(RwLock::new(plugin_registry::CappedHashMap::new())),
+            providers: providers.clone(),
+            log_handle,
+            reporters: init_shared_reporters(&sequencer_config),
+        });
+
+        let (tx, mut rx) = unbounded_channel::<(String, String)>();
+
+        let future = feeds_slots_manager_loop(app_state, tx).await;
+
+        // Attempt to receive with a timeout of 2 seconds
+        let received = tokio::time::timeout(Duration::from_secs(60), rx.recv()).await;
+
+        match received {
+            Ok(Some((key, result))) => {
+                /// assert the received data
+                assert_eq!(
+                    key,
+                    to_hex_string(feed_id.to_be_bytes().to_vec(), None),
+                    "The key does not match the expected value"
+                );
+                assert_eq!(result, naive_packing(original_report_data));
+            }
+            Ok(None) => {
+                panic!("The channel was closed before receiving any data");
+            }
+            Err(_) => {
+                panic!("The channel did not receive any data within the timeout period");
+            }
+        }
+    }
+}
