539efc0876e1f00686ea17ee339acf3b8cd941bb
Radoslav Dimchev <radoslav.dimchev@blocksense.network>
2024-07-25 14:19:46 +0300

    feat(docs.ws): Implement custom copy button

---
+++ apps/docs.blocksense.network/components/common/CopyButton.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+type CopyButtonProps = {
+  textToCopy: string;
+};
+
+export const CopyButton = ({ textToCopy }: CopyButtonProps) => {
+  const [isCopied, setIsCopied] = React.useState(false);
+
+  const onCopy = () => {
+    navigator.clipboard.writeText(textToCopy);
+    setIsCopied(true);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1500);
+  };
+
+  return isCopied ? (
+    <img src="/icons/check.svg" alt="Copied" className="w-5 h-5" />
+  ) : (
+    <img
+      src="/icons/clipboard.svg"
+      alt="Clipboard"
+      onClick={onCopy}
+      className="w-5 h-5 cursor-pointer"
+    />
+  );
+};
