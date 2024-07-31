ff64abb52ed89612fa3553795ed1fce47bcd15b7
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2024-07-31 17:44:43 +0300

    feat(base-utils/fs): Add `jsdoc` to enhance IntelliSense

--- libs/base-utils/src/fs.ts
+++ libs/base-utils/src/fs.ts
@@ -40,38 +40,78 @@ interface PathObject {
  * @returns {Object} An object containing the following methods:
  *
  * - `write`: Writes a string to a file in the base directory.
- *   @param {Object} args - An object containing file path details and the content to write.
- *   @param {string} args.content - The content to write to the file.
+ *   @param {FileArgs & { content: string }} args - An object containing file path details and the content to write.
  *
  * - `writeJSON`: Writes a JSON object to a file in the base directory.
- *   @param {Object} args - An object containing file path details and the content to write.
- *   @param {Object} args.content - The JSON content to write to the file.
+ *   @param {FileArgs & { content: Record<string, unknown> }} args - An object containing file path details and the JSON content to write.
  *
  * - `read`: Reads a file as a string from the base directory.
- *   @param {Object} args - An object containing file path details.
+ *   @param {FileArgs} args - An object containing file path details.
  *
  * - `readJSON`: Reads a JSON file from the base directory.
- *   @param {Object} args - An object containing file path details.
+ *   @param {FileArgs} args - An object containing file path details.
  *
  * Each `args` object should omit the 'dir' property from the `PathObject` type, as the directory is already specified by `baseDir`.
  */
 export function selectDirectory(baseDir: string) {
-  type Location = Omit<PathObject, 'dir'>;
+  type FileArgs = Omit<PathObject, 'dir' | `root`>;
 
   return {
-    write: (args: Location & { content: string }) =>
+    /**
+     * Writes content to a file at the specified directory.
+     *
+     * Params:
+     * - **args** - The arguments for specifying the file and content.
+     * - **args.base** -The file name including extension (if any) such as 'index.html'
+     * - **args.ext** - The file extension (if any) such as '.html'
+     * - **args.name** - The file name without extension (if any) such as 'index'
+     * - **args.content** - The content to be written to the file.
+     * @returns A promise that resolves when the file is written.
+     */
+    write: (args: FileArgs & { content: string }) =>
       fs.writeFile(path.format({ dir: baseDir, ...args }), args.content),
 
-    writeJSON: (args: Location & { content: Record<string, unknown> }) =>
+    /**
+     * Writes a JSON object to a file at the specified directory.
+     *
+     * Params:
+     * - **args** - The arguments for specifying the file and content.
+     * - **args.base** - The file name including extension (if any) such as 'data.json'
+     * - **args.ext** - The file extension (if any) such as '.json'
+     * - **args.name** - The file name without extension (if any) such as 'data'
+     * - **args.content** - The JSON content to be written to the file.
+     * @returns A promise that resolves when the file is written.
+     */
+    writeJSON: (args: FileArgs & { content: Record<string, unknown> }) =>
       fs.writeFile(
         path.format({ dir: baseDir, ...args }),
         JSON.stringify(args.content, null, 2),
       ),
 
-    read: (args: Location) =>
+    /**
+     * Reads content from a file at the specified directory.
+     *
+     * Params:
+     * - **args** - The arguments for specifying the file.
+     * - **args.base** - The file name including extension (if any) such as 'index.html'
+     * - **args.ext** - The file extension (if any) such as '.html'
+     * - **args.name** - The file name without extension (if any) such as 'index'
+     * @returns A promise that resolves with the file content as a string.
+     */
+    read: (args: FileArgs) =>
       fs.readFile(path.format({ dir: baseDir, ...args }), 'utf8'),
 
-    readJSON: (args: Location) =>
+    /**
+     * Reads a JSON object from a file at the specified directory.
+     *
+     * Params:
+     * - **args** - The arguments for specifying the file.
+     * - **args.base** - The file name including extension (if any) such as 'data.json'
+     * - **args.ext** - The file extension (if any) such as '.json'
+     * - **args.name** - The file name without extension (if any) such as 'data'
+     * @returns A promise that resolves with the parsed JSON object.
+     */
+    readJSON: (args: FileArgs) =>
       fs
         .readFile(path.format({ dir: baseDir, ...args }), 'utf8')
         .then(JSON.parse),
