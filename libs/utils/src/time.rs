9633356c41949cd88ed785179e6eda7591b9c097
stefan-nikolov96 <consultsen.stefan@gmail.com>
2024-07-31 11:50:15 +0300

    fix(utils): report UNIX time as millis

--- libs/utils/src/time.rs
+++ libs/utils/src/time.rs
@@ -4,5 +4,5 @@ pub fn current_unix_time() -> u128 {
     SystemTime::now()
         .duration_since(UNIX_EPOCH)
         .expect("SystemTime before UNIX EPOCH!")
-        .as_secs() as u128
+        .as_millis() as u128
 }
