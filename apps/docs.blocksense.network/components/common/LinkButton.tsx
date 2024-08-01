ced4c952506b7aa723b173863a3710bb062ffe02
Kalin Hristov <kalinsky.hristov@gmail.com>
2024-08-01 15:22:31 +0300

    feat(docs.ws): `LinkButton` component uses `shadcn\ui`

--- apps/docs.blocksense.network/components/common/LinkButton.tsx
+++ apps/docs.blocksense.network/components/common/LinkButton.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import Link from 'next/link';
+import { Button } from '@/components/ui/button';
 
-type LinkButtonProps = {
-  label: string;
-  link: string;
-  target?: string;
-};
+interface ButtonProps {
+  href?: string;
+  external?: boolean;
+  className?: string;
+  children?: React.ReactNode;
+  asChild?: true;
+}
 
-export const LinkButton = ({ label, link, target }: LinkButtonProps) => {
+export const LinkButton = ({
+  href,
+  external = false,
+  className,
+  children,
+}: ButtonProps) => {
+  const classes = `link_button me-2 mb-2 font-bold ${className} `;
+
   return (
-    <Link legacyBehavior href={link}>
-      <a
-        target={target}
-        rel={target === '_blank' ? 'noopener noreferrer' : undefined}
-        className="link_button text-white min-w-28 bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-bold rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-      >
-        {label}
-      </a>
-    </Link>
+    <Button className={classes} asChild>
+      {external ? (
+        <a href={href} target="_blank" rel="noopener noreferrer">
+          {children}
+        </a>
+      ) : (
+        href && <Link href={href}>{children}</Link>
+      )}
+    </Button>
   );
 };
