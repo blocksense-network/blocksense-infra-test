14b99f0fba3f1bba7d2da4c6c7a0ede2c2f64a71
Vanya Pavlova <vanya.pavlova@blocksense.network>
2024-08-02 14:29:18 +0300

    feat(docs.ws): Add copy-to-clipboard functionality to Signature

--- apps/docs.blocksense.network/components/sol-contracts/Signature.tsx
+++ apps/docs.blocksense.network/components/sol-contracts/Signature.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { codeToHtml } from 'shiki';
+import { CopyButton } from '@/components/common/CopyButton';
 
 type SignatureProps = {
   signature?: string;
@@ -9,18 +10,25 @@ export const Signature = ({ signature = '' }: SignatureProps) => {
   const [html, setHtml] = React.useState('');
 
   React.useEffect(() => {
-    codeToHtml(signature!, {
+    codeToHtml(signature, {
       lang: 'solidity',
       theme: 'material-theme-lighter',
     }).then((htmlString: any) => setHtml(htmlString));
-  }, []);
+  }, [signature]);
 
   return (
     signature && (
-      <div
-        className="signature__content border border-gray-800 mb-2 py-2"
-        dangerouslySetInnerHTML={{ __html: html }}
-      />
+      <section className="signature__container border border-gray-800 mb-2 px-0 py-2 rounded-md bg-slate-100">
+        <div className="signature__content flex justify-between items-start w-full">
+          <div
+            className="signature__code flex-grow mr-2"
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+          <aside className="signature__copy-button flex-shrink-0 mr-6">
+            <CopyButton textToCopy={signature} />
+          </aside>
+        </div>
+      </section>
     )
   );
 };
