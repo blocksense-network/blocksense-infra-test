7d3588347e3ce72fdb9e709beaf4c4d73f3780af
Stefan Nikolov <consultsen.stefan@gmail.com>
2024-08-12 12:38:04 +0300

    chore(nix): Formatting

--- nix/shells/pkg-sets/rust.nix
+++ nix/shells/pkg-sets/rust.nix
@@ -3,13 +3,14 @@
   self',
   config,
   ...
-}: {
+}:
+{
   env.SEQUENCER_CONFIG_DIR = config.devenv.root + "/apps/sequencer/";
   env.REPORTER_SECRET_KEY_FILE_PATH = config.devenv.root + "/apps/reporter";
   env.FEEDS_CONFIG_DIR = config.devenv.root + "/libs/feed_registry";
 
   packages =
-    [self'.legacyPackages.rustToolchain]
+    [ self'.legacyPackages.rustToolchain ]
     ++ (with pkgs; [
       openssl
       pkg-config
