500ffac53459dde1507079a6fb0ebceafe626610
Stefan Nikolov <consultsen.stefan@gmail.com>
2024-08-12 10:40:47 +0300

    chore(nix): Add FeedConfig Dir to nix config

--- nix/shells/pkg-sets/rust.nix
+++ nix/shells/pkg-sets/rust.nix
@@ -2,6 +2,7 @@
 {
   env.SEQUENCER_CONFIG_DIR = "./apps/sequencer/";
   env.REPORTER_SECRET_KEY_FILE_PATH = "./apps/reporter";
+  env.FEEDS_CONFIG_DIR = "./libs/feed_registry";
 
   packages =
     [ self'.legacyPackages.rustToolchain ]
