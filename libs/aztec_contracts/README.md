915e2da791798e580f583bf890d2f0054c83f419
Nikolay Kostadinov <nikolaykostadinov21@gmail.com>
2024-08-15 17:29:19 +0300

    feat(aztec_contracts): Implement data feed store smart contract in Noir.
    Implement 3 test cases respectively for deployment of contract, unauthorized setting of feed, setting and getting 10 feeds in one transaction.
    
    docs(README): Update README
    
    docs(README): Update README
    
    docs(README): Update README
    
    docs(README): Update README

---
+++ libs/aztec_contracts/README.md
@@ -0,0 +1,66 @@
+# Data Feed Storage
+
+## Overview
+
+```text
+aztec_contracts
+├── contracts/
+│   ├── data_feed_store/
+│   │   ├── src
+│   │   │    ├── artifacts/
+│   │   │    │   └── ... type-safe methods for deploying and interacting
+│   │   │    │           with your contract based on their artifacts
+│   │   │    │
+│   │   │    └── main.nr
+│   │   ├── codegenCache.json
+│   │   └── target/
+│   │       └── ... Contains the Noir ABI artifacts
+│   │
+├── tests/
+│   └── data_feed.test.ts
+│
+├── jest.integration.config
+├── package.json
+└── tsconfig.json
+```
+
+### Important Note:
+
+Bit-wise operations, albeit used in [our Solidity smart contracts](https://github.com/blocksense-network/blocksense/tree/main/libs/contracts), are on the contrary expensive in gates when performed within circuits. That's why the Noir contract calls aren't handled based on the function selector [like their Solidity precursors](https://github.com/blocksense-network/blocksense/tree/main/libs/contracts#calls). That's because Noir program(e.g. in our case the `data_feed_store` contract) compiles to an Abstract Circuit Intermediate Representation which is a tree structure composed of Leaves(**inputs**) and Nodes (which contain arithmetic operations to combine them(**gates**)). And since circuits are made of arithmetic gates, the cost of arithmetic operations tends to be one gate. Whereas for procedural code, they represent several clock cycles. Thus the decision of having a simple mapping between `id` of the data feed and its data represented as 32 `bytes` in the `data_feed_store` contract.
+
+The `contracts` folder contains the Data feed store contract, its ABI artifacts and type-safe methods for deploying and interacting with the contract based on its artifacts. The `tests` folder contains the `data_feed.test.ts` test.
+
+It has three tests respectively:
+
+- Checks that the caller isn't the owner
+- Checks setting and getting of 10 feeds in a single transaction
+- Checks the deployment of the contract
+
+## Testing
+
+To execute the tests you must have the following prerequisites:
+
+1.  Node.js >= v18 (recommend installing with nvm)
+2.  Docker (visit this page of the Docker docs on how to install it)
+3.  To install aztec's tooling, including aztec's sandbox, aztec-cli, aztec-nargo and aztec-up, run:
+
+        bash -i <(curl -s install.aztec.network)
+
+4.  Once these have been installed, to start the sandbox, run:
+
+        aztec start --sandbox
+
+5.  Go to the `data_feed_store` directory and execute:
+
+        aztec-nargo compile --silence-warnings
+
+    This will generate contracts ABI artifacts in the `target` folder.
+
+6.  Next, generate the typescript bindings for the ABI artifacts using:
+
+        aztec codegen target -o src/artifacts
+
+7.  When you complete these steps, you can run the tests by going returning to the root directory `aztec_contracts` and executing:
+
+        yarn install
+        yarn test
---
+++ libs/aztec_contracts/tests/data_feed.test.ts
@@ -0,0 +1,124 @@
+import {
+  DataFeedStoreContractArtifact,
+  DataFeedStoreContract,
+} from '../contracts/data_feed_store/src/artifacts/DataFeedStore';
+import {
+  AccountWallet,
+  CompleteAddress,
+  ContractDeployer,
+  Fr,
+  PXE,
+  waitForPXE,
+  TxStatus,
+  createPXEClient,
+  getContractInstanceFromDeployParams,
+} from '@aztec/aztec.js';
+import { getInitialTestAccountsWallets } from '@aztec/accounts/testing';
+import { describe } from 'node:test';
+
+const setupSandbox = async () => {
+  const { PXE_URL = 'http://localhost:8080' } = process.env;
+  const pxe = createPXEClient(PXE_URL);
+  await waitForPXE(pxe);
+  return pxe;
+};
+
+describe('Data feed store contract', () => {
+  let pxe: PXE;
+  let wallets: AccountWallet[] = [];
+  let accounts: CompleteAddress[] = [];
+
+  beforeAll(async () => {
+    pxe = await setupSandbox();
+
+    wallets = await getInitialTestAccountsWallets(pxe);
+    accounts = wallets.map(w => w.getCompleteAddress());
+  });
+
+  it('Deploys the contract', async () => {
+    const salt = Fr.random();
+    const dataFeedStoreContractArtifact = DataFeedStoreContractArtifact;
+    const deployArgs = wallets[0].getCompleteAddress().address;
+
+    const deploymentData = getContractInstanceFromDeployParams(
+      dataFeedStoreContractArtifact,
+      {
+        constructorArgs: [deployArgs],
+        salt,
+        deployer: wallets[0].getAddress(),
+      },
+    );
+
+    const deployer = new ContractDeployer(
+      dataFeedStoreContractArtifact,
+      wallets[0],
+    );
+    const tx = deployer.deploy(deployArgs).send({ contractAddressSalt: salt });
+    const receipt = await tx.getReceipt();
+
+    expect(receipt).toEqual(
+      expect.objectContaining({
+        status: TxStatus.PENDING,
+        error: '',
+      }),
+    );
+
+    const receiptAfterMined = await tx.wait({ wallet: wallets[0] });
+
+    expect(await pxe.getContractInstance(deploymentData.address)).toBeDefined();
+    expect(
+      await pxe.isContractPubliclyDeployed(deploymentData.address),
+    ).toBeDefined();
+    expect(receiptAfterMined).toEqual(
+      expect.objectContaining({
+        status: TxStatus.SUCCESS,
+      }),
+    );
+
+    expect(receiptAfterMined.contract.instance.address).toEqual(
+      deploymentData.address,
+    );
+  }, 300_000);
+
+  it('Should revert if the caller is not the owner', async () => {
+    const index_zero = new Fr(0);
+    const data = Array.from(
+      { length: 32 },
+      () => new Fr(Math.floor(Math.random() * 256)),
+    );
+
+    const contract = await DataFeedStoreContract.deploy(wallets[0])
+      .send()
+      .deployed();
+
+    await expect(
+      contract
+        .withWallet(wallets[1])
+        .methods.setFeed(data, index_zero)
+        .send()
+        .wait(),
+    ).rejects.toThrow('You are not the owner!');
+  }, 300_000);
+
+  it.only('Should get and set 10 feeds in a single transaction', async () => {
+    const data = Array.from(
+      { length: 32 },
+      () => new Fr(Math.floor(Math.random() * 256)),
+    );
+
+    const contract = await DataFeedStoreContract.deploy(wallets[0])
+      .send()
+      .deployed();
+
+    for (let i = 0; i < 10; i++) {
+      const index_i = new Fr(i);
+      await contract.methods.setFeed(data, index_i).send().wait();
+      const get_feed_tx = await contract.methods.getFeed(index_i).simulate();
+      for (let j = 0; j < 32; j++) {
+        expect(Number(get_feed_tx[j])).toEqual(
+          parseInt(data[j].value.toString(16), 16),
+        );
+      }
+    }
+  }, 300_000);
+});
