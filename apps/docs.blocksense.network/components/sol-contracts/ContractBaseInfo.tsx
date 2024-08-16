41f7948afe7f214bd818466960e1124529998134
Vanya Pavlova <vanya.pavlova@blocksense.network>
2024-08-16 10:19:40 +0300

    feat(docs.ws): Modify ContractBaseInfo against latest version of docs - remove shadcn ui card component - add icons for kind and abstract sections - apply minor changes to Natspec component for the scope of   ContractBaseInfo - Modify ContractBaseInfo code to be more compact

--- apps/docs.blocksense.network/components/sol-contracts/ContractBaseInfo.tsx
+++ apps/docs.blocksense.network/components/sol-contracts/ContractBaseInfo.tsx
@@ -1,13 +1,6 @@
 import React from 'react';
 
 import { ContractDocItem } from '@blocksense/sol-reflector';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-} from '@/components/ui/card';
 
 import { NatSpec } from '@/sol-contracts-components/NatSpec';
 import { AnchorLinkTitle } from '@/sol-contracts-components/AnchorLinkTitle';
@@ -17,26 +10,63 @@ export const ContractBaseInfo = ({ ...contract }: ContractDocItem) => {
   return (
     <>
       <AnchorLinkTitle title={contract.name} titleLevel={2} />
-      <div className="contract-base-info border-solid border border-slate-100 bg-gray-50 py-3 lg:px-6  mt-6 flex items-start space-y-4">
-        <Card className="contract-base-info__card w-full bg-white shadow-md rounded-lg mt-4 mb-4 py-3 px-3">
-          <CardHeader className="contract-base-info__header">
-            <CardDescription className="contract-base-info__description bg-gray-50 rounded-lg font-bold px-3 py-2 flex items-center">
-              <span className="contract-base-info__kind text-base text-black">
-                Kind: {contract.contractKind}
-              </span>
-              <span className="contract-base-info__abstract__abstract ml-4 text-base text-black">
-                {contract.contractKind == 'contract' &&
-                  `Abstract: ${contract.abstract.toString()}`}
-              </span>
-            </CardDescription>
-          </CardHeader>
+      <div className="contract-base-info border-solid border border-slate-200 bg-white shadow-md px-2 py-2 md:px-4 md:pt-4 pb-0 rounded-md mt-6 flex flex-col lg:flex-row items-start">
+        <section className="contract-base-info__content w-full">
+          <header className="contract-base-info__header px-2 py-2 mb-3 flex flex-col lg:flex-row justify-between lg:items-center">
+            <h2 className="contract-base-info__base-contracts-title text-2xl mr-2 font-semibold text-gray-800">
+              Overview
+            </h2>
+
+            <aside className="contract-base-info__description border-solid border border-slate-200 bg-slate-50 rounded-md font-bold px-4 py-2 mt-4 lg:mt-0">
+              <dl className="contract-base-info__list flex flex-col lg:flex-row lg:items-center lg:space-x-6">
+                <div
+                  className={`contract-base-info__item flex items-center ${contract.contractKind === 'contract' ? 'mb-1 lg:mb-0' : 'mb-0'}`}
+                >
+                  <img
+                    className="w-5 h-5 mr-2"
+                    src="/icons/blocksense-kind.svg"
+                    alt="Kind"
+                    loading="lazy"
+                  />
+                  <dt className="contract-base-info__label text-gray-500 font-normal">
+                    Kind:
+                  </dt>
+                  <dd className="contract-base-info__value text-gray-900 text-base ml-1">
+                    {contract.contractKind}
+                  </dd>
+                </div>
+
+                {contract.contractKind === 'contract' && (
+                  <div className="contract-base-info__separator hidden lg:block w-[1px] bg-gray-900 h-6 mx-4"></div>
+                )}
+
+                {contract.contractKind === 'contract' && (
+                  <div className="contract-base-info__item flex items-center">
+                    <img
+                      className="w-5 h-5 mr-2"
+                      src="/icons/blocksense-abstract.svg"
+                      alt="Abstract"
+                      loading="lazy"
+                    />
+                    <dt className="contract-base-info__label text-gray-500 font-normal">
+                      Abstract:
+                    </dt>
+                    <dd className="contract-base-info__value text-gray-900 text-base ml-1">
+                      {contract.abstract.toString()}
+                    </dd>
+                  </div>
+                )}
+              </dl>
+            </aside>
+          </header>
+
           {contract._baseContracts.length > 0 && (
-            <CardContent className="contract-base-info__content">
+            <section className="contract-base-info__content px-0 mb-4">
               <div className="contract-base-info__base-contracts">
-                <span className="contract-base-info__base-contracts-title text-xl font-semibold text-gray-800 ml-4">
+                <h3 className="contract-base-info__base-contracts-title text-xl font-semibold text-gray-800 ml-2">
                   Base Contracts
-                </span>
-                <ul className="contract-base-info__base-contracts-list ml-6 mt-2 list-disc list-inside text-gray-700">
+                </h3>
+                <ul className="contract-base-info__base-contracts-list ml-6 mt-2 mb-2 list-image-none list-outside text-gray-700">
                   {contract._baseContracts.map(baseContract => (
                     <li
                       className="contract-base-info__base-contracts-item"
@@ -47,16 +77,15 @@ export const ContractBaseInfo = ({ ...contract }: ContractDocItem) => {
                   ))}
                 </ul>
               </div>
-            </CardContent>
+            </section>
           )}
-          <span className="contract-base-info__base-contracts-title text-xl font-semibold text-gray-800 ml-4">
-            Overview
-          </span>
-          <ContractOverview contract={contract} />
-          <CardFooter>
+          <aside className="contract-base-info__base-contracts-description mx-2">
+            <ContractOverview contract={contract} />
+          </aside>
+          <footer className="contract-base-info__header mt-6 mx-2 mb-6">
             <NatSpec natspec={contract.natspec} />
-          </CardFooter>
-        </Card>
+          </footer>
+        </section>
       </div>
     </>
   );
--- apps/docs.blocksense.network/components/sol-contracts/NatSpec.tsx
+++ apps/docs.blocksense.network/components/sol-contracts/NatSpec.tsx
@@ -10,7 +10,7 @@ export const NatSpec = ({ natspec }: NatSpecProps) => {
   return (
     <>
       {Object.keys(natspec).length > 0 && (
-        <div className="natspec px-4 py-4 mb-4 border-solid border border-slate-200 bg-slate-50 rounded-lg text-black">
+        <div className="natspec px-4 py-4 border-solid border border-slate-200 bg-slate-50 rounded-lg text-black">
           <p className="natspec__title text-lg font-semibold text-slate-600">
             Description
           </p>
@@ -26,9 +26,7 @@ export const NatSpec = ({ natspec }: NatSpecProps) => {
             </p>
           )}
           {natspec.dev && (
-            <div className="natspec__dev  text-slate-600 mb-6">
-              {natspec.dev}
-            </div>
+            <div className="natspec__dev  text-slate-600">{natspec.dev}</div>
           )}
           {Object.entries(natspec.custom || {}).map(([key, value], index) => (
             <div className="natspec__custom mb-5" key={index}>
