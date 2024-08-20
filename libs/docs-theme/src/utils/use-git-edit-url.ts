e5f29ae323b09228386cb094efa370c7e7fbd1dd
Radoslav Dimchev <radoslav.dimchev@blocksense.network>
2024-08-20 10:46:06 +0300

    fix(docs.ws): `Edit this page` leads to valid urls

--- libs/docs-theme/src/utils/use-git-edit-url.ts
+++ libs/docs-theme/src/utils/use-git-edit-url.ts
@@ -1,5 +1,9 @@
+import * as path from 'path';
+
 import gitUrlParse from 'git-url-parse';
 import { useConfig } from '../contexts';
+
+import SOL_REFLECTION_JSON from '@blocksense/contracts/docs/fine';
 
 export function useGitEditUrl(filePath = ''): string {
   const config = useConfig();
@@ -7,5 +11,18 @@ export function useGitEditUrl(filePath = ''): string {
 
   if (!repo) throw new Error('Invalid `docsRepositoryBase` URL!');
 
-  return `${repo.href}/${filePath}`;
+  if (filePath.includes('reference-documentation')) {
+    const contractFileName = path.basename(filePath).replace('.mdx', '.sol');
+    let gitAbsolutePath = 'edit/main/libs/contracts/';
+    SOL_REFLECTION_JSON.some((sourceUnit: any) => {
+      if (sourceUnit.absolutePath.includes(contractFileName)) {
+        gitAbsolutePath += sourceUnit.absolutePath;
+        return true;
+      }
+    });
+    return `${repo.href}/${gitAbsolutePath}`;
+  } else {
+    const gitEditUrlPart = 'edit/main/apps/docs.blocksense.network';
+    return `${repo.href}/${gitEditUrlPart}/${filePath}`;
+  }
 }
