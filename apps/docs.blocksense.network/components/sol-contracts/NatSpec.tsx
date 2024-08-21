587d49a4d70373f01fee01c6ad3c8820be04ccc9
Radoslav Dimchev <radoslav.dimchev@blocksense.network>
2024-08-21 16:54:57 +0300

    feat(docs.ws): Format NatSpec new rows

--- apps/docs.blocksense.network/components/sol-contracts/NatSpec.tsx
+++ apps/docs.blocksense.network/components/sol-contracts/NatSpec.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+
 import { NatSpec as NatSpecType } from '@blocksense/sol-reflector';
+
+import { NatSpecItem } from '@/sol-contracts-components/NatSpecItem';
 
 type NatSpecProps = {
   natspec: NatSpecType;
@@ -10,48 +13,11 @@ export const NatSpec = ({ natspec }: NatSpecProps) => {
     <>
       {Object.keys(natspec).length > 0 && (
         <ol className="relative border-l-2 border-gray-200 dark:border-gray-700">
-          {natspec.author && (
-            <li className="mb-4">
-              <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-                Author
-              </h3>
-              <p className="block mb-2 text-sm font-normal text-gray-500 dark:text-gray-500">
-                {natspec.author}
-              </p>
-            </li>
-          )}
-          {natspec.notice && (
-            <li className="mb-4">
-              <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-                Notice
-              </h3>
-              <p className="block mb-2 text-sm font-normal text-gray-500 dark:text-gray-500 italic">
-                {natspec.notice}
-              </p>
-            </li>
-          )}
-          {natspec.dev && (
-            <li className="mb-4">
-              <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-                Developer notes
-              </h3>
-              <p className="block mb-2 text-sm font-normal text-gray-500 dark:text-gray-500 italic">
-                {natspec.dev}
-              </p>
-            </li>
-          )}
+          <NatSpecItem title="Author" content={natspec.author} />
+          <NatSpecItem title="Notice" content={natspec.notice} />
+          <NatSpecItem title="Developer notes" content={natspec.dev} />
           {Object.entries(natspec.custom || {}).map(([key, value], index) => (
-            <li
-              className={`ml-4 ${index < Object.keys(natspec.custom || {}).length - 1 ? 'mb-4' : ''}`}
-              key={index}
-            >
-              <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-                {key}
-              </h3>
-              <p className="block mb-2 text-sm font-normal text-gray-500 dark:text-gray-500 italic">
-                {value}
-              </p>
-            </li>
+            <NatSpecItem key={index} title={key} content={value} />
           ))}
         </ol>
       )}
