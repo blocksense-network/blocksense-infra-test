1f886bca6e3ba3343b45f12e24ba774a1527e3fd
Kalin Hristov <kalinsky.hristov@gmail.com>
2024-08-28 12:42:49 +0300

    feat(docs.ws): Add dynamically `Overview` page
    Add dynamically Overview page with links to all Blocksense supported Contracts

--- apps/docs.blocksense.network/src/generate-contracts-mdx.ts
+++ apps/docs.blocksense.network/src/generate-contracts-mdx.ts
@@ -34,22 +34,51 @@ import { SourceUnit } from '@/sol-contracts-components/SourceUnit';
   return content;
 }
 
+function generateLinkContent(name: string): string {
+  const content = `<ContractAnchorLink label='${name}'></ContractAnchorLink>\n`;
+
+  return content;
+}
+
+function generateOverviewContent(contractsMetaJSON: {
+  [key: string]: string;
+}): string {
+  let overviewContent = `import { ContractAnchorLink } from '@blocksense/docs.blocksense.network/components/sol-contracts/ContractAnchorLink';\n
+  # Introduction\n
+  Explore the range of smart contracts available within the Blocksense protocol. This overview covers contracts for interacting with data feeds, historical data feed storage, proxy contracts, and interfaces for integrating with data aggregation services.\n
+  <br/>`;
+  overviewContent += `<ul className="nx-mt-6 nx-list-none first:nx-mt-0 ltr:nx-ml-0 rtl:nx-mr-6">`;
+  Object.keys(contractsMetaJSON).map(name => {
+    overviewContent += generateLinkContent(name);
+  });
+  overviewContent += `</ul>`;
+  return overviewContent;
+}
+
 function generateSolRefDocFiles(): Promise<string[]> {
   const mdxFiles = solReflection.map(sourceUnit => ({
     name: path.parse(sourceUnit.absolutePath).name,
     content: generateMarkdownContent(sourceUnit),
   }));
 
-  const metaJSON = mdxFiles.reduce(
+  let metaJSON = mdxFiles.reduce(
     (obj, { name }) => ({ [name]: name, ...obj }),
     {},
   );
+
+  const overviewContent = generateOverviewContent(metaJSON);
+
+  mdxFiles.push({ name: 'index', content: overviewContent });
+  metaJSON = { index: 'Overview', ...metaJSON };
 
   const { write, writeJSON } = selectDirectory(pagesContractRefDocFolder);
 
   return Promise.all([
     ...mdxFiles.map(args => write({ ext: '.mdx', ...args })),
-    writeJSON({ base: '_meta.json', content: metaJSON }),
+    writeJSON({
+      base: '_meta.json',
+      content: metaJSON,
+    }),
   ]);
 }
 
