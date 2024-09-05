f06122528a6d98ac860f01a46fd4562e8c37135e
Kalin Hristov <kalinsky.hristov@gmail.com>
2024-09-05 12:34:57 +0300

    feat(docs.ws): `ContractsFileTree` component has `anchor-links`
    `ContractsFileTree` component has `anchor-links` to `GitHub`

--- apps/docs.blocksense.network/components/sol-contracts/ContractsFileTree.tsx
+++ apps/docs.blocksense.network/components/sol-contracts/ContractsFileTree.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { FileTree } from '@blocksense/docs-theme';
+import { ghContractFolder } from '@/src/constants';
 
 type TreeNode = {
   name: string;
   children?: TreeNode[];
   id: number;
+  path: string;
 };
 
-export const renderTree = ({ name, children, id }: TreeNode) => {
+export const renderTree = ({ name, children, id, path }: TreeNode) => {
+  const constructedHref = `${ghContractFolder}contracts/${path}`;
   if (children) {
     return (
       <FileTree.Folder name={name} key={id} defaultOpen>
@@ -15,7 +18,7 @@ export const renderTree = ({ name, children, id }: TreeNode) => {
       </FileTree.Folder>
     );
   } else {
-    return <FileTree.File name={name} key={id} />;
+    return <FileTree.File name={name} key={id} href={constructedHref} />;
   }
 };
 
