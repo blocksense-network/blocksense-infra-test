196b3b69e855869c94960ab4d212205732b95ad6
Petar Kirov <petar.p.kirov@gmail.com>
2024-09-11 14:17:40 +0300

    improve(data-feeds-config-generator): Declare `configDir` path

--- apps/data-feeds-config-generator/src/paths.ts
+++ apps/data-feeds-config-generator/src/paths.ts
@@ -1,5 +1,6 @@
 import { rootDir } from '@blocksense/base-utils/env';
 
+export const configDir = `${rootDir}/config`;
 export const workspaceDir = `${rootDir}/apps/data-feeds-config-generator`;
 export const artifactsDir = `${workspaceDir}/artifacts`;
 export const chainlinkFeedsDir = `${artifactsDir}/chainlink_feeds`;
