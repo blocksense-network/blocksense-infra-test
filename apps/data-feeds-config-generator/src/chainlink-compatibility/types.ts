af14cc2840540e3aad82035fd8e5ed6758d9d7a8
Petar Kirov <petar.p.kirov@gmail.com>
2024-09-20 15:53:09 +0300

    feat(data-feeds-config-generator): Add `getAllProposedFeedsInRegistry` function

--- apps/data-feeds-config-generator/src/chainlink-compatibility/types.ts
+++ apps/data-feeds-config-generator/src/chainlink-compatibility/types.ts
@@ -1,7 +1,10 @@
 import * as S from '@effect/schema/Schema';
+import * as ParseResult from '@effect/schema/ParseResult';
+import * as bigInt_ from 'effect/BigInt';
 
 import {
   ChainId,
+  ethereumAddress,
   networkNameToChainId,
 } from '@blocksense/base-utils/evm-utils';
 
@@ -126,3 +129,43 @@ export const chainlinkNetworkNameToChainId = {
 } satisfies {
   [Net in NetworkName]: ChainId | null;
 };
+
+export class NumberFromBigInt extends S.transformOrFail(
+  S.Union(S.BigIntFromSelf, S.Number),
+  S.Number,
+  {
+    strict: true,
+    encode: (n, _, ast) =>
+      ParseResult.fromOption(
+        bigInt_.fromNumber(n),
+        () => new ParseResult.Type(ast, n),
+      ),
+    decode: (b, _, ast) =>
+      ParseResult.fromOption(
+        bigInt_.toNumber(BigInt(b)),
+        () => new ParseResult.Type(ast, b),
+      ),
+  },
+).annotations({ identifier: 'NumberFromBigInt' }) {}
+
+export const ConfirmedFeedEvent = S.Struct({
+  asset: ethereumAddress,
+  denomination: ethereumAddress,
+  latestAggregator: ethereumAddress,
+  previousAggregator: ethereumAddress,
+  nextPhaseId: NumberFromBigInt, // uint16 in Solidity
+  sender: ethereumAddress,
+});
+
+export type ConfirmedFeedEvent = S.Schema.Type<typeof ConfirmedFeedEvent>;
+
+export const decodeConfirmedFeedEvent = S.decodeUnknownSync(ConfirmedFeedEvent);
+
+export const FeedRegistryEventsPerAggregatorSchema = S.Record({
+  key: ethereumAddress,
+  value: ConfirmedFeedEvent,
+});
+
+export type FeedRegistryEventsPerAggregator = S.Schema.Type<
+  typeof FeedRegistryEventsPerAggregatorSchema
+>;
--- apps/data-feeds-config-generator/src/data-services/chainlink_feeds.ts
+++ apps/data-feeds-config-generator/src/data-services/chainlink_feeds.ts
@@ -1,7 +1,21 @@
-import { selectDirectory } from '@blocksense/base-utils/fs';
+import { Web3 } from 'web3';
 
+import { assertIsObject } from '@blocksense/base-utils/assert';
+import { selectDirectory } from '@blocksense/base-utils/fs';
+import { getRpcUrl } from '@blocksense/base-utils/evm-utils';
+
+import {
+  NetworkName,
+  EthereumAddress,
+  parseEthereumAddress,
+  isZeroAddress,
+} from '@blocksense/base-utils/evm-utils';
+
+import {
+  decodeConfirmedFeedEvent,
+  FeedRegistryEventsPerAggregator,
+} from '../chainlink-compatibility/types';
 import { RawDataFeeds, decodeChainLinkFeedsInfo } from './types';
-import { artifactsDir } from '../paths';
 
 export async function collectRawDataFeeds(directoryPath: string) {
   const { readAllJSONFiles } = selectDirectory(directoryPath);
@@ -22,4 +36,125 @@ export async function collectRawDataFeeds(directoryPath: string) {
   }
 
   return rawDataFeeds;
+}
+
+export const feedRegistries: {
+  [key in NetworkName]?: EthereumAddress;
+} = {
+  'ethereum-mainnet': parseEthereumAddress(
+    '0x47Fb2585D2C56Fe188D0E6ec628a38b74fCeeeDf',
+  ),
+};
+
+export async function getAllProposedFeedsInRegistry(
+  network: NetworkName,
+  web3: Web3 = new Web3(getRpcUrl(network)),
+): Promise<FeedRegistryEventsPerAggregator> {
+  const registryAddress = feedRegistries[network];
+  if (!registryAddress) {
+    throw new Error(`No feed registry found for network ${network}`);
+  }
+  return await getAllProposedFeedsRegistryEvents(web3, registryAddress);
+}
+
+export async function getAllProposedFeedsRegistryEvents(
+  web3: Web3,
+  feedRegistryAddress: EthereumAddress,
+): Promise<FeedRegistryEventsPerAggregator> {
+  const abi = [
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'asset',
+          type: 'address',
+        },
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'denomination',
+          type: 'address',
+        },
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'latestAggregator',
+          type: 'address',
+        },
+        {
+          indexed: false,
+          internalType: 'address',
+          name: 'previousAggregator',
+          type: 'address',
+        },
+        {
+          indexed: false,
+          internalType: 'uint16',
+          name: 'nextPhaseId',
+          type: 'uint16',
+        },
+        {
+          indexed: false,
+          internalType: 'address',
+          name: 'sender',
+          type: 'address',
+        },
+      ],
+      name: 'FeedConfirmed',
+      type: 'event',
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'asset',
+          type: 'address',
+        },
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'denomination',
+          type: 'address',
+        },
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'proposedAggregator',
+          type: 'address',
+        },
+        {
+          indexed: false,
+          internalType: 'address',
+          name: 'currentAggregator',
+          type: 'address',
+        },
+        {
+          indexed: false,
+          internalType: 'address',
+          name: 'sender',
+          type: 'address',
+        },
+      ],
+      name: 'FeedProposed',
+      type: 'event',
+    },
+  ] as const;
+
+  const registryContract = new web3.eth.Contract(abi, feedRegistryAddress);
+
+  const proposedFeeds = (
+    await registryContract.getPastEvents('FeedConfirmed', {
+      fromBlock: 0,
+      toBlock: 'latest',
+    })
+  )
+    .map(f => decodeConfirmedFeedEvent(assertIsObject(f).returnValues))
+    .filter(ev => !isZeroAddress(ev.latestAggregator))
+    .map(ev => [ev.latestAggregator, ev]);
+
+  return Object.fromEntries(proposedFeeds);
 }
