778839124c53a5ab80e83c54d08a98c6d5977455
Vanya Pavlova <vanya.pavlova@blocksense.network>
2024-09-24 11:53:47 +0300

    feat(docs.ws): Blocksense fonts used accross the project should be preloaded

--- apps/docs.blocksense.network/theme.config.tsx
+++ apps/docs.blocksense.network/theme.config.tsx
@@ -1,6 +1,24 @@
 import { Header } from './components/common/Header';
 import { Footer } from './components/common/Footer';
 
+const fonts = [
+  '/fonts/fira/FiraCode-Regular.ttf',
+  '/fonts/noto-sans/NotoSans-Black.ttf',
+  '/fonts/noto-sans/NotoSans-Bold.ttf',
+  '/fonts/noto-sans/NotoSans-BoldItalic.ttf',
+  '/fonts/noto-sans/NotoSans-ExtraBold.ttf',
+  '/fonts/noto-sans/NotoSans-Italic.ttf',
+  '/fonts/noto-sans/NotoSans-Light.ttf',
+  '/fonts/noto-sans/NotoSans-LightItalic.ttf',
+  '/fonts/noto-sans/NotoSans-Medium.ttf',
+  '/fonts/noto-sans/NotoSans-Regular.ttf',
+  '/fonts/noto-sans/NotoSans-SemiBold.ttf',
+  '/fonts/noto-sans/NotoSans-SemiBoldItalic.ttf',
+  '/fonts/noto-sans/NotoSans-Thin.ttf',
+  '/fonts/noto-sans/NotoSans-ThinItalic.ttf',
+  '/fonts/SpaceMono-Bold.ttf',
+];
+
 export default {
   logo: Header,
   project: {
@@ -16,10 +34,19 @@ export default {
     };
   },
   head: (
-    <link
-      rel="icon"
-      href="/images/blocksense-favicon.png"
-      type="image/png"
-    ></link>
+    <>
+      <link rel="icon" href="/images/blocksense-favicon.png" type="image/png" />
+
+      {fonts.map(font => (
+        <link
+          key={font}
+          rel="preload"
+          href={font}
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
+      ))}
+    </>
   ),
 };
