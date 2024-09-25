dec337d36ed46d96a7b48c767c8fd1bc15102c42
Vanya Pavlova <vanya.pavlova@blocksense.network>
2024-09-25 20:32:07 +0300

    feat(docs.ws): Added missing meta description for docs.ws

--- apps/docs.blocksense.network/theme.config.tsx
+++ apps/docs.blocksense.network/theme.config.tsx
@@ -35,8 +35,11 @@ export default {
   },
   head: (
     <>
+      <meta
+        name="description"
+        content="Blocksense is the ZK rollup for scaling oracle data to infinity. Everyone will be able to create secure oracles in minutes."
+      />
       <link rel="icon" href="/images/blocksense-favicon.png" type="image/png" />
-
       {fonts.map(font => (
         <link
           key={font}
