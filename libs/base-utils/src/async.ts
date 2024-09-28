6c4563ca4dcb8a6d9273d3e400113922dc1f7f14
Dimo99 <dimodimov1999@gmail.com>
2024-09-28 04:37:44 +0300

    feat(base-utils/async): Add `everyAsync` function

--- libs/base-utils/src/async.ts
+++ libs/base-utils/src/async.ts
@@ -21,3 +21,21 @@ export async function filterAsync<T>(
   const boolArray = await Promise.all(array.map(asyncPredicate));
   return array.filter((_, i) => boolArray[i]);
 }
+
+/**
+ * Checks whether all elements in an array satisfy an asynchronous predicate.
+ *
+ * @template T The type of the elements in the input array.
+ *
+ * @param {T[]} array - The array to check.
+ * @param {(entry: T) => Promise<boolean>} asyncPredicate - The asynchronous predicate function to apply to each element of the array.
+ *
+ * @returns {Promise<boolean>} A promise that resolves to `true` if all elements satisfy the async predicate, otherwise `false`.
+ */
+export async function everyAsync<T>(
+  array: T[],
+  asyncPredicate: (entry: T) => Promise<boolean>,
+): Promise<boolean> {
+  const boolArray = await Promise.all(array.map(asyncPredicate));
+  return boolArray.every(b => b);
+}
