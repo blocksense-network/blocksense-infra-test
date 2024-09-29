5b37e7a53f0a4d6ba593b3d5a3ba95d0daeacdd8
Petar Kirov <petar.p.kirov@gmail.com>
2024-09-29 17:08:49 +0300

    feat(base-utils/array-iter): Add `entries`, `fromEntries`, and `tuple` functions

---
+++ libs/base-utils/src/array-iter.ts
@@ -0,0 +1,13 @@
+export function entries<K extends string, V>(obj: Record<K, V>): [K, V][] {
+  return Object.entries(obj) as [K, V][];
+}
+
+export function fromEntries<K extends string, V>(
+  entries: [K, V][],
+): Record<K, V> {
+  return Object.fromEntries(entries) as Record<K, V>;
+}
+
+export function tuple<Args extends any[]>(...args: Args): Args {
+  return args;
+}
