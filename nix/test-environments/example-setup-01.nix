e9612a8fd1adb64596da15984a8fa1cbde7fba85
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2024-10-08 21:33:32 +0300

    config(nix/test-environments): Read required data from config files

--- nix/test-environments/example-setup-01.nix
+++ nix/test-environments/example-setup-01.nix
@@ -1,6 +1,13 @@
 { config, ... }:
 let
+  # Function to read and parse the JSON file
+  readJson = path: builtins.fromJSON (builtins.readFile path);
+
   testKeysDir = config.devenv.root + "/nix/test-environments/test-keys";
+  deploymentFilePath = config.devenv.root + "/config/evm_contracts_deployment_v1.json";
+
+  upgradeableProxyContractAddress =
+    (readJson deploymentFilePath)."local".contracts.coreContracts.UpgradeableProxy.address;
 in
 {
   services.blocksense = {
@@ -53,11 +60,11 @@ in
           # In a production environment, use a secret manager like Agenix, to
           # prevent secrets from being copyed to the Nix Store.
           private_key_path = "${testKeysDir}/sequencer-private-key";
-          contract_address = "0xeE5a4826068C5326a7f06fD6c7cBf816F096846c";
+          contract_address = upgradeableProxyContractAddress;
         };
         b = {
           private_key_path = "${testKeysDir}/sequencer-private-key";
-          contract_address = "0xeE5a4826068C5326a7f06fD6c7cBf816F096846c";
+          contract_address = upgradeableProxyContractAddress;
         };
       };
 
@@ -102,10 +109,9 @@ in
         full-batch = true;
         batch-size = 5;
         resources = {
-          SECRET_KEY_PATH = ./test-keys/reporter_secret_key;
-          CMC_API_KEY_PATH = config.devenv.root + "/nix/test-environments/test-keys/CMC_API_KEY";
-          YH_FINANCE_API_KEY_PATH =
-            config.devenv.root + "/nix/test-environments/test-keys/YH_FINANCE_API_KEY";
+          SECRET_KEY_PATH = "${testKeysDir}/reporter_secret_key";
+          CMC_API_KEY_PATH = "${testKeysDir}/CMC_API_KEY";
+          YH_FINANCE_API_KEY_PATH = "${testKeysDir}/YH_FINANCE_API_KEY";
         };
         reporter = {
           pub_key = "ea30af86b930d539c55677b05b4a5dad9fce1f758ba09d152d19a7d6940f8d8a8a8fb9f90d38a19e988d721cddaee4567d2e";
