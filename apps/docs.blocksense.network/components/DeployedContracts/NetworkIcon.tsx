9612d9f6eff35f526dee839d68404d55b0479683
Mila Genova <mila.genova@blocksense.network>
2024-11-08 16:12:42 +0200

    feat(docs.ws): Make `NetworkIcon` a button

--- apps/docs.blocksense.network/components/DeployedContracts/NetworkIcon.tsx
+++ apps/docs.blocksense.network/components/DeployedContracts/NetworkIcon.tsx
@@ -1,17 +1,21 @@
 import * as React from 'react';
 import { ImageWrapper } from '../common/ImageWrapper';
 import { networkMetadata, parseNetworkName } from '@blocksense/base-utils/evm';
+import { Button } from 'nextra/components';
 
 type NetworkIconProps = {
   network: string;
+  onClick: () => void;
 };
 
-export const NetworkIcon = ({ network }: NetworkIconProps) => {
-  console.log(network);
+export const NetworkIcon = ({ network, onClick }: NetworkIconProps) => {
   const chainId = networkMetadata[parseNetworkName(network)].chainId;
   const iconPath = `/images/network-icons/${network.split('-')[0]}.png`;
   return (
-    <div className="p-2 bg-gray-100 text-black w-36 h-36 aspect-square flex flex-col items-center justify-center rounded-md">
+    <Button
+      className="p-2 bg-gray-100 text-black w-36 h-36 aspect-square flex flex-col items-center justify-center rounded-md"
+      onClick={onClick}
+    >
       {' '}
       <ImageWrapper
         src={iconPath}
@@ -20,6 +24,6 @@ export const NetworkIcon = ({ network }: NetworkIconProps) => {
       />
       <div className="pt-2 font-bold text-xs">{network}</div>
       <div className="pt-2 font-semibold text-xs">ChainId:{chainId}</div>
-    </div>
+    </Button>
   );
 };
