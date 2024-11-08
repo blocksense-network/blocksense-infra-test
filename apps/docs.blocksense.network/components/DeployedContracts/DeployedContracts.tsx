6a8399a295a374fa79f07fc16ee733c5206e9be4
Mila Genova <mila.genova@blocksense.network>
2024-11-08 16:15:29 +0200

    feat(docs.ws): Show Deployed Contracts info only for selected network

--- apps/docs.blocksense.network/components/DeployedContracts/DeployedContracts.tsx
+++ apps/docs.blocksense.network/components/DeployedContracts/DeployedContracts.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 
+import { parseNetworkName } from '@blocksense/base-utils/evm';
+
 import {
   DataTable,
   getFacetedFilters,
@@ -33,7 +35,9 @@ export const DeployedContracts = ({
     JSON.parse(deployedProxyContractsString),
   );
 
-  console.log(deployedCoreContracts);
+  const [selectedNetwork, setSelectedNetwork] = React.useState<string | null>(
+    null,
+  );
 
   const filters = React.useMemo(
     () =>
@@ -54,72 +58,93 @@ export const DeployedContracts = ({
         titleLevel={2}
         itemsLength={1}
       >
-        <div className="flex flex-wrap justify-center gap-4 pt-3">
-          {deployedCoreContracts[0].networks.map(network => (
-            <NetworkIcon network={network} />
-          ))}
-        </div>
-      </ContractItemWrapper>
-      <ContractItemWrapper
-        title="Core Contracts"
-        titleLevel={2}
-        itemsLength={deployedCoreContracts.length}
-      >
         <article className="mt-4 mb-6">
           <span className="text-gray-500 text-md">
-            Explore the deployed core contracts, including their addresses and
-            networks where they are available. These contracts are key
-            components of the Blocksense platform and provide essential
-            functionalities that support the ecosystem.
+            We have deployed our contracts on the following networks. Select a
+            network to view detailed information about the deployed contracts.
             <br />
-            Discover more into our smart contracts
-            <a
-              href={smartContractsUrl}
-              className="nx-text-primary-600 nx-underline nx-decoration-from-font [text-underline-position:from-font] mx-1"
-            >
-              architecture
-            </a>
-            documentation section.
           </span>
         </article>
-        <div className="container px-0">
-          {deployedCoreContracts.map(contract => (
-            <CoreContractCard
-              key={contract.address}
-              contract={{
-                name: contract.contract,
-                address: contract.address,
-                networks: contract.networks,
-              }}
+
+        <div className="flex flex-wrap justify-center gap-4 pt-3">
+          {deployedCoreContracts[0].networks.map(network => (
+            <NetworkIcon
+              key={network}
+              network={network}
+              onClick={() => setSelectedNetwork(network)}
             />
           ))}
         </div>
       </ContractItemWrapper>
-
-      <div className="mt-6">
+      {selectedNetwork && (
         <ContractItemWrapper
-          title="Aggregator Proxy Contracts"
+          title="Core Contracts"
           titleLevel={2}
-          itemsLength={deployedProxyContracts.length}
+          itemsLength={deployedCoreContracts.length}
         >
           <article className="mt-4 mb-6">
             <span className="text-gray-500 text-md">
-              Blocksense aggregator proxy contracts table allows users to
-              explore contracts that serve as an alternative to the Chainlink
-              proxy contracts. Additionally, the table provides information
-              about data feed names, IDs, and relevant addresses.
+              Explore the deployed core contracts, including their addresses and
+              networks where they are available. These contracts are key
+              components of the Blocksense platform and provide essential
+              functionalities that support the ecosystem.
+              <br />
+              Discover more into our smart contracts
+              <a
+                href={smartContractsUrl}
+                className="nx-text-primary-600 nx-underline nx-decoration-from-font [text-underline-position:from-font] mx-1"
+              >
+                architecture
+              </a>
+              documentation section.
             </span>
           </article>
-          <DataTable
-            columns={proxyContractsColumns}
-            data={deployedProxyContracts}
-            columnsTitles={proxyColumnsTitles}
-            filters={filters}
-            filterCell="description"
-            rowLink={dataFeedUrl}
-          />
+          <div className="container px-0">
+            {deployedCoreContracts.map(contract => (
+              <CoreContractCard
+                key={contract.address}
+                contract={{
+                  name: contract.contract,
+                  address: contract.address,
+                  networks: selectedNetwork
+                    ? contract.networks.filter(
+                        network => network == parseNetworkName(selectedNetwork),
+                      )
+                    : contract.networks,
+                }}
+              />
+            ))}
+          </div>
         </ContractItemWrapper>
-      </div>
+      )}
+      {selectedNetwork && (
+        <div className="mt-6">
+          <ContractItemWrapper
+            title="Aggregator Proxy Contracts"
+            titleLevel={2}
+            itemsLength={deployedProxyContracts.length}
+          >
+            <article className="mt-4 mb-6">
+              <span className="text-gray-500 text-md">
+                Blocksense aggregator proxy contracts table allows users to
+                explore contracts that serve as an alternative to the Chainlink
+                proxy contracts. Additionally, the table provides information
+                about data feed names, IDs, and relevant addresses.
+              </span>
+            </article>
+            <DataTable
+              columns={proxyContractsColumns}
+              data={deployedProxyContracts.filter(
+                element => element.network === selectedNetwork,
+              )}
+              columnsTitles={proxyColumnsTitles}
+              filters={filters}
+              filterCell="description"
+              rowLink={dataFeedUrl}
+            />
+          </ContractItemWrapper>
+        </div>
+      )}
     </section>
   );
 };
