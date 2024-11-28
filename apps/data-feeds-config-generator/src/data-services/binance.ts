24b26c3c6ebdda95f9aa6449951a562104042ce2
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2024-11-29 00:05:21 +0200

    feat(data-feeds): Implement `binance` supported symbols fetcher

---
+++ apps/data-feeds-config-generator/src/data-services/binance.ts
@@ -0,0 +1,62 @@
+import * as S from '@effect/schema/Schema';
+
+import { selectDirectory } from '@blocksense/base-utils/fs';
+
+import { artifactsDir } from '../paths';
+
+/**
+ * Schema for the information about symbols received from Binance.
+ */
+const BinanceSymbolInfoSchema = S.Struct({
+  symbol: S.String,
+  baseAsset: S.String,
+  quoteAsset: S.String,
+  baseAssetPrecision: S.Number,
+  quoteAssetPrecision: S.Number,
+});
+
+/**
+ * Type for the information about symbols received from Binance.
+ */
+export type BinanceSymbolInfo = S.Schema.Type<typeof BinanceSymbolInfoSchema>;
+
+/**
+ * Function to decode Binance symbol information.
+ */
+export const decodeBinanceSymbolInfo = S.decodeUnknownSync(
+  S.Array(BinanceSymbolInfoSchema),
+);
+
+/**
+ * Function to fetch detailed information about symbols from Binance.
+ */
+export async function fetchBinanceSymbolsInfo(): Promise<
+  readonly BinanceSymbolInfo[]
+> {
+  const exchangeInfoUrl = 'https://api.binance.com/api/v3/exchangeInfo';
+
+  const response = await fetch(exchangeInfoUrl, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+
+  const data = (await response.json()) as { symbols: unknown[] };
+
+  const supportedBinanceSymbols = decodeBinanceSymbolInfo(data.symbols);
+
+  {
+    const { writeJSON } = selectDirectory(artifactsDir);
+    await writeJSON({
+      content: { supportedBinanceSymbols },
+      name: 'binance_symbols',
+    });
+  }
+
+  return supportedBinanceSymbols;
+}
