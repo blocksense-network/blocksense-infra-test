dc3bbc0d560937684789ee3076bb30b4eb36e793
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2024-11-29 02:00:54 +0200

    feat(data-feeds): Implement `bybit` supported symbols fetcher

---
+++ apps/data-feeds-config-generator/src/data-services/bybit.ts
@@ -0,0 +1,73 @@
+import * as S from '@effect/schema/Schema';
+
+import { selectDirectory } from '@blocksense/base-utils/fs';
+
+import { artifactsDir } from '../paths';
+
+/**
+ * Schema for the information about symbols received from Bybit.
+ */
+const BybitSymbolInfoSchema = S.Struct({
+  symbol: S.String,
+  baseCoin: S.String,
+  quoteCoin: S.String,
+  status: S.String,
+  lotSizeFilter: S.Struct({
+    basePrecision: S.String,
+    quotePrecision: S.String,
+  }),
+});
+
+/**
+ * Type for the information about symbols received from Bybit.
+ */
+export type BybitSymbolInfo = S.Schema.Type<typeof BybitSymbolInfoSchema>;
+
+/**
+ * Function to decode Bybit symbol information.
+ */
+export const decodeBybitSymbolInfo = S.decodeUnknownSync(
+  S.Array(BybitSymbolInfoSchema),
+);
+
+/**
+ * Function to fetch detailed information about symbols from Bybit.
+ */
+export async function fetchBybitSymbolsInfo(): Promise<
+  readonly BybitSymbolInfo[]
+> {
+  const instrumentsInfoUrl = 'https://api.bybit.com/v5/market/instruments-info';
+  const params = new URLSearchParams({ category: 'spot' });
+  const headers = { Accept: 'application/json' };
+
+  const response = await fetch(`${instrumentsInfoUrl}?${params.toString()}`, {
+    method: 'GET',
+    headers,
+  });
+
+  if (!response.ok) {
+    throw new Error(`Error: ${response.status} ${response.statusText}`);
+  }
+
+  const data = (await response.json()) as {
+    retCode: number;
+    retMsg: string;
+    result?: { list: unknown[] };
+  };
+
+  if (data.retCode !== 0) {
+    throw new Error(`Error: ${data.retCode} ${data.retMsg}`);
+  }
+
+  const supportedBybitSymbols = decodeBybitSymbolInfo(data.result?.list);
+
+  {
+    const { writeJSON } = selectDirectory(artifactsDir);
+    await writeJSON({
+      content: { supportedBybitSymbols },
+      name: 'bybit_symbols',
+    });
+  }
+
+  return supportedBybitSymbols;
+}
