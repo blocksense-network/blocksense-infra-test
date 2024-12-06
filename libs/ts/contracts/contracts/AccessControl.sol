56e0eb4be384eb47a9bc3e97005012e32f22d2a2
Aneta Tsvetkova <anetastsvetkova@gmail.com>
2024-12-06 20:34:11 +0200

    feat(contracts): Reorganise access control to save a little bit gas

--- libs/ts/contracts/contracts/AccessControl.sol
+++ libs/ts/contracts/contracts/AccessControl.sol
@@ -2,47 +2,47 @@
 pragma solidity ^0.8.24;
 
 contract AccessControl {
-  address internal immutable owner;
+  address internal immutable OWNER;
   bytes32 internal constant ADMIN_LOCATION =
     0x0000000000000000000000000000000000000000000000000000abc123abc123;
 
   constructor(address owner_) {
-    owner = owner_;
+    OWNER = owner_;
   }
 
   fallback() external {
-    address _owner = owner;
+    address _owner = OWNER;
 
     // no selector is passed
     // whether read or write operation will be executed is determined by the caller
     assembly {
       let _caller := caller()
 
-      // function checkAdminRole(address _caller) external view returns (bool) {
-      if iszero(eq(_caller, _owner)) {
-        mstore(0, or(ADMIN_LOCATION, calldataload(0)))
-        mstore(0, sload(keccak256(0, 0x20)))
-        return(0, 0x20)
-      }
-
       // function setAdmins(address[] calldata addresses) external {
-      let length := calldatasize()
-      for {
-        let pointer := 0
-      } lt(pointer, length) {
-        pointer := add(pointer, 20)
-      } {
-        let admin := and(
-          calldataload(pointer),
-          0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF000000000000000000000000
-        )
+      if eq(_caller, _owner) {
+        let length := calldatasize()
+        for {
+          let pointer := 0
+        } lt(pointer, length) {
+          pointer := add(pointer, 20)
+        } {
+          let admin := and(
+            calldataload(pointer),
+            0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF000000000000000000000000
+          )
 
-        mstore(0, or(ADMIN_LOCATION, admin))
+          mstore(0, or(ADMIN_LOCATION, admin))
 
-        sstore(keccak256(0, 0x20), 1)
+          sstore(keccak256(0, 0x20), 1)
+        }
+
+        return(0, 0)
       }
 
-      return(0, 0)
+      // function checkAdminRole(address caller) external view returns (bool) {
+      mstore(0, or(ADMIN_LOCATION, calldataload(0)))
+      mstore(0, sload(keccak256(0, 0x20)))
+      return(0, 0x20)
     }
   }
 }
