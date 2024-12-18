89cc3d41b83a6a5cc336cea4483928eab183a547
Kristin Kirkov <kristin.kirkov@metacraft-labs.com>
2024-12-18 13:34:51 +0200

    feat(base-utils): Add kebabToCamelCase function

--- libs/ts/base-utils/src/string.ts
+++ libs/ts/base-utils/src/string.ts
@@ -21,3 +21,29 @@ export function kebabToSnakeCase<Str extends string>(
 ): KebabToSnakeCase<Str> {
   return str.replaceAll(/-/g, '_').toUpperCase() as KebabToSnakeCase<Str>;
 }
+
+/**
+ * Converts a kebab-case string literal type to camelCase.
+ */
+export type KebabToCamelCase<S extends string> =
+  S extends `${infer T}-${infer U}`
+    ? `${T}${Capitalize<KebabToCamelCase<U>>}`
+    : S;
+
+/**
+ * Converts a kebab-case string to camelCase.
+ * @param str - The kebab-case string to convert.
+ * @returns The camelCase version of the input string.
+ * @example
+ * ```ts
+ * kebabToCamelCase('foo-bar-baz'); // 'fooBarBaz'
+ * ```
+ * @see {@link KebabToCamelCase}
+ */
+export function kebabToCamelCase<Str extends string>(
+  str: Str,
+): KebabToCamelCase<Str> {
+  return str.replace(/-([a-z])/g, (_, char) =>
+    char.toUpperCase(),
+  ) as KebabToCamelCase<Str>;
+}
