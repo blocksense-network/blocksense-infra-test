blocksense-network/blocksense

feat(sequencer/stream_blocks): Stream the blocks to kafka msg queue

libs/blockchain_data_model/src/lib.rs
@@ -34,7 +34,7 @@ pub struct BlockFeedConfig {
     pub script: DataChunk,
 }
 
-#[derive(Debug, PartialEq, SimpleSerialize, Default)]
+#[derive(Debug, PartialEq, SimpleSerialize, Default, Clone)]
 pub struct AssetFeedUpdate {
     id: FeedIdChunk,
     feed_data: DataChunk,
@@ -51,11 +51,24 @@ pub struct BlockHeader {
     add_remove_feeds_merkle_root: HashType,
 }
 
+impl BlockHeader {
+    pub fn serialize(&mut self) -> Vec<u8> {
+        ssz_rs::serialize(self).expect("Serialization of BlockHeader failed")
+    }
+}
+
+
 #[derive(Debug, PartialEq, SimpleSerialize, Default, Clone)]
 pub struct AddRemoveFeeds {
     block_height: u64,
     new_feeds: [Option<BlockFeedConfig>; MAX_NEW_FEEDS_IN_BLOCK],
     feed_ids_to_rm: [Option<u32>; MAX_FEED_ID_TO_DELETE_IN_BLOCK],
+}
+
+impl AddRemoveFeeds {
+    pub fn serialize(&mut self) -> Vec<u8> {
+        ssz_rs::serialize(self).expect("Serialization of AddRemoveFeeds failed")
+    }
 }
 
 pub const MAX_ASSET_FEED_UPDATES_IN_BLOCK: usize =

blocksense-network/blocksense-infra-test

libs/blockchain_data_model/src/lib.rs
@@ -34,7 +34,7 @@ pub struct BlockFeedConfig {
     pub script: DataChunk,
 }
 
-#[derive(Debug, PartialEq, SimpleSerialize, Default)]
+#[derive(Debug, PartialEq, SimpleSerialize, Default, Clone)]
 pub struct AssetFeedUpdate {
     id: FeedIdChunk,
     feed_data: DataChunk,
@@ -51,11 +51,23 @@ pub struct BlockHeader {
     add_remove_feeds_merkle_root: HashType,
 }
 
+impl BlockHeader {
+    pub fn serialize(&mut self) -> Vec<u8> {
+        ssz_rs::serialize(self).expect("Serialization of BlockHeader failed")
+    }
+}
+
 #[derive(Debug, PartialEq, SimpleSerialize, Default, Clone)]
 pub struct FeedActions {
     block_height: u64,
     new_feeds: [Option<BlockFeedConfig>; MAX_NEW_FEEDS_IN_BLOCK],
     feed_ids_to_rm: [Option<u32>; MAX_FEED_ID_TO_DELETE_IN_BLOCK],
+}
+
+impl FeedActions {
+    pub fn serialize(&mut self) -> Vec<u8> {
+        ssz_rs::serialize(self).expect("Serialization of AddRemoveFeeds failed")
+    }
 }
 
 pub const MAX_ASSET_FEED_UPDATES_IN_BLOCK: usize =