f8c2a87a4682c91d498bba01b88bdf6643097d02
Kristin Kirkov <kristin.kirkov@metacraft-labs.com>
2025-01-08 11:46:14 +0200

    ref(base-utils): Add inevm-testnet to base-utils

--- libs/ts/base-utils/src/evm/networks.ts
+++ libs/ts/base-utils/src/evm/networks.ts
@@ -35,6 +35,7 @@ const networks = [
   'fantom-testnet',
   'gnosis-mainnet',
   'gnosis-chiado',
+  'inevm-testnet',
   'ink-sepolia',
   'kusama-moonriver',
   'linea-mainnet',
@@ -60,9 +61,9 @@ const networks = [
 
 const chainIds = [
   99999999999, 1, 11155111, 17000, 11124, 1088, 42161, 421614, 43114, 43113,
-  8453, 84532, 80084, 56, 97, 42220, 44787, 5115, 250, 4002, 100, 10200, 763373,
-  1285, 59144, 59141, 169, 3441006, 5003, 41454, 2810, 10, 11155420, 137, 80002,
-  1101, 2442, 534352, 534351, 167000, 167009, 324, 300,
+  8453, 84532, 80084, 56, 97, 42220, 44787, 5115, 250, 4002, 100, 10200, 2424,
+  763373, 1285, 59144, 59141, 169, 3441006, 5003, 41454, 2810, 10, 11155420,
+  137, 80002, 1101, 2442, 534352, 534351, 167000, 167009, 324, 300,
 ] as const;
 
 export const networkName = S.Literal(...networks);
@@ -92,6 +93,7 @@ export enum Currency {
   cBTC = 'cBTC',
   DMON = 'DMON',
   FTM = 'FTM',
+  INJ = 'INJ',
   METIS = 'METIS',
   MATIC = 'MATIC',
   MNT = 'MNT',
@@ -238,6 +240,12 @@ export const networkMetadata = {
     isTestnet: true,
     explorerUrl: 'https://gnosis-chiado.blockscout.com',
     currency: Currency.xDAI,
+  },
+  'inevm-testnet': {
+    chainId: 2424,
+    isTestnet: true,
+    explorerUrl: 'https://testnet.explorer.inevm.com/',
+    currency: Currency.INJ,
   },
   'ink-sepolia': {
     chainId: 763373,
