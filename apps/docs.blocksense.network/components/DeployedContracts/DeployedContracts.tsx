blocksense-network/blocksense

feat(docs.ws): `Next.js` successfully generates `Contracts > Deployed Contracts` Page

apps/docs.blocksense.network/components/DeployedContracts/DeployedContracts.tsx
@@ -1,7 +1,18 @@
 import * as React from 'react';
-
+import type { GetStaticProps } from 'next';
+import { Brand } from 'effect/Brand';
 import { parseNetworkName } from '@blocksense/base-utils/evm';
-
+import CHAINLINK_COMPATIBILITY from '@blocksense/monorepo/chainlink_compatibility';
+import CONTRACTS_DEPLOYMENT_CONFIG from '@blocksense/monorepo/evm_contracts_deployment_v1';
+import {
+  decodeChainlinkCompatibilityConfig,
+  decodeDeploymentConfig,
+  DeploymentConfig,
+} from '@blocksense/config-types';
+import {
+  CoreContract,
+  ProxyContractData,
+} from '@/src/deployed-contracts/types';
 import { DataTable } from '@/components/ui/DataTable/DataTable';
 import {
   columns as proxyContractsColumns,
@@ -9,30 +20,109 @@ import {
 } from '@/components/DeployedContracts/proxyContractsColumns';
 import { ContractItemWrapper } from '@/components/sol-contracts/ContractItemWrapper';
 import { CoreContractCard } from '@/components/DeployedContracts/CoreContractCard';
-import {
-  decodeCoreContracts,
-  decodeProxyContracts,
-} from '@/src/deployed-contracts/types';
 import { dataFeedUrl } from '@/src/constants';
 import { NetworkIcon } from '@/components/DeployedContracts/NetworkIcon';
 import { Callout } from '@blocksense/docs-theme';
 
-type DeployedContractsProps = {
-  deployedCoreContractsString: string;
-  deployedProxyContractsString: string;
+export const getStaticProps = (() => {
+  const networksData = decodeDeploymentConfig(CONTRACTS_DEPLOYMENT_CONFIG);
+
+  const getCoreContractsData = (networksData: DeploymentConfig) => {
+    const parsedCoreContracts: CoreContract[] = [];
+
+    Object.entries(networksData).forEach(([_networkName, networkData]) => {
+      if (!networkData) return;
+      if (_networkName === 'local') return;
+      const networkName = parseNetworkName(_networkName);
+      const coreContracts = networkData.contracts.coreContracts;
+
+      Object.entries(coreContracts).forEach(([contractName, contractsData]) => {
+        const existingContract = parsedCoreContracts.find(
+          contract => contract.contract === contractName,
+        );
+
+        if (existingContract) {
+          existingContract.networks.push(networkName);
+        } else {
+          parsedCoreContracts.push({
+            contract: contractName,
+            address: contractsData.address,
+            networks: [networkName],
+          });
+        }
+      });
+    });
+    return parsedCoreContracts;
+  };
+
+  const getProxyContractsContent = (networksData: DeploymentConfig) => {
+    type ChainLinkProxyData =
+      | (string &
+          Brand<'Hex String'> &
+          Brand<'Unformatted Data'> &
+          Brand<'EthereumAddress'>)
+      | undefined
+      | null;
+
+    const { blocksenseFeedsCompatibility } = decodeChainlinkCompatibilityConfig(
+      CHAINLINK_COMPATIBILITY,
+    );
+    const supportedNetworks: ProxyContractData[] = Object.entries(networksData)
+      .map(([_networkName, networkData]) => {
+        if (!networkData) return [];
+        if (_networkName === 'local') return [];
+        const networkName = parseNetworkName(_networkName);
+        const { ChainlinkProxy } = networkData.contracts;
+
+        return ChainlinkProxy.map(proxy => {
+          const compatibilityData = Object.entries(
+            blocksenseFeedsCompatibility,
+          ).find(([_id, data]) => data.description === proxy.description)?.[1];
+
+          if (!compatibilityData) {
+            throw new Error(
+              `No compatibility data found for ${proxy.description}`,
+            );
+          }
+
+          const chainLinkProxyData: ChainLinkProxyData =
+            Object.entries(
+              compatibilityData.chainlink_compatibility.chainlink_aggregators,
+            ).find(([network, _data]) => network === networkName)?.[1] ?? null;
+
+          return {
+            ...proxy,
+            id: compatibilityData.id,
+            network: networkName,
+            chainlink_proxy: chainLinkProxyData,
+          };
+        });
+      })
+      .flat();
+    return supportedNetworks;
+  };
+
+  const parsedCoreContracts = getCoreContractsData(networksData);
+  const parsedProxyContracts = getProxyContractsContent(networksData);
+
+  return {
+    props: { parsedCoreContracts, parsedProxyContracts },
+    revalidate: false,
+  };
+}) satisfies GetStaticProps<{
+  parsedCoreContracts: CoreContract[];
+  parsedProxyContracts: ProxyContractData[];
+}>;
+
+type FeedsDataProps = {
+  parsedCoreContracts: CoreContract[];
+  parsedProxyContracts: ProxyContractData[];
 };
 
 export const DeployedContracts = ({
-  deployedCoreContractsString,
-  deployedProxyContractsString,
-}: DeployedContractsProps) => {
-  const deployedCoreContracts = decodeCoreContracts(
-    JSON.parse(deployedCoreContractsString),
-  );
-  const deployedProxyContracts = decodeProxyContracts(
-    JSON.parse(deployedProxyContractsString),
-  );
-
+  parsedCoreContracts: deployedCoreContracts,
+  parsedProxyContracts: deployedProxyContracts,
+}: FeedsDataProps) => {
   const [selectedNetwork, setSelectedNetwork] = React.useState<string | null>(
     null,
   );

blocksense-network/blocksense-infra-test

apps/docs.blocksense.network/components/DeployedContracts/DeployedContracts.tsx
@@ -1,7 +1,18 @@
 import * as React from 'react';
-
+import type { GetStaticProps } from 'next';
+import { Brand } from 'effect/Brand';
 import { parseNetworkName } from '@blocksense/base-utils/evm';
-
+import CHAINLINK_COMPATIBILITY from '@blocksense/monorepo/chainlink_compatibility';
+import CONTRACTS_DEPLOYMENT_CONFIG from '@blocksense/monorepo/evm_contracts_deployment_v1';
+import {
+  decodeChainlinkCompatibilityConfig,
+  decodeDeploymentConfig,
+  DeploymentConfig,
+} from '@blocksense/config-types';
+import {
+  CoreContract,
+  ProxyContractData,
+} from '@/src/deployed-contracts/types';
 import { DataTable } from '@/components/ui/DataTable/DataTable';
 import {
   columns as proxyContractsColumns,
@@ -9,30 +20,109 @@ import {
 } from '@/components/DeployedContracts/proxyContractsColumns';
 import { ContractItemWrapper } from '@/components/sol-contracts/ContractItemWrapper';
 import { CoreContractCard } from '@/components/DeployedContracts/CoreContractCard';
-import {
-  decodeCoreContracts,
-  decodeProxyContracts,
-} from '@/src/deployed-contracts/types';
 import { dataFeedUrl } from '@/src/constants';
 import { NetworkIcon } from '@/components/DeployedContracts/NetworkIcon';
 import { Callout } from '@blocksense/docs-theme';
 
-type DeployedContractsProps = {
-  deployedCoreContractsString: string;
-  deployedProxyContractsString: string;
+export const getStaticProps = (() => {
+  const networksData = decodeDeploymentConfig(CONTRACTS_DEPLOYMENT_CONFIG);
+
+  const getCoreContractsData = (networksData: DeploymentConfig) => {
+    const parsedCoreContracts: CoreContract[] = [];
+
+    Object.entries(networksData).forEach(([_networkName, networkData]) => {
+      if (!networkData) return;
+      if (_networkName === 'local') return;
+      const networkName = parseNetworkName(_networkName);
+      const coreContracts = networkData.contracts.coreContracts;
+
+      Object.entries(coreContracts).forEach(([contractName, contractsData]) => {
+        const existingContract = parsedCoreContracts.find(
+          contract => contract.contract === contractName,
+        );
+
+        if (existingContract) {
+          existingContract.networks.push(networkName);
+        } else {
+          parsedCoreContracts.push({
+            contract: contractName,
+            address: contractsData.address,
+            networks: [networkName],
+          });
+        }
+      });
+    });
+    return parsedCoreContracts;
+  };
+
+  const getProxyContractsContent = (networksData: DeploymentConfig) => {
+    type ChainLinkProxyData =
+      | (string &
+          Brand<'Hex String'> &
+          Brand<'Unformatted Data'> &
+          Brand<'EthereumAddress'>)
+      | undefined
+      | null;
+
+    const { blocksenseFeedsCompatibility } = decodeChainlinkCompatibilityConfig(
+      CHAINLINK_COMPATIBILITY,
+    );
+    const supportedNetworks: ProxyContractData[] = Object.entries(networksData)
+      .map(([_networkName, networkData]) => {
+        if (!networkData) return [];
+        if (_networkName === 'local') return [];
+        const networkName = parseNetworkName(_networkName);
+        const { CLAggregatorAdapter } = networkData.contracts;
+
+        return CLAggregatorAdapter.map(proxy => {
+          const compatibilityData = Object.entries(
+            blocksenseFeedsCompatibility,
+          ).find(([_id, data]) => data.description === proxy.description)?.[1];
+
+          if (!compatibilityData) {
+            throw new Error(
+              `No compatibility data found for ${proxy.description}`,
+            );
+          }
+
+          const chainLinkProxyData: ChainLinkProxyData =
+            Object.entries(
+              compatibilityData.chainlink_compatibility.chainlink_aggregators,
+            ).find(([network, _data]) => network === networkName)?.[1] ?? null;
+
+          return {
+            ...proxy,
+            id: compatibilityData.id,
+            network: networkName,
+            chainlink_proxy: chainLinkProxyData,
+          };
+        });
+      })
+      .flat();
+    return supportedNetworks;
+  };
+
+  const parsedCoreContracts = getCoreContractsData(networksData);
+  const parsedProxyContracts = getProxyContractsContent(networksData);
+
+  return {
+    props: { parsedCoreContracts, parsedProxyContracts },
+    revalidate: false,
+  };
+}) satisfies GetStaticProps<{
+  parsedCoreContracts: CoreContract[];
+  parsedProxyContracts: ProxyContractData[];
+}>;
+
+type FeedsDataProps = {
+  parsedCoreContracts: CoreContract[];
+  parsedProxyContracts: ProxyContractData[];
 };
 
 export const DeployedContracts = ({
-  deployedCoreContractsString,
-  deployedProxyContractsString,
-}: DeployedContractsProps) => {
-  const deployedCoreContracts = decodeCoreContracts(
-    JSON.parse(deployedCoreContractsString),
-  );
-  const deployedProxyContracts = decodeProxyContracts(
-    JSON.parse(deployedProxyContractsString),
-  );
-
+  parsedCoreContracts: deployedCoreContracts,
+  parsedProxyContracts: deployedProxyContracts,
+}: FeedsDataProps) => {
   const [selectedNetwork, setSelectedNetwork] = React.useState<string | null>(
     null,
   );