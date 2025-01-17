c95bac50e20f8dbb840fd828d4cf962affb1687c
Kalin Hristov <kalinsky.hristov@gmail.com>
2025-01-17 14:41:12 +0200

    feat(docs.ws): `Next.js` successfully generates all contract pages
    Use `generateStaticParams()` for prerendering the static HTML for all contracts in our `fine.json`,
    which are exposed at the `reference-documentation/contract/[contract]` URL.

---
+++ apps/docs.blocksense.network/components/ReferenceDocumentation/[contract].tsx
@@ -0,0 +1,39 @@
+import * as path from 'path';
+import { SourceUnit } from './SourceUnit';
+
+import { SourceUnitDocItem } from '@blocksense/sol-reflector';
+import SOL_REFLECTION_JSON from '@blocksense/contracts/docs/fine';
+
+const solReflection = SOL_REFLECTION_JSON as SourceUnitDocItem[];
+
+export function generateStaticParams() {
+  return solReflection.map((sourceUnit: SourceUnitDocItem) => ({
+    sourceUnitName: path.parse(sourceUnit.absolutePath).name,
+  }));
+}
+
+type Params = {
+  contract: string;
+};
+
+export default async function ContractPageWrapper({
+  params,
+}: {
+  params: Params;
+}) {
+  const { contract: contractName } = await params;
+
+  const sourceUnit = solReflection.find((info: SourceUnitDocItem) => {
+    if (!info || !info.absolutePath) {
+      return false;
+    }
+
+    return path.parse(info.absolutePath).name === contractName;
+  });
+
+  if (!sourceUnit) {
+    throw new Error(`No deployment info found for contract: ${contractName}`);
+  }
+
+  return <SourceUnit sourceUnit={sourceUnit} />;
+}
