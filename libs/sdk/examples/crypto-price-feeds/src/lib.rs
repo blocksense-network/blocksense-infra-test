bc5e9ab3f62f944d019624ed1d8f22c8ef014d20
Antoni Dikov <antoni.dikov@gmail.com>
2025-01-21 19:03:24 +0200

    feat(oracle): Cypro price oracle returns proper data to the sequencer

--- libs/sdk/examples/crypto-price-feeds/src/lib.rs
+++ libs/sdk/examples/crypto-price-feeds/src/lib.rs
@@ -1,8 +1,7 @@
-use anyhow::{Result, Context};
+use anyhow::{Result, Context, bail};
 // use async_trait::async_trait;
 use blocksense_sdk::{
-    // oracle::{DataFeedResult, DataFeedResultValue, Payload, Settings},
-    oracle::{Payload, Settings},
+    oracle::{DataFeedResult, DataFeedResultValue, Payload, Settings},
     // price_pair::{DataProvider, OraclePriceHelper},
     oracle_component,
     spin::http::{send, Method, Request, Response},
@@ -17,7 +16,9 @@ use url::Url;
 //1. Move all specific exchange logic to separate files.
 //2. Move URLS to constants
 //3. Try to minimize object cloning.
-//3. Try to minimize object cloning.
+
+const USD_SYMBOLS: [&'static str; 3] = ["USD", "USDC", "USDT"];
+
 
 #[derive(Default, Debug, Clone, PartialEq, Deserialize)]
 pub struct BinancePrice {
@@ -25,7 +26,7 @@ pub struct BinancePrice {
     pub price: String,
 }
 
-async fn get_binance_prices(symbols: Vec<String>) -> Result<HashMap<String, String>> {
+async fn get_binance_prices(resources: &Vec<ResourceData>, results: &mut HashMap<String, Vec<ResourceResult>>) -> Result<()> {
     let url = Url::parse(
         "https://api.binance.com/api/v3/ticker/price",
     )?;
@@ -41,17 +42,10 @@ async fn get_binance_prices(symbols: Vec<String>) -> Result<HashMap<String, Stri
     let body = resp.into_body();
     let string = String::from_utf8(body)?;
     let values: Vec<BinancePrice> = serde_json::from_str(&string)?;
-    let map: HashMap<String, String> = values.into_iter().map(|value| (value.symbol, value.price)).collect();
-    let mut result: HashMap<String, String> = HashMap::<String,String>::new();
-    //TODO(adikov): We need a proper way to get trade volume from Binance API.
-    for symbol in symbols {
-        if map.contains_key(&symbol) {
-            //TODO(adikov): remove unwrap
-            result.insert(symbol.clone(), map.get(&symbol).unwrap().clone());
-        }
-    }
+    let response: HashMap<String, String> = values.into_iter().map(|value| (value.symbol, value.price)).collect();
 
-    Ok(result)
+    fill_results(resources, results, response)?;
+    Ok(())
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Deserialize)]
@@ -65,7 +59,7 @@ pub struct KrakenResponse {
     pub result: HashMap<String, KrakenPrice>,
 }
 
-async fn get_kraken_prices(symbols: Vec<String>) -> Result<HashMap<String, String>> {
+async fn get_kraken_prices(resources: &Vec<ResourceData>, results: &mut HashMap<String, Vec<ResourceResult>>) -> Result<()> {
     let url = Url::parse(
         "https://api.kraken.com/0/public/Ticker",
     )?;
@@ -81,16 +75,14 @@ async fn get_kraken_prices(symbols: Vec<String>) -> Result<HashMap<String, Strin
     let body = resp.into_body();
     let string = String::from_utf8(body)?;
     let value: KrakenResponse = serde_json::from_str(&string)?;
-    let mut result: HashMap<String, String> = HashMap::<String,String>::new();
-
-    for symbol in symbols {
-        if value.result.contains_key(&symbol) {
-            //TODO(adikov): remove unwrap
-            result.insert(symbol.clone(), value.result.get(&symbol).unwrap().a.first().unwrap().clone());
-        }
+    let mut response: HashMap<String, String> = HashMap::new();
+    for (symbol, price) in value.result {
+        response.insert(symbol.clone(), price.a.first().context(format!("Kraken has no price in response for symbol: {}", symbol))?.clone());
     }
 
-    Ok(result)
+    fill_results(resources, results, response)?;
+
+    Ok(())
 }
 
 //TODO(adikov): Include all the needed fields form the response like volume.
@@ -115,7 +107,7 @@ pub struct BybitResponse {
     pub result: BybitResult,
 }
 
-async fn get_bybit_prices(symbols: Vec<String>) -> Result<HashMap<String, String>> {
+async fn get_bybit_prices(resources: &Vec<ResourceData>, results: &mut HashMap<String, Vec<ResourceResult>>) -> Result<()> {
     let url = Url::parse_with_params(
         "https://api.bybit.com/v5/market/tickers",
         &[("category", "spot"), ("symbols", "")],
@@ -132,50 +124,57 @@ async fn get_bybit_prices(symbols: Vec<String>) -> Result<HashMap<String, String
     let body = resp.into_body();
     let string = String::from_utf8(body)?;
     let value: BybitResponse = serde_json::from_str(&string)?;
-    let map: HashMap<String, String> = value.result.list.into_iter().map(|value| (value.symbol, value.last_price)).collect();
-    let mut result: HashMap<String, String> = HashMap::<String,String>::new();
+    let response: HashMap<String, String> = value.result.list.into_iter().map(|value| (value.symbol, value.last_price)).collect();
 
-    for symbol in symbols {
-        if map.contains_key(&symbol) {
-            //TODO(adikov): remove unwrap
-            result.insert(symbol.clone(), map.get(&symbol).unwrap().clone());
-        }
-    }
+    fill_results(resources, results, response)?;
 
-    Ok(result)
+    Ok(())
 }
 
-#[derive(Default, Debug, Clone, PartialEq, Deserialize)]
-pub struct CoinbaseData {
-    pub currency: String,
-    pub rates: HashMap<String, String>,
-}
+// #[derive(Default, Debug, Clone, PartialEq, Deserialize)]
+// pub struct CoinbaseData {
+//     pub currency: String,
+//     pub rates: HashMap<String, String>,
+// }
 
-#[derive(Default, Debug, Clone, PartialEq, Deserialize)]
-pub struct CoinbaseResponse {
-    pub data: CoinbaseData,
-}
+// #[derive(Default, Debug, Clone, PartialEq, Deserialize)]
+// pub struct CoinbaseResponse {
+//     pub data: CoinbaseData,
+// }
 
-async fn get_coinbase_prices(currency: String) -> Result<HashMap<String, String>> {
-    let url = Url::parse_with_params(
-        "https://api.coinbase.com/v2/exchange-rates",
-        &[("currency", currency)],
-    )?;
+// async fn get_coinbase_prices(resources: &Vec<ResourceData>, results: &mut HashMap<String, Vec<ResourceResult>>) -> Result<()> {
+//     let url = Url::parse_with_params(
+//         "https://api.coinbase.com/v2/exchange-rates",
+//         &[("currency", "USD")],
+//     )?;
 
-    let mut req = Request::builder();
-    req.method(Method::Get);
-    req.uri(url);
-    req.header("Accepts", "application/json");
+//     let mut req = Request::builder();
+//     req.method(Method::Get);
+//     req.uri(url);
+//     req.header("Accepts", "application/json");
 
-    let req = req.build();
-    let resp: Response = send(req).await?;
+//     let req = req.build();
+//     let resp: Response = send(req).await?;
 
-    let body = resp.into_body();
-    let string = String::from_utf8(body)?;
-    let value: CoinbaseResponse = serde_json::from_str(&string)?;
+//     let body = resp.into_body();
+//     let string = String::from_utf8(body)?;
+//     let value: CoinbaseResponse = serde_json::from_str(&string)?;
 
-    Ok(value.data.rates)
-}
+//     for resource in resources {
+//         if value.data.rates.contains_key(&resource.symbol) {
+//             //TODO(adikov): remove unwrap
+//             let res = results.entry(resource.id.clone()).or_insert(vec![]);
+//             res.push(ResourceResult {
+//                 id: resource.id.clone(),
+//                 symbol: resource.symbol.clone(),
+//                 usd_symbol: resource.symbol.to_string(),
+//                 result: value.data.rates.get(&resource.symbol).unwrap().clone(),
+//             });
+//         }
+//     }
+
+//     Ok(())
+// }
 
 #[derive(Default, Debug, Clone, PartialEq, Deserialize)]
 pub struct CmcResource {
@@ -183,69 +182,140 @@ pub struct CmcResource {
     pub cmc_quote: String,
 }
 
+#[derive(Debug)]
+struct ResourceData {
+    pub symbol: String,
+    pub id: String,
+}
+
+#[derive(Debug)]
+struct ResourceResult {
+    pub id: String,
+    pub symbol: String,
+    pub usd_symbol: String,
+    pub result: String,
+    //TODO(adikov): Add balance information when we start getting it.
+}
+
+fn fill_results(resources: &Vec<ResourceData>, results: &mut HashMap<String, Vec<ResourceResult>>, response: HashMap<String, String>) -> Result<()> {
+    //TODO(adikov): We need a proper way to get trade volume from Binance API.
+    for resource in resources {
+        // First USD pair found.
+        for symbol in USD_SYMBOLS {
+            let quote = format!("{}{}", resource.symbol, symbol);
+            if response.contains_key(&quote) {
+                //TODO(adikov): remove unwrap
+                let res = results.entry(resource.id.clone()).or_insert(vec![]);
+                res.push(ResourceResult {
+                    id: resource.id.clone(),
+                    symbol: resource.symbol.clone(),
+                    usd_symbol: symbol.to_string(),
+                    result: response.get(&quote).unwrap().clone(),
+                });
+                break;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn vwap(results: &Vec<ResourceResult>) -> Result<f64> {
+
+    if results.is_empty() {
+        bail!("Missing results");
+    }
+
+    //TODO(adikov): Implement vwap logic here.
+    // Assume a five-minute chart. The calculation is the same regardless of what intraday time frame is used.
+    // 1. Find the average price the stock traded at over the first five-minute period of the day.
+    //    To do this, add the high, low, and close, then divide by three.
+    //    Multiply this by the volume for that period. Record the result in a spreadsheet, under column PV (price, volume).
+    // 2. Divide PV by the volume for that period. This will produce the VWAP.
+    // 3. To maintain the VWAP throughout the day, continue to add the PV value from each period to the prior values.
+    //    Divide this total by the total volume up to that point.
+    //
+    //   THIS IS NOT THE PROPER IMPLEMENTATION IT IS FOR TEST PURPOSES
+    let mut sum: f64 = 0.0f64;
+    for res in results {
+       sum = sum + res.result.parse::<f64>()?;
+    }
+
+    Ok(sum / results.len() as f64)
+}
+
+fn process_results(results: HashMap<String, Vec<ResourceResult>>) -> Result<Payload> {
+    let mut payload: Payload = Payload::new();
+    for (feed_id, results) in results.iter() {
+
+        payload.values.push(match vwap(results) {
+            Ok(price) => {
+                DataFeedResult {
+                    id: feed_id.clone(),
+                    value: DataFeedResultValue::Numerical(price),
+                }
+            }
+            Err(err) => {
+                DataFeedResult {
+                    id: feed_id.clone(),
+                    value: DataFeedResultValue::Error(err.to_string()),
+                }
+            }
+        });
+    }
+
+    Ok(payload)
+}
+
+fn print_results(resources: &Vec<ResourceData>, results: &HashMap<String, Vec<ResourceResult>>) {
+    let mut missing = "[".to_string();
+    for res in resources {
+        if !results.contains_key(&res.id) {
+            missing.push_str(&format!("({}-{}),", res.id, res.symbol).to_string());
+        }
+    }
+    println!("missing ids(id-symbol): {}]", missing);
+
+    let mut print = "[".to_string();
+    for (id,  results) in results {
+        print.push_str(&format!("({}-{}),", id, results.len()).to_string());
+    }
+    println!("(id-echange_count): {}]", print);
+}
+
 #[oracle_component]
 async fn oracle_request(settings: Settings) -> Result<Payload> {
-    let mut resources: HashMap<String, CmcResource> = HashMap::new();
-    let mut binance_quotes: Vec<String> = vec![];
-    let mut kraken_quotes: Vec<String> = vec![];
-    let mut bybit_quotes: Vec<String> = vec![];
+    let mut resources: Vec<ResourceData> = vec![];
+    let mut results: HashMap<String, Vec<ResourceResult>> = HashMap::<String, Vec<ResourceResult>>::new();
     // let mut ids: Vec<String> = vec![];
     //TODO(adikov): Make sure citrea feeds exist so that we can properly test.
     // let citrea_feeds = vec!["BTCUSD", "ETHUSD", "EURCUSD", "USDTUSD", "USDCUSD", "PAXGUSD", "TBTCUSD", "WBTCUSD", "WSTETHUSD"];
     for feed in settings.data_feeds.iter() {
         let data: CmcResource = serde_json::from_str(&feed.data).context("Couldn't parse Data Feed resource properly")?;
-        resources.insert(feed.id.clone(), data.clone());
+        resources.push(ResourceData { symbol: data.cmc_quote.clone(), id: feed.id.clone()});
         //TODO(adikov): We need to get all the proper symbols from the new data feed configuration.
-        binance_quotes.push(format!("{}{}", data.cmc_quote, "USDT"));
-        kraken_quotes.push(format!("{}{}", data.cmc_quote, "USDT"));
-        bybit_quotes.push(format!("{}{}", data.cmc_quote, "USDT"));
     }
 
-    let binance = get_binance_prices(binance_quotes).await?;
-    println!("Binance - {:?}", binance);
-    let kraken = get_kraken_prices(kraken_quotes).await?;
-    println!("Kraken - {:?}", kraken);
-    let bybit = get_bybit_prices(bybit_quotes).await?;
-    println!("Bybit - {:?}", bybit);
-    let coinbase = get_coinbase_prices("USD".to_string()).await?;
-    println!("Coinbase - {:?}", coinbase);
+    get_binance_prices(&resources, &mut results).await?;
+    print_results(&resources, &results);
+
+    get_kraken_prices(&resources, &mut results).await?;
+    print_results(&resources, &results);
+
+    get_bybit_prices(&resources, &mut results).await?;
+    print_results(&resources, &results);
+
+    //TODO(adikov): Transform coinbase price to match the others - 1 / coinbase price
+    // get_coinbase_prices(&resources, &mut results).await?;
+    // print_results(&resources, &results);
 
     //TODO(adikov): Write the logic for transforming the data to DataFeedResult
     // We need proper way to match binance, kraken, bybit and coinbase response to
     // data feed ID.
 
-    let payload: Payload = Payload::new();
-    for (_feed_id, _) in resources.iter() {
-        // payload.values.push(match value.data.get(&data.cmc_id.parse::<u64>()?) {
-        //     Some(cmc) => {
-        //         let value = if let Some(&CmcValue { price }) = cmc.quote.get("USD") {
-        //             DataFeedResultValue::Numerical(price)
-        //         } else {
-        //             DataFeedResultValue::Error(format!(
-        //                 "No price in USD for data feed with id {}",
-        //                 data.cmc_id
-        //             ))
-        //         };
+    let payload = process_results(results)?;
+    println!("Final Payload - {:?}", payload);
 
-        //         DataFeedResult {
-        //             id: feed_id.clone(),
-        //             value,
-        //         }
-        //     }
-        //     None => {
-        //         let error = if value.status.error_code == 0 {
-        //                 format!("CMC data feed with id {} is not found", data.cmc_id)
-        //             } else {
-        //                 println!("CMC returned error with code = {} message = {}", value.status.error_code, value.status.error_message);
-        //                 value.status.error_message.to_string()
-        //             };
-        //         DataFeedResult {
-        //             id: feed_id.clone(),
-        //             value: DataFeedResultValue::Error(error),
-        //         }
-        //     }
-        // });
-    }
 
     Ok(payload)
 }
