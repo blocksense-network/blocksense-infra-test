42b44cc1379d8dc80d805c25b4319f13d89df77e
Aneta Tsvetkova <anetastsvetkova@gmail.com>
2025-01-28 15:50:58 +0200

    feat(tests): Adjust cl adapter consumer example tests for adfs contract

---
+++ libs/ts/contracts/test/examples/CLAggregatorAdapterConsumer.test.ts
@@ -0,0 +1,77 @@
+import { artifacts, ethers } from 'hardhat';
+
+import * as utils from './utils/clAggregatorAdapterConsumer';
+import { expect } from 'chai';
+import { deployContract } from '../experiments/utils/helpers/common';
+import { CLAggregatorAdapterConsumer } from '../../typechain';
+import {
+  CLAdapterWrapper,
+  UpgradeableProxyADFSWrapper,
+} from '../utils/wrappers';
+import { encodeDataAndTimestamp } from '../utils/helpers/common';
+
+describe('Example: CLAggregatorAdapterConsumer', function () {
+  let clAggregatorAdapter: CLAdapterWrapper;
+  let clAggregatorAdapterConsumer: CLAggregatorAdapterConsumer;
+
+  beforeEach(async function () {
+    const admin = (await ethers.getSigners())[9];
+    const sequencer = (await ethers.getSigners())[10];
+    const accessControlAdmin = (await ethers.getSigners())[5];
+
+    const proxy = new UpgradeableProxyADFSWrapper();
+    await proxy.init(await admin.getAddress(), accessControlAdmin);
+
+    await proxy.implementation.accessControl.setAdminStates(
+      accessControlAdmin,
+      [sequencer.address],
+      [true],
+    );
+
+    clAggregatorAdapter = new CLAdapterWrapper();
+    await clAggregatorAdapter.init('ETH/USD', 8, 3, proxy);
+
+    const value = encodeDataAndTimestamp(1234, Date.now());
+    await clAggregatorAdapter.setFeed(sequencer, value, 1n);
+
+    clAggregatorAdapterConsumer =
+      await deployContract<CLAggregatorAdapterConsumer>(
+        'CLAggregatorAdapterConsumer',
+        clAggregatorAdapter.contract.target,
+      );
+  });
+
+  [
+    { title: 'get decimals', fnName: 'getDecimals' },
+    { title: 'get description', fnName: 'getDescription' },
+    { title: 'get latest answer', fnName: 'getLatestAnswer' },
+    { title: 'get latest round', fnName: 'getLatestRound' },
+    { title: 'get latest round data', fnName: 'getLatestRoundData' },
+  ].forEach(data => {
+    it('Should ' + data.title, async function () {
+      await getAndCompareData([], data.fnName as keyof typeof utils);
+    });
+  });
+
+  it('Should get round data', async function () {
+    await getAndCompareData([1], 'getRoundData');
+  });
+
+  const getAndCompareData = async (
+    data: any[],
+    functionName: keyof typeof utils,
+  ) => {
+    const contractData = await clAggregatorAdapterConsumer.getFunction(
+      functionName,
+    )(...data);
+
+    const config = {
+      address: clAggregatorAdapter.contract.target,
+      abiJson: (await artifacts.readArtifact('CLAggregatorAdapter')).abi,
+      provider: clAggregatorAdapter.contract.runner!,
+    };
+    const utilData = await utils[functionName](config, ...data);
+
+    expect(contractData).to.deep.equal(utilData);
+  };
+});
---
+++ libs/ts/contracts/test/examples/CLFeedRegistryAdapterConsumer.test.ts
@@ -0,0 +1,131 @@
+import { artifacts, ethers } from 'hardhat';
+import { CLFeedRegistryAdapterConsumer } from '../../typechain';
+import { deployContract, TOKENS } from '../experiments/utils/helpers/common';
+import * as utils from './utils/clFeedRegistryAdapterConsumer';
+import { expect } from 'chai';
+import {
+  CLAdapterWrapper,
+  CLRegistryBaseWrapper,
+  UpgradeableProxyADFSWrapper,
+} from '../utils/wrappers';
+import { encodeDataAndTimestamp } from '../utils/helpers/common';
+
+const aggregatorData = [
+  {
+    description: 'ETH/USD',
+    decimals: 8,
+    key: 3,
+    base: TOKENS.ETH,
+    quote: TOKENS.USD,
+  },
+  {
+    description: 'BTC/USD',
+    decimals: 6,
+    key: 132,
+    base: TOKENS.BTC,
+    quote: TOKENS.USD,
+  },
+];
+
+describe('Example: CLFeedRegistryAdapterConsumer', function () {
+  let feedRegistry: CLRegistryBaseWrapper;
+  let clFeedRegistryAdapterConsumer: CLFeedRegistryAdapterConsumer;
+
+  beforeEach(async function () {
+    let aggregators = [];
+
+    const admin = (await ethers.getSigners())[9];
+    const sequencer = (await ethers.getSigners())[10];
+    const accessControlAdmin = (await ethers.getSigners())[5];
+    const registryOwner = (await ethers.getSigners())[11];
+
+    const proxy = new UpgradeableProxyADFSWrapper();
+    await proxy.init(await admin.getAddress(), accessControlAdmin);
+
+    await proxy.implementation.accessControl.setAdminStates(
+      accessControlAdmin,
+      [sequencer.address],
+      [true],
+    );
+
+    for (const data of aggregatorData) {
+      const newAdapter = new CLAdapterWrapper();
+      await newAdapter.init(data.description, data.decimals, data.key, proxy);
+      aggregators.push(newAdapter);
+
+      const value = encodeDataAndTimestamp(data.key * 1000, Date.now());
+      await newAdapter.setFeed(sequencer, value, 1n);
+    }
+
+    feedRegistry = new CLRegistryBaseWrapper('CLRegistryV2', proxy.contract);
+    await feedRegistry.init(registryOwner);
+
+    await feedRegistry.setFeeds(
+      aggregatorData.map((d, i) => {
+        return {
+          base: d.base,
+          quote: d.quote,
+          feed: aggregators[i],
+        };
+      }),
+    );
+
+    clFeedRegistryAdapterConsumer =
+      await deployContract<CLFeedRegistryAdapterConsumer>(
+        'CLFeedRegistryAdapterConsumer',
+        feedRegistry.contract.target,
+      );
+  });
+
+  [
+    { title: 'get decimals', fnName: 'getDecimals' },
+    { title: 'get description', fnName: 'getDescription' },
+    { title: 'get latest answer', fnName: 'getLatestAnswer' },
+    { title: 'get latest round', fnName: 'getLatestRound' },
+    { title: 'get latest round data', fnName: 'getLatestRoundData' },
+    { title: 'get feed', fnName: 'getFeed' },
+  ].forEach(data => {
+    it('Should ' + data.title, async function () {
+      await getAndCompareData(
+        [
+          [TOKENS.ETH, TOKENS.USD],
+          [TOKENS.BTC, TOKENS.USD],
+        ],
+        data.fnName as keyof typeof utils,
+      );
+    });
+  });
+
+  it('Should get round data', async function () {
+    await getAndCompareData(
+      [
+        [TOKENS.ETH, TOKENS.USD, 1],
+        [TOKENS.BTC, TOKENS.USD, 1],
+      ],
+      'getRoundData',
+    );
+  });
+
+  const getAndCompareData = async (
+    data: any[],
+    functionName: keyof typeof utils,
+  ) => {
+    const contractData1 = await clFeedRegistryAdapterConsumer.getFunction(
+      functionName,
+    )(...data[0]);
+    const contractData2 = await clFeedRegistryAdapterConsumer.getFunction(
+      functionName,
+    )(...data[1]);
+
+    const config = {
+      address: feedRegistry.contract.target,
+      abiJson: (await artifacts.readArtifact('CLFeedRegistryAdapterExp')).abi,
+      provider: feedRegistry.contract.runner!,
+    };
+    const utilData1 = await utils[functionName](config, ...data[0]);
+    const utilData2 = await utils[functionName](config, ...data[1]);
+
+    expect(contractData1).to.deep.equal(utilData1);
+    expect(contractData2).to.deep.equal(utilData2);
+  };
+});
