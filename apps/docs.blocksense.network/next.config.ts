0ffdfb12ed68209c0e1c5004f6d8c20e681572f5
Petar Kirov <petar.p.kirov@gmail.com>
2025-01-29 20:27:17 +0200

    config(docs.ws/next): Remove unused `redirects` config incompatible with static exports

--- apps/docs.blocksense.network/next.config.ts
+++ apps/docs.blocksense.network/next.config.ts
@@ -11,29 +11,6 @@ const nextConfig = withNextra({
     // ESLint behaves weirdly in this monorepo.
     ignoreDuringBuilds: true,
   },
-  redirects: async () => [
-    // TODO: decide if we need this
-    // {
-    //   source: '/docs/guide/:slug(typescript|latex|tailwind-css|mermaid)',
-    //   destination: '/docs/advanced/:slug',
-    //   permanent: true,
-    // },
-    // {
-    //   source: '/docs/docs-theme/built-ins/:slug(callout|steps|tabs|bleed)',
-    //   destination: '/docs/built-ins/:slug',
-    //   permanent: true,
-    // },
-    // {
-    //   source: '/docs/docs-theme/api/use-config',
-    //   destination: '/docs/docs-theme/api',
-    //   permanent: true,
-    // },
-    // {
-    //   source: '/docs/guide/advanced/:slug',
-    //   destination: '/docs/advanced/:slug',
-    //   permanent: true,
-    // },
-  ],
   webpack(config) {
     // rule.exclude doesn't work starting from Next.js 15
     const { test: _test, ...imageLoaderOptions } = config.module.rules.find(
