90112048d1396aa45145e729bfef1e52f378e261
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2025-02-10 22:09:50 +0200

    feat(oracles): Implement Fetcher trait

--- apps/oracles/crypto-price-feeds/src/common.rs
+++ apps/oracles/crypto-price-feeds/src/common.rs
@@ -1,5 +1,8 @@
 use anyhow::Result;
+use blocksense_sdk::spin::http::{Method, Request, Response};
+use serde::de::DeserializeOwned;
 use std::collections::HashMap;
+use url::Url;
 
 pub const USD_SYMBOLS: [&str; 3] = ["USD", "USDC", "USDT"];
 
@@ -46,4 +49,34 @@ pub fn fill_results(
     }
 
     Ok(())
+}
+
+pub trait Fetcher {
+    type ParsedResponse;
+    type ApiResponse: DeserializeOwned; // Ensure it's deserializable
+
+    fn prepare_get_request(
+        &self,
+        base_url: &str,
+        params: Option<&[(&str, &str)]>,
+    ) -> Result<Request> {
+        let url = match params {
+            Some(p) => Url::parse_with_params(base_url, p)?,
+            None => Url::parse(base_url)?,
+        };
+
+        let mut req = Request::builder();
+        req.method(Method::Get);
+        req.uri(url.as_str());
+        req.header("Accepts", "application/json");
+
+        Ok(req.build())
+    }
+
+    fn deserialize_response(&self, resp: Response) -> Result<Self::ApiResponse> {
+        let body = resp.into_body();
+        serde_json::from_slice(&body).map_err(|e| e.into())
+    }
+
+    fn parse_response(&self, response: Self::ApiResponse) -> Result<Self::ParsedResponse>;
 }
