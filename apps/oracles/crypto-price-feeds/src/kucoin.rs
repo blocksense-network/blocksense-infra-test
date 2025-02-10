a2199a2a1c2209b75322c69d16665f089214aa8e
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2025-02-10 22:10:12 +0200

    feat(oracles): Implement Fetcher trait for Kucoin

--- apps/oracles/crypto-price-feeds/src/kucoin.rs
+++ apps/oracles/crypto-price-feeds/src/kucoin.rs
@@ -1,10 +1,9 @@
 use anyhow::Result;
-use blocksense_sdk::spin::http::{send, Method, Request, Response};
+use blocksense_sdk::spin::http::{send, Response};
 
 use serde::Deserialize;
-use url::Url;
 
-use crate::common::PairPriceData;
+use crate::common::{Fetcher, PairPriceData};
 
 #[derive(Default, Debug, Clone, PartialEq, Deserialize)]
 pub struct KuCoinPrice {
@@ -23,29 +22,32 @@ pub struct KuCoinResponse {
     pub data: KuCoinResult,
 }
 
+struct KuCoinFetcher;
+
+impl Fetcher for KuCoinFetcher {
+    type ParsedResponse = PairPriceData;
+    type ApiResponse = KuCoinResponse;
+
+    fn parse_response(&self, value: KuCoinResponse) -> Result<Self::ParsedResponse> {
+        let response: Self::ParsedResponse = value
+            .data
+            .ticker
+            .into_iter()
+            .filter(|value| value.last.is_some())
+            // KuCoin have symbols in format "X-Y". We need to match logic in `fill_results`
+            .map(|value| (value.symbol.replace("-", ""), value.last.unwrap()))
+            .collect();
+
+        Ok(response)
+    }
+}
+
 pub async fn get_kucoin_prices() -> Result<PairPriceData> {
-    let url = Url::parse("https://api.kucoin.com/api/v1/market/allTickers")?;
+    let fetcher = KuCoinFetcher {};
+    let req = fetcher.prepare_get_request("https://api.kucoin.com/api/v1/market/allTickers", None);
+    let resp: Response = send(req?).await?;
+    let deserialized = fetcher.deserialize_response(resp)?;
+    let pair_prices: PairPriceData = fetcher.parse_response(deserialized)?;
 
-    let mut req = Request::builder();
-    req.method(Method::Get);
-    req.uri(url);
-    req.header("Accepts", "application/json");
-
-    let req = req.build();
-    let resp: Response = send(req).await?;
-
-    let body = resp.into_body();
-    let body_as_string = String::from_utf8(body)?;
-    let value: KuCoinResponse = serde_json::from_str(&body_as_string)?;
-
-    let response: PairPriceData = value
-        .data
-        .ticker
-        .into_iter()
-        .filter(|value| value.last.is_some())
-        // KuCoin have symbols in format "X-Y". We need to match logic in `fill_results`
-        .map(|value| (value.symbol.replace("-", ""), value.last.unwrap()))
-        .collect();
-
-    Ok(response)
+    Ok(pair_prices)
 }
