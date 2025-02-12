e8e05d78c2902e344299f88fd2867c4cf41e9428
Mila Genova <mila.genova@blocksense.network>
2025-02-12 15:13:26 +0200

    feat(oracles): Implement Fetcher trait for `Binance`

--- apps/oracles/crypto-price-feeds/src/binance.rs
+++ apps/oracles/crypto-price-feeds/src/binance.rs
@@ -1,35 +1,40 @@
 use anyhow::Result;
-use blocksense_sdk::spin::http::{send, Method, Request, Response};
+use blocksense_sdk::spin::http::{send, Response};
 
 use serde::Deserialize;
-use url::Url;
 
-use crate::common::PairPriceData;
+use crate::common::{Fetcher, PairPriceData};
 
 #[derive(Default, Debug, Clone, PartialEq, Deserialize)]
-pub struct BinancePrice {
+pub struct BinancePriceData {
     pub symbol: String,
     pub price: String,
 }
 
+type BinancePriceResponse = Vec<BinancePriceData>;
+
+struct BinancePriceFetcher;
+
+impl Fetcher for BinancePriceFetcher {
+    type ParsedResponse = PairPriceData;
+    type ApiResponse = BinancePriceResponse;
+
+    fn parse_response(&self, value: BinancePriceResponse) -> Result<Self::ParsedResponse> {
+        let response: Self::ParsedResponse = value
+            .into_iter()
+            .map(|value| (value.symbol, value.price))
+            .collect();
+
+        Ok(response)
+    }
+}
+
 pub async fn get_binance_prices() -> Result<PairPriceData> {
-    let url = Url::parse("https://api.binance.com/api/v3/ticker/price")?;
+    let fetcher = BinancePriceFetcher {};
+    let req = fetcher.prepare_get_request("https://api.binance.com/api/v3/ticker/price", None);
+    let resp: Response = send(req?).await?;
+    let deserialized = fetcher.deserialize_response(resp)?;
+    let pair_prices: PairPriceData = fetcher.parse_response(deserialized)?;
 
-    let mut req = Request::builder();
-    req.method(Method::Get);
-    req.uri(url);
-    req.header("Accepts", "application/json");
-
-    let req = req.build();
-    let resp: Response = send(req).await?;
-
-    let body = resp.into_body();
-    let string = String::from_utf8(body)?;
-    let values: Vec<BinancePrice> = serde_json::from_str(&string)?;
-    let response: PairPriceData = values
-        .into_iter()
-        .map(|value| (value.symbol, value.price))
-        .collect();
-
-    Ok(response)
+    Ok(pair_prices)
 }
