e5ff3f429686ffeaf182cce86601ed4e6420417d
Mila Genova <mila.genova@blocksense.network>
2025-02-13 16:33:01 +0200

    feat(oracles): Implement Fetcher trait for `MEXC`

--- apps/oracles/crypto-price-feeds/src/mexc.rs
+++ apps/oracles/crypto-price-feeds/src/mexc.rs
@@ -1,35 +1,40 @@
 use anyhow::Result;
-use blocksense_sdk::spin::http::{send, Method, Request, Response};
+use blocksense_sdk::spin::http::{send, Response};
 
 use serde::Deserialize;
-use url::Url;
 
-use crate::common::PairPriceData;
+use crate::common::{Fetcher, PairPriceData};
 
 #[derive(Default, Debug, Clone, PartialEq, Deserialize)]
-pub struct MEXCPrice {
+pub struct MEXCPriceData {
     pub symbol: String,
     pub price: String,
 }
 
+type MEXCPriceResponse = Vec<MEXCPriceData>;
+
+struct MEXCPriceFetcher;
+
+impl Fetcher for MEXCPriceFetcher {
+    type ParsedResponse = PairPriceData;
+    type ApiResponse = MEXCPriceResponse;
+
+    fn parse_response(&self, value: MEXCPriceResponse) -> Result<Self::ParsedResponse> {
+        let response: PairPriceData = value
+            .into_iter()
+            .map(|value| (value.symbol, value.price))
+            .collect();
+
+        Ok(response)
+    }
+}
+
 pub async fn get_mexc_prices() -> Result<PairPriceData> {
-    let url = Url::parse("https://api.mexc.com/api/v3/ticker/price")?;
-
-    let mut req = Request::builder();
-    req.method(Method::Get);
-    req.uri(url);
-    req.header("Accepts", "application/json");
-
-    let req = req.build();
+    let fetcher = MEXCPriceFetcher {};
+    let req = fetcher.prepare_get_request("https://api.mexc.com/api/v3/ticker/price", None)?;
     let resp: Response = send(req).await?;
+    let deserialized = fetcher.deserialize_response(resp)?;
+    let pair_prices: PairPriceData = fetcher.parse_response(deserialized)?;
 
-    let body = resp.into_body();
-    let string = String::from_utf8(body)?;
-    let values: Vec<MEXCPrice> = serde_json::from_str(&string)?;
-    let response: PairPriceData = values
-        .into_iter()
-        .map(|value| (value.symbol, value.price))
-        .collect();
-
-    Ok(response)
+    Ok(pair_prices)
 }
