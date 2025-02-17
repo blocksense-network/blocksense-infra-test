fb3278f74887aa245f582c6d4a0bb14c426b3d00
Hristo Staykov <izo8611@gmail.com>
2025-02-17 19:41:34 +0200

    refactor(sequencer/feeds): Moved UpdateToSend to libs/feeds_processing

--- libs/data_feeds/src/feeds_processing.rs
+++ libs/data_feeds/src/feeds_processing.rs
@@ -92,6 +92,39 @@ pub fn naive_packing(feed_result: &FeedType, digits_in_fraction: usize) -> Vec<u
     feed_result.as_bytes(digits_in_fraction)
 }
 
+use std::collections::HashMap;
+
+#[derive(Debug, Clone, Default)]
+pub struct UpdateToSend {
+    pub block_height: u64,
+    pub updates: Vec<VotedFeedUpdate>,
+    // The key in this map is the feed id for which we provide a proof for the aggregated value.
+    pub proofs: HashMap<u32, Vec<DataFeedPayload>>,
+}
+
+impl UpdateToSend {
+    // The updates to be sent to different networks go through multiple filters.
+    // Eventually we might need to reduce the proof to only contain records for
+    // the relevant updates. The following function does that and returns the
+    // count of removed elements.
+    pub fn normalize_proof(&mut self) -> usize {
+        let removed_elements_count = self.proofs.len() - self.updates.len();
+
+        if removed_elements_count > 0 {
+            let mut normalized_proofs = HashMap::new();
+            for update in self.updates.iter() {
+                let Some(proof) = self.proofs.remove(&update.feed_id) else {
+                    error!("Logical ERROR! no proof found for key: {}", update.feed_id);
+                    continue;
+                };
+                normalized_proofs.insert(update.feed_id, proof);
+            }
+            self.proofs = normalized_proofs;
+        }
+        removed_elements_count
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::time::SystemTime;
