fc53f25a25779aeec457748aa67ca6ee67d45a23
Radoslav Dimchev <radoslav.dimchev@blocksense.network>
2025-02-18 16:25:01 +0200

    fix(docs.ws): `ContractBaseInfo` alignment

--- apps/docs.blocksense.network/components/sol-contracts/ContractBaseInfo.tsx
+++ apps/docs.blocksense.network/components/sol-contracts/ContractBaseInfo.tsx
@@ -13,18 +13,18 @@ export const ContractBaseInfo = ({ ...contract }: ContractDocItem) => {
   return (
     <>
       <AnchorLinkTitle title={contract.name} titleLevel={2} />
-      <div className="contract-base-info border-solid border border-slate-200 bg-white shadow-md px-2 py-2 md:px-4 md:pt-4 pb-0 rounded-md mt-6 mb-6 flex flex-col lg:flex-row items-start dark:bg-neutral-900 dark:border-neutral-600">
+      <div className="contract-base-info border-solid border border-slate-200 bg-white shadow-md px-2 py-2 md:px-4 md:pt-4 pb-0 rounded-md mt-6 mb-6 flex flex-col xl:flex-row items-start dark:bg-neutral-900 dark:border-neutral-600">
         <section className="contract-base-info__content w-full">
-          <header className="contract-base-info__header px-2 py-2 flex flex-col items-start mb-3 lg:flex-row lg:justify-between lg:items-center">
+          <header className="contract-base-info__header px-2 py-2 flex flex-col items-start mb-3 xl:flex-row xl:justify-between xl:items-center">
             <h2 className="contract-base-info__base-contracts-title text-2xl mr-2 font-semibold text-gray-800 dark:text-white">
               Overview
             </h2>
 
-            <div className="flex flex-col w-full justify-end lg:flex-row lg:items-center lg:space-x-2">
-              <aside className="contract-base-info__description border-solid border border-slate-200 bg-slate-50 rounded-md font-bold px-4 py-1.5 mt-4 mb-2 md:mb-0 lg:my-0 dark:bg-neutral-900 dark:border-neutral-600">
-                <dl className="contract-base-info__list flex flex-col lg:flex-row lg:items-center lg:space-x-2">
+            <div className="flex flex-col w-full justify-end xl:flex-row xl:items-center xl:space-x-2">
+              <aside className="contract-base-info__description border-solid border border-slate-200 bg-slate-50 rounded-md font-bold px-4 py-1.5 dark:bg-neutral-900 dark:border-neutral-600">
+                <dl className="contract-base-info__list flex flex-col xl:flex-row xl:items-center xl:space-x-2">
                   <div
-                    className={`contract-base-info__item flex items-center ${contract.contractKind === 'contract' ? 'mb-2 lg:mb-0' : 'mb-0'}`}
+                    className={`contract-base-info__item flex items-center ${contract.contractKind === 'contract' ? 'mb-2 xl:mb-0' : 'mb-0'}`}
                   >
                     <ImageWrapper
                       src="/icons/blocksense-kind.svg"
@@ -40,23 +40,19 @@ export const ContractBaseInfo = ({ ...contract }: ContractDocItem) => {
                   </div>
 
                   {contract.contractKind === 'contract' && (
-                    <>
-                      <div className="contract-base-info__separator hidden lg:block w-[1px] bg-gray-900 dark:bg-neutral-600 h-4 mx-4"></div>
-
-                      <div className="contract-base-info__item flex items-center">
-                        <ImageWrapper
-                          src="/icons/blocksense-abstract.svg"
-                          alt="Abstract"
-                          className="relative w-5 h-5 mr-2 invert"
-                        />
-                        <dt className="contract-base-info__label text-gray-500 font-normal dark:text-white">
-                          Abstract:
-                        </dt>
-                        <dd className="contract-base-info__value text-gray-900 text-base ml-1 dark:text-white">
-                          {contract.abstract.toString()}
-                        </dd>
-                      </div>
-                    </>
+                    <div className="contract-base-info__item flex items-center">
+                      <ImageWrapper
+                        src="/icons/blocksense-abstract.svg"
+                        alt="Abstract"
+                        className="relative w-5 h-5 mr-2 invert"
+                      />
+                      <dt className="contract-base-info__label text-gray-500 font-normal dark:text-white">
+                        Abstract:
+                      </dt>
+                      <dd className="contract-base-info__value text-gray-900 text-base ml-1 dark:text-white">
+                        {contract.abstract.toString()}
+                      </dd>
+                    </div>
                   )}
                 </dl>
               </aside>
@@ -64,7 +60,7 @@ export const ContractBaseInfo = ({ ...contract }: ContractDocItem) => {
               {/* Always show ABI button if ABI exists */}
               {contract.contractKind !== 'library' && (
                 <div
-                  className={`contract-base-info__item flex items-center ${contract.contractKind === 'contract' && !contract.abstract ? 'lg:ml-4' : ''}`}
+                  className={`contract-base-info__item flex items-center ${contract.contractKind === 'contract' && !contract.abstract ? 'xl:ml-4' : ''}`}
                 >
                   <ABIModal
                     abi={contract.abi}
