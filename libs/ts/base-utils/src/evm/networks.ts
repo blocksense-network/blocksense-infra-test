d30c762231b4a2a1f070cd6e9493ac1cfbd32ef1
Kristin Kirkov <kristin.kirkov@metacraft-labs.com>
2025-02-19 15:35:57 +0200

    feat(base-utils): Add shape-sepolia to base-utils

--- libs/ts/base-utils/src/evm/networks.ts
+++ libs/ts/base-utils/src/evm/networks.ts
@@ -63,6 +63,7 @@ const networks = [
   'rollux-testnet',
   'scroll-mainnet',
   'scroll-sepolia',
+  'shape-sepolia',
   'songbird-coston',
   'sonic-blaze',
   'taiko-mainnet',
@@ -78,8 +79,8 @@ const chainIds = [
   43114, 43113, 8453, 84532, 80084, 168587773, 56, 97, 42220, 44787, 5115, 338,
   250, 4002, 114, 100, 10200, 1666700000, 1663, 2424, 763373, 2358, 1285, 59144,
   59141, 169, 3441006, 5003, 41454, 2818, 2810, 10, 11155420, 5611, 137, 80002,
-  1101, 2442, 57000, 534352, 534351, 16, 57054, 167000, 167009, 41, 324, 300,
-  4801,
+  1101, 2442, 57000, 534352, 534351, 11011, 16, 57054, 167000, 167009, 41, 324,
+  300, 4801,
 ] as const;
 
 export const networkName = S.Literal(...networks);
@@ -431,6 +432,12 @@ export const networkMetadata = {
     chainId: 534351,
     isTestnet: true,
     explorerUrl: 'https://sepolia.scrollscan.com',
+    currency: Currency.ETH,
+  },
+  'shape-sepolia': {
+    chainId: 11011,
+    isTestnet: true,
+    explorerUrl: 'https://explorer.test.mezo.org',
     currency: Currency.ETH,
   },
   'songbird-coston': {
