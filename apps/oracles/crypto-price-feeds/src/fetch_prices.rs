222fc6a2a9bd6f0ce5815d2b706d62348c9fd8a1
Martin Ivanov <xeartybg@gmail.com>
2025-02-21 23:28:59 +0200

    feat(oracles): Add the `BoxedFuture` typedef

--- apps/oracles/crypto-price-feeds/src/fetch_prices.rs
+++ apps/oracles/crypto-price-feeds/src/fetch_prices.rs
@@ -3,9 +3,13 @@ use anyhow::Result;
 use std::collections::HashMap;
 use std::time::Instant;
 
-use futures::stream::{FuturesUnordered, StreamExt};
 use std::future::Future;
 use std::pin::Pin;
+
+use futures::{
+    future::LocalBoxFuture,
+    stream::{FuturesUnordered, StreamExt},
+};
 
 use crate::common::{fill_results, PairPriceData, ResourceData, ResourceResult};
 
@@ -42,7 +46,7 @@ pub async fn fetch_all_prices(
     resources: &Vec<ResourceData>,
     results: &mut HashMap<String, Vec<ResourceResult>>,
 ) -> Result<()> {
-    let mut futures = FuturesUnordered::<BoxedFuture>::new();
+    let mut futures = FuturesUnordered::<LocalBoxFuture<Result<(String, PairPriceData)>>>::new();
     let start = Instant::now();
 
     // Push exchange futures into FuturesUnordered
