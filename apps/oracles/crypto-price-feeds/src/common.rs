cd45651930796bd5553bf225ab9c72a6a8cb4b3a
Martin Ivanov <xeartybg@gmail.com>
2025-02-22 02:09:00 +0200

    feat(oracles): Implement `http` utilities

--- apps/oracles/crypto-price-feeds/src/common.rs
+++ apps/oracles/crypto-price-feeds/src/common.rs
@@ -1,7 +1,8 @@
-use anyhow::Result;
-use blocksense_sdk::spin::http::{Method, Request, Response};
-use serde::de::DeserializeOwned;
 use std::collections::HashMap;
+
+use anyhow::{bail, Result};
+use blocksense_sdk::spin::http::{send, Method, Request, Response};
+use serde::de::DeserializeOwned;
 use url::Url;
 
 pub const USD_SYMBOLS: [&str; 3] = ["USD", "USDC", "USDT"];
@@ -52,4 +53,43 @@ pub trait Fetcher {
     }
 
     fn parse_response(&self, response: Self::ApiResponse) -> Result<Self::ParsedResponse>;
+}
+
+pub type QueryParam<'a, 'b> = (&'a str, &'b str);
+
+#[allow(dead_code)]
+pub fn prepare_get_request(
+    base_url: &str,
+    params: Option<&[QueryParam<'_, '_>]>,
+) -> Result<Request> {
+    let url = match params {
+        Some(p) => Url::parse_with_params(base_url, p)?,
+        None => Url::parse(base_url)?,
+    };
+
+    let mut req = Request::builder();
+    req.method(Method::Get);
+    req.uri(url.as_str());
+    req.header("Accepts", "application/json");
+
+    Ok(req.build())
+}
+
+#[allow(dead_code)]
+pub async fn http_get_json<T>(url: &str, params: Option<&[QueryParam<'_, '_>]>) -> Result<T>
+where
+    T: DeserializeOwned,
+{
+    let request = prepare_get_request(url, params)?;
+    let response: Response = send(request).await?;
+
+    let status_code: u16 = *response.status();
+    let request_successful = (200..=299).contains(&status_code);
+
+    if !request_successful {
+        bail!("HTTP get request error: returned status code {status_code}");
+    }
+
+    let body = response.body();
+    serde_json::from_slice(body).map_err(Into::into)
 }
