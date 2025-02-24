9715b03c4cf3f037048292543fab3f086ace02c9
Mila Genova <mila.genova@blocksense.network>
2025-02-24 15:48:14 +0200

    refactor(oracles): Re-implement price fetching from `MEXC`

--- apps/oracles/crypto-price-feeds/src/exchanges/mexc.rs
+++ apps/oracles/crypto-price-feeds/src/exchanges/mexc.rs
@@ -1,9 +1,9 @@
 use anyhow::Result;
-use blocksense_sdk::spin::http::{send, Response};
+use futures::{future::LocalBoxFuture, FutureExt};
 
 use serde::Deserialize;
 
-use crate::common::{Fetcher, PairPriceData};
+use crate::{common::PairPriceData, http::http_get_json, traits::prices_fetcher::PricesFetcher};
 
 #[derive(Default, Debug, Clone, PartialEq, Deserialize)]
 pub struct MEXCPriceData {
@@ -13,28 +13,22 @@ pub struct MEXCPriceData {
 
 type MEXCPriceResponse = Vec<MEXCPriceData>;
 
-struct MEXCPriceFetcher;
+pub struct MEXCPriceFetcher;
 
-impl Fetcher for MEXCPriceFetcher {
-    type ParsedResponse = PairPriceData;
-    type ApiResponse = MEXCPriceResponse;
+impl PricesFetcher for MEXCPriceFetcher {
+    fn fetch(&self) -> LocalBoxFuture<Result<PairPriceData>> {
+        async {
+            let response = http_get_json::<MEXCPriceResponse>(
+                "https://api.mexc.com/api/v3/ticker/price",
+                None,
+            )
+            .await?;
 
-    fn parse_response(&self, value: MEXCPriceResponse) -> Result<Self::ParsedResponse> {
-        let response: PairPriceData = value
-            .into_iter()
-            .map(|value| (value.symbol, value.price))
-            .collect();
-
-        Ok(response)
+            Ok(response
+                .into_iter()
+                .map(|value| (value.symbol, value.price))
+                .collect())
+        }
+        .boxed_local()
     }
-}
-
-pub async fn get_mexc_prices() -> Result<PairPriceData> {
-    let fetcher = MEXCPriceFetcher {};
-    let req = fetcher.prepare_get_request("https://api.mexc.com/api/v3/ticker/price", None)?;
-    let resp: Response = send(req).await?;
-    let deserialized = fetcher.deserialize_response(resp)?;
-    let pair_prices: PairPriceData = fetcher.parse_response(deserialized)?;
-
-    Ok(pair_prices)
 }
--- apps/oracles/crypto-price-feeds/src/fetch_prices.rs
+++ apps/oracles/crypto-price-feeds/src/fetch_prices.rs
@@ -11,7 +11,8 @@ use crate::{
         binance::BinancePriceFetcher, binance_us::BinanceUsPriceFetcher,
         bitfinex::BitfinexPriceFetcher, bitget::BitgetPriceFetcher, bybit::BybitPriceFetcher,
         coinbase::CoinbasePriceFetcher, crypto_com_exchange::CryptoComPriceFetcher,
-        gate_io::GateIoPriceFetcher, kucoin::KuCoinPriceFetcher, okx::OKXPriceFetcher,
+        gate_io::GateIoPriceFetcher, kucoin::KuCoinPriceFetcher, mexc::MEXCPriceFetcher,
+        okx::OKXPriceFetcher,
     },
     symbols_cache::load_exchange_symbols,
     traits::prices_fetcher::PricesFetcher,
@@ -30,6 +31,7 @@ pub async fn fetch_all_prices(resources: &[ResourceData]) -> Result<TradingPairT
         ("Crypto.com", Box::new(CryptoComPriceFetcher)),
         ("Gate.io", Box::new(GateIoPriceFetcher)),
         ("KuCoin", Box::new(KuCoinPriceFetcher)),
+        ("MEXC", Box::new(MEXCPriceFetcher)),
         ("OKX", Box::new(OKXPriceFetcher::new(&symbols.okx))),
     ];
 
