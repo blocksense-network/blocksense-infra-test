2a2bf480a2a3d726df988f2dc6e107f542fd2017
Mila Genova <mila.genova@blocksense.network>
2025-02-24 16:10:21 +0200

    refactor(oracles): Re-implement price fetching from `Kraken`

--- apps/oracles/crypto-price-feeds/src/exchanges/kraken.rs
+++ apps/oracles/crypto-price-feeds/src/exchanges/kraken.rs
@@ -1,11 +1,12 @@
-use anyhow::{Context, Ok, Result};
-use blocksense_sdk::spin::http::{send, Response};
 use std::collections::HashMap;
+
+use anyhow::{Context, Result};
+use futures::{future::LocalBoxFuture, FutureExt};
 
 use serde::Deserialize;
 use serde_json::Value;
 
-use crate::common::{Fetcher, PairPriceData};
+use crate::{common::PairPriceData, http::http_get_json, traits::prices_fetcher::PricesFetcher};
 
 #[derive(Default, Debug, Clone, PartialEq, Deserialize)]
 pub struct KrakenPriceData {
@@ -18,38 +19,33 @@ pub struct KrakenPriceResponse {
     pub result: HashMap<String, KrakenPriceData>,
 }
 
-struct KrakenPriceFetcher;
+pub struct KrakenPriceFetcher;
 
-impl Fetcher for KrakenPriceFetcher {
-    type ParsedResponse = PairPriceData;
-    type ApiResponse = KrakenPriceResponse;
+impl PricesFetcher for KrakenPriceFetcher {
+    fn fetch(&self) -> LocalBoxFuture<Result<PairPriceData>> {
+        async {
+            let response = http_get_json::<KrakenPriceResponse>(
+                "https://api.kraken.com/0/public/Ticker",
+                None,
+            )
+            .await?;
 
-    fn parse_response(&self, value: KrakenPriceResponse) -> Result<Self::ParsedResponse> {
-        let mut response: PairPriceData = HashMap::new();
-        for (symbol, price) in value.result {
-            response.insert(
-                symbol.clone(),
-                price
-                    .a
-                    .first()
-                    .context(format!(
-                        "Kraken has no price in response for symbol: {}",
-                        symbol
-                    ))?
-                    .clone(),
-            );
+            response
+                .result
+                .into_iter()
+                .map(|(symbol, price_data)| {
+                    let price = price_data
+                        .a
+                        .first()
+                        .context(format!(
+                            "Kraken has no price in response for symbol: {}",
+                            symbol
+                        ))?
+                        .clone();
+                    Ok((symbol, price))
+                })
+                .collect::<Result<PairPriceData>>()
         }
-
-        Ok(response)
+        .boxed_local()
     }
-}
-
-pub async fn get_kraken_prices() -> Result<PairPriceData> {
-    let fetcher = KrakenPriceFetcher {};
-    let req = fetcher.prepare_get_request("https://api.kraken.com/0/public/Ticker", None)?;
-    let resp: Response = send(req).await?;
-    let deserialized = fetcher.deserialize_response(resp)?;
-    let pair_prices: PairPriceData = fetcher.parse_response(deserialized)?;
-
-    Ok(pair_prices)
 }
