20457067ef28094262144bb6da458d3eb5aa1c49
Mila Genova <mila.genova@blocksense.network>
2025-03-05 21:56:59 +0200

    feat(oracles): Add volume data from Coinbase

--- apps/oracles/crypto-price-feeds/src/exchanges/coinbase.rs
+++ apps/oracles/crypto-price-feeds/src/exchanges/coinbase.rs
@@ -3,6 +3,7 @@ use futures::{future::LocalBoxFuture, FutureExt};
 use std::collections::HashMap;
 
 use serde::Deserialize;
+use serde_this_or_that::as_f64;
 
 use crate::{
     common::{PairPriceData, PricePoint},
@@ -19,6 +20,13 @@ pub struct CoinbasePriceData {
 #[derive(Default, Debug, Clone, PartialEq, Deserialize)]
 pub struct CoinbasePriceResponse {
     pub data: CoinbasePriceData,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Deserialize)]
+pub struct CoinbaseVolumeData {
+    pub id: String,
+    #[serde(deserialize_with = "as_f64")]
+    pub spot_volume_24hour: f64,
 }
 
 pub struct CoinbasePriceFetcher;
@@ -38,15 +46,30 @@ impl PricesFetcher<'_> for CoinbasePriceFetcher {
             )
             .await?;
 
+            // This request sometimes fails.
+            let volume_response = http_get_json::<Vec<CoinbaseVolumeData>>(
+                "https://api.exchange.coinbase.com/products/volume-summary",
+                None,
+            )
+            .await?;
+
             Ok(response
                 .data
                 .rates
                 .into_iter()
                 .filter_map(|(asset, price)| match price.parse::<f64>() {
                     Ok(price_as_number) => {
+                        let id = format!("{}-{}", asset, "USD");
+
+                        let volume = volume_response
+                            .iter()
+                            .find(|data| data.id == id)
+                            .map(|data| data.spot_volume_24hour)
+                            .unwrap_or(0.0);
+
                         let price = 1.0 / price_as_number;
                         let pair = format!("{}{}", asset, "USD");
-                        Some((pair, PricePoint { price, volume: 1.0 }))
+                        Some((pair, PricePoint { price, volume }))
                     }
                     Err(_) => None,
                 })
