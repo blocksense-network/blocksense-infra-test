blocksense-network/blocksense

feat(tasks): Add Safe module to allow admin multisig to modify sequencer multisig

libs/ts/contracts/contracts/safe/AdminExecutorModule.sol
@@ -1,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import '@safe-global/safe-contracts/contracts/Safe.sol';
+
+contract AdminExecutorModule {
+  address payable public immutable MULTISIG;
+  address public immutable ADMIN_MULTISIG;
+
+  error OnlyAdminMultisig();
+
+  constructor(address multisig, address adminMultisig) {
+    MULTISIG = payable(multisig);
+    ADMIN_MULTISIG = adminMultisig;
+  }
+
+  function executeTransaction(bytes calldata data) external {
+    if (msg.sender != ADMIN_MULTISIG) {
+      revert OnlyAdminMultisig();
+    }
+
+    Safe(MULTISIG).execTransactionFromModule(
+      MULTISIG,
+      0,
+      data,
+      Enum.Operation.Call
+    );
+  }
+}

blocksense-network/blocksense-infra-test

libs/ts/contracts/contracts/safe/AdminExecutorModule.sol
@@ -1,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import '@safe-global/safe-contracts/contracts/base/ModuleManager.sol';
+
+contract AdminExecutorModule {
+  address payable public immutable MULTISIG;
+  address public immutable ADMIN_MULTISIG;
+
+  error OnlyAdminMultisig();
+
+  constructor(address multisig, address adminMultisig) {
+    MULTISIG = payable(multisig);
+    ADMIN_MULTISIG = adminMultisig;
+  }
+
+  function executeTransaction(bytes calldata data) external {
+    if (msg.sender != ADMIN_MULTISIG) {
+      revert OnlyAdminMultisig();
+    }
+
+    ModuleManager(MULTISIG).execTransactionFromModule(
+      MULTISIG,
+      0,
+      data,
+      Enum.Operation.Call
+    );
+  }
+}