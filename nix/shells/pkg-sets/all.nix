8c22aae1d532100523c489086ba783751946e166
Martin Ivanov <xeartybg@gmail.com>
2025-03-25 13:47:25 +0200

    feat(nix/shells): Create a symlink of the blocksense config files directory in `$GIT_ROOT/config/generated`

--- nix/shells/pkg-sets/all.nix
+++ nix/shells/pkg-sets/all.nix
@@ -14,7 +14,16 @@
     ../../test-environments/example-setup-01.nix
   ];
 
-  enterShell = ''
-    ln -fs ${config.process.managers.process-compose.configFile} ${config.devenv.root}/process-compose.yml
-  '';
+  enterShell =
+    let
+      generated-config-dir = "${config.devenv.root}/config/generated";
+      install-config-dir-symlink = ''
+        [[ -L ${generated-config-dir} ]] && unlink ${generated-config-dir}
+        ln -fs ${config.services.blocksense.config-dir} ${generated-config-dir}
+      '';
+    in
+    ''
+      ln -fs ${config.process.managers.process-compose.configFile} ${config.devenv.root}/process-compose.yml
+      ${install-config-dir-symlink}
+    '';
 }
