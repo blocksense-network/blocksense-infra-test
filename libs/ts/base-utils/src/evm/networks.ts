d1afaf74363e97bbec69ada75885652a923cdfad
Kristin Kirkov <kristin.kirkov@metacraft-labs.com>
2025-03-26 09:51:21 +0200

    feat(base-utils): Add metal-l2-testnet to base-utils

--- libs/ts/base-utils/src/evm/networks.ts
+++ libs/ts/base-utils/src/evm/networks.ts
@@ -59,6 +59,7 @@ const networks = [
   'manta-mainnet',
   'manta-sepolia',
   'mantle-sepolia',
+  'metal-l2-testnet',
   'metis-sepolia',
   'megaeth-testnet',
   'mezo-matsnet-testnet',
@@ -97,10 +98,10 @@ const chainIds = [
   43114, 43113, 8453, 84532, 80084, 80069, 168587773, 28882, 56, 97, 325000,
   42220, 44787, 5115, 338, 111557560, 18880, 250, 4002, 114, 10888, 100, 10200,
   1666700000, 743111, 1663, 2424, 763373, 2358, 1285, 59144, 59141, 1952959480,
-  994873017, 169, 3441006, 5003, 59902, 6342, 31611, 10143, 2818, 2810, 10,
-  11155420, 5611, 7849306, 137, 80002, 1101, 2442, 57000, 200018, 31, 534352,
-  534351, 11011, 50312, 16, 57054, 1660990954, 167000, 167009, 842, 41, 324,
-  300, 4801,
+  994873017, 169, 3441006, 5003, 1750, 59902, 6342, 31611, 10143, 2818, 2810,
+  10, 11155420, 5611, 7849306, 137, 80002, 1101, 2442, 57000, 200018, 31,
+  534352, 534351, 11011, 50312, 16, 57054, 1660990954, 167000, 167009, 842, 41,
+  324, 300, 4801,
 ] as const;
 
 export const networkName = S.Literal(...networks);
@@ -438,6 +439,12 @@ export const networkMetadata = {
     isTestnet: true,
     explorerUrl: 'https://sepolia.mantlescan.xyz/',
     currency: Currency.MNT,
+  },
+  'metal-l2-testnet': {
+    chainId: 1750,
+    isTestnet: true,
+    explorerUrl: 'https://testnet.explorer.metall2.com',
+    currency: Currency.ETH,
   },
   'metis-sepolia': {
     chainId: 59902,
