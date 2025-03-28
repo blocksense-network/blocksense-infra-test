1018d5a5c9e6d2f6e9d03fe445d6157e46e42544
Mila Genova <mila.genova@blocksense.network>
2025-03-28 14:34:42 +0200

    feat(DFCG): Implement function to list exchanges with significant discrepancies in price

--- apps/data-feeds-config-generator/src/generation/initial/data-providers.ts
+++ apps/data-feeds-config-generator/src/generation/initial/data-providers.ts
@@ -149,3 +149,48 @@ function isPairSupportedByCryptoProvider(
 
   return isBaseCompatible && isCompatibleQuote;
 }
+
+export function detectOutliers(
+  feedPriceData: Record<string, Record<string, number>[]>,
+): string[] {
+  const outlinerts: string[] = [];
+  const [asset, pricesArray] = Object.entries(feedPriceData)[0];
+
+  const zeroPricesFiltered = pricesArray.filter(priceData => {
+    const [exchangeName, price] = Object.entries(priceData)[0];
+    if (price === 0 || price < 0 || price === null) {
+      console.log(
+        `Detected Outlier : ${exchangeName} (${price}) for feed ${asset}`,
+      );
+      outlinerts.push(exchangeName);
+      return false;
+    }
+    return true;
+  });
+
+  // Convert array of { Exchange: Price } into a single object { Exchange: Price }
+  const prices: Record<string, number> = Object.assign(
+    {},
+    ...zeroPricesFiltered,
+  );
+  const exchanges = Object.keys(prices);
+  const priceValues = Object.values(prices);
+
+  const sortedPrices = [...priceValues].sort((a, b) => a - b);
+  const middle = Math.floor(sortedPrices.length / 2);
+  const medianPrice =
+    sortedPrices.length % 2 === 0
+      ? (sortedPrices[middle - 1] + sortedPrices[middle]) / 2
+      : sortedPrices[middle];
+
+  exchanges.forEach(exchange => {
+    const price = prices[exchange];
+    if (Math.abs(price - medianPrice) / medianPrice > 0.1) {
+      console.log(
+        `Detected Outlier : ${exchange} (${price}) for feed ${asset}`,
+      );
+      outlinerts.push(exchange);
+    }
+  });
+  return outlinerts;
+}
