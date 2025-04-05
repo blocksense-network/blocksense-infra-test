2f57a723a7d63de36169cecc937521cf64905f1e
Vanya Pavlova <vanya.pavlova@blocksense.network>
2025-04-05 13:47:56 +0300

    feat(nft.blocksense.network): Define typography

--- apps/nft.blocksense.network/src/geist.ts
+++ apps/nft.blocksense.network/src/geist.ts
@@ -3,11 +3,13 @@ import localFont from 'next/font/local';
 export const geist = localFont({
   src: '../public/font/Geist-Regular.woff2',
   variable: '--font-geist',
+  fallback: ['system-ui', 'arial'],
   weight: '100 900',
 });
 
 export const geistMono = localFont({
   src: '../public/font/GeistMono-Regular.woff2',
   variable: '--font-geist-mono',
+  fallback: ['system-ui', 'arial'],
   weight: '100 900',
 });
