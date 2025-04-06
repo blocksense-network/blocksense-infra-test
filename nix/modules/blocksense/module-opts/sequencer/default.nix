b8d40debb163852a6f171a0662c6cba7dd18d7fd
Martin Ivanov <xeartybg@gmail.com>
2025-04-07 00:55:00 +0300

    refactor(nix/modules): Inline `block-config` module options

--- nix/modules/blocksense/module-opts/sequencer/default.nix
+++ nix/modules/blocksense/module-opts/sequencer/default.nix
@@ -25,9 +25,29 @@ lib: with lib; {
     };
   };
 
-  block-config = mkOption {
-    type = types.submodule (import ./block-config.nix lib);
-    description = mdDoc "Block creation configuration.";
+  block-config = {
+    max-feed-updates-to-batch = mkOption {
+      type = types.int;
+      default = 1;
+      description = mdDoc "The maximum number of keys to batch together before sending a report.";
+    };
+
+    block-generation-period = mkOption {
+      type = types.int;
+      default = 500;
+      description = mdDoc "The maximum duration (in ms) to wait before sending aggregating the votes.";
+    };
+
+    genesis-block-timestamp-ms = mkOption {
+      type = types.nullOr types.int;
+      description = mdDoc "Time of genesis of blockchain.";
+    };
+
+    aggregation-consensus-discard-period-blocks = mkOption {
+      type = types.int;
+      default = 1000;
+      description = mdDoc "Maximum number of blocks to consider for consensus. If there are blocks (batches) that are older and still await consensus, they will be dropped.";
+    };
   };
 
   providers = mkOption {
