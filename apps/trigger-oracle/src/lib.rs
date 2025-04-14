blocksense-network/blocksense

feat(trigger-oracle): Start validating the second consensus

apps/trigger-oracle/src/lib.rs
@@ -41,9 +41,11 @@ use wasmtime_wasi_http::{
     types::HostFutureIncomingResponse, HttpResult,
 };
 
+use config::FeedStrideAndDecimals;
 use crypto::JsonSerializableSignature;
-use data_feeds::generate_signature::generate_signature;
+use data_feeds::{feeds_processing::VotedFeedUpdate, generate_signature::generate_signature};
 use feed_registry::types::{DataFeedPayload, FeedError, FeedType, PayloadMetaData};
+use feeds_processing::utils::validate;
 use prometheus::{
     actix_server::handle_prometheus_metrics,
     metrics::{
@@ -69,7 +71,7 @@ use blocksense::oracle::oracle_types as oracle;
 
 pub(crate) type RuntimeData = HttpRuntimeData;
 pub(crate) type _Store = spin_core::Store<RuntimeData>;
-type DataFeedResults = Arc<RwLock<HashMap<String, FeedType>>>;
+type DataFeedResults = Arc<RwLock<HashMap<u32, VotedFeedUpdate>>>;
 
 const TIME_BEFORE_KAFKA_READ_RETRY_IN_MS: u64 = 500;
 const TOTAL_RETRIES_FOR_KAFKA_READ: u64 = 10;
@@ -118,6 +120,8 @@ struct TriggerMetadata {
 #[derive(Clone, Eq, Debug, Default, Deserialize, Serialize)]
 pub struct DataFeedSetting {
     pub id: String,
+    pub stride: u16,
+    pub decimals: u8,
     pub data: String,
 }
 
@@ -283,11 +287,23 @@ impl TriggerExecutor for OracleTrigger {
         let (aggregated_consensus_sender, aggregated_consensus_receiver) = unbounded_channel();
         let (signal_data_feed_sender, _) = channel(16);
         let data_feed_results: DataFeedResults = Arc::new(RwLock::new(HashMap::new()));
+        let mut feeds_config = HashMap::new();
         //TODO(adikov): Move all the logic to a different struct and handle
         //errors properly.
         // For each component, run its own timer loop
 
         let components = self.queue_components.clone();
+        for component in components.values() {
+            for df in &component.oracle_settings {
+                feeds_config.insert(
+                    df.id.parse::<u32>()?,
+                    FeedStrideAndDecimals {
+                        stride: df.stride,
+                        decimals: df.decimals,
+                    },
+                );
+            }
+        }
         tracing::debug!("Components: {:?}", &components);
         tracing::trace!("Starting oracle scripts");
         let mut loops: Vec<_> = self
@@ -325,6 +341,7 @@ impl TriggerExecutor for OracleTrigger {
         let mut manager = Self::start_manager(
             data_feed_receiver,
             aggregated_consensus_receiver,
+            feeds_config,
             data_feed_results,
             &sequencer_post_batch_url,
             &sequencer_aggregated_consensus_url,
@@ -638,7 +655,8 @@ impl OracleTrigger {
     fn start_manager(
         payload_rx: UnboundedReceiver<(String, Payload)>,
         second_consensus_rx: UnboundedReceiver<ConsensusSecondRoundBatch>,
-        data_feed_results: DataFeedResults,
+        feeds_config: HashMap<u32, FeedStrideAndDecimals>,
+        latest_votes: DataFeedResults,
         sequencer_post_batch_url: &Url,
         sequencer_aggregated_consensus_url: &Url,
         secret_key: &str,
@@ -647,7 +665,7 @@ impl OracleTrigger {
     ) -> Vec<JoinHandle<TerminationReason>> {
         let process_payload_future = Self::process_payload(
             payload_rx,
-            data_feed_results.clone(),
+            latest_votes.clone(),
             sequencer_post_batch_url.to_owned(),
             secret_key.to_owned(),
             reporter_id,
@@ -655,7 +673,8 @@ impl OracleTrigger {
 
         let process_aggregated_consensus_future = Self::process_aggregated_consensus(
             second_consensus_rx,
-            data_feed_results.clone(),
+            feeds_config,
+            latest_votes.clone(),
             sequencer_aggregated_consensus_url.to_owned(),
             second_consensus_secret_key.to_owned(),
             reporter_id,
@@ -669,7 +688,7 @@ impl OracleTrigger {
 
     async fn process_payload(
         mut rx: UnboundedReceiver<(String, Payload)>,
-        data_feed_results: DataFeedResults,
+        latest_votes: DataFeedResults,
         sequencer_url: Url,
         secret_key: String,
         reporter_id: u64,
@@ -686,21 +705,37 @@ impl OracleTrigger {
                 let result = match value {
                     oracle::DataFeedResultValue::Numerical(value) => {
                         let feed = FeedType::Numerical(value);
-                        data_feed_results
-                            .write()
-                            .await
-                            .entry(id.clone())
-                            .or_insert_with(|| feed.clone());
-                        Ok(feed)
+                        if let Ok(feed_id) = id.parse::<u32>() {
+                            latest_votes
+                                .write()
+                                .await
+                                .entry(feed_id)
+                                .or_insert_with(|| VotedFeedUpdate {
+                                    feed_id,
+                                    value: feed.clone(),
+                                    end_slot_timestamp: timestamp,
+                                });
+                            Ok(feed)
+                        } else {
+                            Err(FeedError::APIError(format!("Id cannot be parsed - {}", id)))
+                        }
                     }
                     oracle::DataFeedResultValue::Text(value) => {
                         let feed = FeedType::Text(value);
-                        data_feed_results
-                            .write()
-                            .await
-                            .entry(id.clone())
-                            .or_insert_with(|| feed.clone());
-                        Ok(feed)
+                        if let Ok(feed_id) = id.parse::<u32>() {
+                            latest_votes
+                                .write()
+                                .await
+                                .entry(feed_id)
+                                .or_insert_with(|| VotedFeedUpdate {
+                                    feed_id,
+                                    value: feed.clone(),
+                                    end_slot_timestamp: timestamp,
+                                });
+                            Ok(feed)
+                        } else {
+                            Err(FeedError::APIError(format!("Id cannot be parsed - {}", id)))
+                        }
                     }
                     oracle::DataFeedResultValue::Error(error_string) => {
                         Err(FeedError::APIError(error_string))
@@ -763,7 +798,8 @@ impl OracleTrigger {
 
     async fn process_aggregated_consensus(
         mut ss_rx: UnboundedReceiver<ConsensusSecondRoundBatch>,
-        _data_feed_results: DataFeedResults,
+        feeds_config: HashMap<u32, FeedStrideAndDecimals>,
+        latest_votes: DataFeedResults,
         sequencer: Url,
         second_consensus_secret_key: String,
         reporter_id: u64,
@@ -777,6 +813,7 @@ impl OracleTrigger {
                     continue;
                 }
             };
+
             let signed = match sign_hash(&signer, &tx).await {
                 Ok(s) => s,
                 Err(e) => {
@@ -785,11 +822,28 @@ impl OracleTrigger {
                 }
             };
 
+            let block_height = aggregated_consensus.block_height;
+            let network = aggregated_consensus.network.clone();
+            match validate(
+                feeds_config.clone(),
+                aggregated_consensus,
+                latest_votes.read().await.clone(),
+                HashMap::new(),
+            )
+            .await
+            {
+                Ok(()) => (),
+                Err(e) => {
+                    tracing::error!("Failed to validate second consensus: {}", &e);
+                    continue;
+                }
+            };
+
             let signature = bytes_to_hex_string(signed.signature);
             let report = ReporterResponse {
-                block_height: aggregated_consensus.block_height,
+                block_height,
                 reporter_id,
-                network: aggregated_consensus.network,
+                network,
                 signature,
             };
 

blocksense-network/blocksense-infra-test

apps/trigger-oracle/src/lib.rs
@@ -1,4 +1,3 @@
-use blocksense_data_feeds::generate_signature::generate_signature;
 use clap::Args;
 use serde::{Deserialize, Serialize};
 
@@ -42,8 +41,11 @@ use wasmtime_wasi_http::{
     types::HostFutureIncomingResponse, HttpResult,
 };
 
+use blocksense_config::FeedStrideAndDecimals;
 use blocksense_crypto::JsonSerializableSignature;
+use blocksense_data_feeds::{feeds_processing::VotedFeedUpdate, generate_signature::generate_signature};
 use blocksense_feed_registry::types::{DataFeedPayload, FeedError, FeedType, PayloadMetaData};
+use blocksense_feeds_processing::utils::validate;
 use blocksense_metrics::{
     actix_server::handle_prometheus_metrics,
     metrics::{
@@ -69,7 +71,7 @@ use blocksense::oracle::oracle_types as oracle;
 
 pub(crate) type RuntimeData = HttpRuntimeData;
 pub(crate) type _Store = spin_core::Store<RuntimeData>;
-type DataFeedResults = Arc<RwLock<HashMap<String, FeedType>>>;
+type DataFeedResults = Arc<RwLock<HashMap<u32, VotedFeedUpdate>>>;
 
 const TIME_BEFORE_KAFKA_READ_RETRY_IN_MS: u64 = 500;
 const TOTAL_RETRIES_FOR_KAFKA_READ: u64 = 10;
@@ -118,6 +120,8 @@ struct TriggerMetadata {
 #[derive(Clone, Eq, Debug, Default, Deserialize, Serialize)]
 pub struct DataFeedSetting {
     pub id: String,
+    pub stride: u16,
+    pub decimals: u8,
     pub data: String,
 }
 
@@ -283,11 +287,23 @@ impl TriggerExecutor for OracleTrigger {
         let (aggregated_consensus_sender, aggregated_consensus_receiver) = unbounded_channel();
         let (signal_data_feed_sender, _) = channel(16);
         let data_feed_results: DataFeedResults = Arc::new(RwLock::new(HashMap::new()));
+        let mut feeds_config = HashMap::new();
         //TODO(adikov): Move all the logic to a different struct and handle
         //errors properly.
         // For each component, run its own timer loop
 
         let components = self.queue_components.clone();
+        for component in components.values() {
+            for df in &component.oracle_settings {
+                feeds_config.insert(
+                    df.id.parse::<u32>()?,
+                    FeedStrideAndDecimals {
+                        stride: df.stride,
+                        decimals: df.decimals,
+                    },
+                );
+            }
+        }
         tracing::debug!("Components: {:?}", &components);
         tracing::trace!("Starting oracle scripts");
         let mut loops: Vec<_> = self
@@ -325,6 +341,7 @@ impl TriggerExecutor for OracleTrigger {
         let mut manager = Self::start_manager(
             data_feed_receiver,
             aggregated_consensus_receiver,
+            feeds_config,
             data_feed_results,
             &sequencer_post_batch_url,
             &sequencer_aggregated_consensus_url,
@@ -638,7 +655,8 @@ impl OracleTrigger {
     fn start_manager(
         payload_rx: UnboundedReceiver<(String, Payload)>,
         second_consensus_rx: UnboundedReceiver<ConsensusSecondRoundBatch>,
-        data_feed_results: DataFeedResults,
+        feeds_config: HashMap<u32, FeedStrideAndDecimals>,
+        latest_votes: DataFeedResults,
         sequencer_post_batch_url: &Url,
         sequencer_aggregated_consensus_url: &Url,
         secret_key: &str,
@@ -647,7 +665,7 @@ impl OracleTrigger {
     ) -> Vec<JoinHandle<TerminationReason>> {
         let process_payload_future = Self::process_payload(
             payload_rx,
-            data_feed_results.clone(),
+            latest_votes.clone(),
             sequencer_post_batch_url.to_owned(),
             secret_key.to_owned(),
             reporter_id,
@@ -655,7 +673,8 @@ impl OracleTrigger {
 
         let process_aggregated_consensus_future = Self::process_aggregated_consensus(
             second_consensus_rx,
-            data_feed_results.clone(),
+            feeds_config,
+            latest_votes.clone(),
             sequencer_aggregated_consensus_url.to_owned(),
             second_consensus_secret_key.to_owned(),
             reporter_id,
@@ -669,7 +688,7 @@ impl OracleTrigger {
 
     async fn process_payload(
         mut rx: UnboundedReceiver<(String, Payload)>,
-        data_feed_results: DataFeedResults,
+        latest_votes: DataFeedResults,
         sequencer_url: Url,
         secret_key: String,
         reporter_id: u64,
@@ -686,21 +705,37 @@ impl OracleTrigger {
                 let result = match value {
                     oracle::DataFeedResultValue::Numerical(value) => {
                         let feed = FeedType::Numerical(value);
-                        data_feed_results
-                            .write()
-                            .await
-                            .entry(id.clone())
-                            .or_insert_with(|| feed.clone());
-                        Ok(feed)
+                        if let Ok(feed_id) = id.parse::<u32>() {
+                            latest_votes
+                                .write()
+                                .await
+                                .entry(feed_id)
+                                .or_insert_with(|| VotedFeedUpdate {
+                                    feed_id,
+                                    value: feed.clone(),
+                                    end_slot_timestamp: timestamp,
+                                });
+                            Ok(feed)
+                        } else {
+                            Err(FeedError::APIError(format!("Id cannot be parsed - {}", id)))
+                        }
                     }
                     oracle::DataFeedResultValue::Text(value) => {
                         let feed = FeedType::Text(value);
-                        data_feed_results
-                            .write()
-                            .await
-                            .entry(id.clone())
-                            .or_insert_with(|| feed.clone());
-                        Ok(feed)
+                        if let Ok(feed_id) = id.parse::<u32>() {
+                            latest_votes
+                                .write()
+                                .await
+                                .entry(feed_id)
+                                .or_insert_with(|| VotedFeedUpdate {
+                                    feed_id,
+                                    value: feed.clone(),
+                                    end_slot_timestamp: timestamp,
+                                });
+                            Ok(feed)
+                        } else {
+                            Err(FeedError::APIError(format!("Id cannot be parsed - {}", id)))
+                        }
                     }
                     oracle::DataFeedResultValue::Error(error_string) => {
                         Err(FeedError::APIError(error_string))
@@ -763,7 +798,8 @@ impl OracleTrigger {
 
     async fn process_aggregated_consensus(
         mut ss_rx: UnboundedReceiver<ConsensusSecondRoundBatch>,
-        _data_feed_results: DataFeedResults,
+        feeds_config: HashMap<u32, FeedStrideAndDecimals>,
+        latest_votes: DataFeedResults,
         sequencer: Url,
         second_consensus_secret_key: String,
         reporter_id: u64,
@@ -777,6 +813,7 @@ impl OracleTrigger {
                     continue;
                 }
             };
+
             let signed = match sign_hash(&signer, &tx).await {
                 Ok(s) => s,
                 Err(e) => {
@@ -785,11 +822,28 @@ impl OracleTrigger {
                 }
             };
 
+            let block_height = aggregated_consensus.block_height;
+            let network = aggregated_consensus.network.clone();
+            match validate(
+                feeds_config.clone(),
+                aggregated_consensus,
+                latest_votes.read().await.clone(),
+                HashMap::new(),
+            )
+            .await
+            {
+                Ok(()) => (),
+                Err(e) => {
+                    tracing::error!("Failed to validate second consensus: {}", &e);
+                    continue;
+                }
+            };
+
             let signature = bytes_to_hex_string(signed.signature);
             let report = ReporterResponse {
-                block_height: aggregated_consensus.block_height,
+                block_height,
                 reporter_id,
-                network: aggregated_consensus.network,
+                network,
                 signature,
             };
 