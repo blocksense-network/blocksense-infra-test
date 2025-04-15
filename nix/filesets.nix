ebf97fb21778662b16115d1bd9b702dfcce79400
Petar Kirov <petar.p.kirov@gmail.com>
2025-04-15 13:26:38 +0300

    build(nix/filesets): Reduce rebuilds on json config files changes (take 2)

--- nix/filesets.nix
+++ nix/filesets.nix
@@ -17,11 +17,16 @@ with lib.fileset;
           "rs"
           "toml"
           "wit"
-          "json"
-          "sol"
         ]
       ) root)
 
+      # JSON files must be listed one by one, otherwise changing an
+      # unrelated JSON file will cause all Rust derivations to be rebuilt
+      (root + "/apps/sequencer_tests/Safe.json")
+      (root + "/apps/oracles/eth-rpc/src/abi/YnETHx.json")
+      (root + "/apps/sequencer_tests/SafeProxyFactory.json")
+      (root + "/libs/gnosis_safe/safe_abi.json")
+      (root + "/apps/oracles/eth-rpc/src/abi/YnETHx.json")
     ];
     src = toSource { inherit root fileset; };
   };
