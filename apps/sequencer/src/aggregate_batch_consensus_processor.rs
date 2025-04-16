blocksense-network/blocksense

refactor(sequencer/config): Change `genesis_block_timestamp` to `u128` and rename it to `current_unit_time_ms`

apps/sequencer/src/aggregate_batch_consensus_processor.rs
@@ -6,7 +6,7 @@ use gnosis_safe::data_types::ReporterResponse;
 use gnosis_safe::utils::{signature_to_bytes, SignatureWithAddress};
 use tokio::sync::mpsc::UnboundedReceiver;
 use tracing::{debug, error, info, trace};
-use utils::time::{current_unix_time, system_time_to_millis};
+use utils::time::current_unix_time;
 
 use crate::providers::provider::GNOSIS_SAFE_CONTRACT_NAME;
 use crate::sequencer_state::SequencerState;
@@ -23,15 +23,10 @@ pub async fn aggregation_batch_consensus_loop(
     tokio::task::Builder::new()
         .name("aggregation_batch_consensus_loop")
         .spawn_local(async move {
-            let block_genesis_time = match block_config.genesis_block_timestamp {
-                Some(genesis_time) => system_time_to_millis(genesis_time),
-                None => current_unix_time(),
-            };
-
             let block_height_tracker = SlotTimeTracker::new(
                 "aggregation_batch_consensus_loop".to_string(),
                 Duration::from_millis(block_config.block_generation_period),
-                block_genesis_time,
+                block_config.genesis_block_timestamp_ms.unwrap_or_else(current_unix_time),
             );
 
             let timeout_period_blocks = block_config.aggregation_consensus_discard_period_blocks;

apps/sequencer/src/block_creator.rs
@@ -23,7 +23,7 @@ use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
 use tokio::sync::RwLock;
 use tokio::time::Duration;
 use tracing::{debug, error, info, info_span, warn};
-use utils::time::{current_unix_time, system_time_to_millis};
+use utils::time::current_unix_time;
 
 use crate::feeds::feed_config_conversions::feed_config_to_block;
 use crate::sequencer_state::SequencerState;
@@ -54,15 +54,10 @@ pub async fn block_creator_loop(
             }
             info!("block_generation_period set to {}", block_generation_period);
 
-            let block_genesis_time = match block_config.genesis_block_timestamp {
-                Some(genesis_time) => system_time_to_millis(genesis_time),
-                None => current_unix_time(),
-            };
-
             let block_generation_time_tracker = SlotTimeTracker::new(
                 "block_creator_loop".to_string(),
                 Duration::from_millis(block_generation_period),
-                block_genesis_time,
+                block_config.genesis_block_timestamp_ms.unwrap_or_else(current_unix_time),
             );
 
             // Updates that overflowed the capacity of a block
@@ -355,7 +350,7 @@ mod tests {
         let block_config = BlockConfig {
             max_feed_updates_to_batch: 3,
             block_generation_period: 100,
-            genesis_block_timestamp: None,
+            genesis_block_timestamp_ms: None,
             aggregation_consensus_discard_period_blocks: 100,
         };
 

blocksense-network/blocksense-infra-test

apps/sequencer/src/aggregate_batch_consensus_processor.rs
@@ -4,7 +4,7 @@ use blocksense_config::BlockConfig;
 use blocksense_feed_registry::{registry::SlotTimeTracker, types::Repeatability};
 use blocksense_gnosis_safe::data_types::ReporterResponse;
 use blocksense_gnosis_safe::utils::{signature_to_bytes, SignatureWithAddress};
-use blocksense_utils::time::{current_unix_time, system_time_to_millis};
+use blocksense_utils::time::current_unix_time;
 use tokio::sync::mpsc::UnboundedReceiver;
 use tracing::{debug, error, info};
 
@@ -23,15 +23,10 @@ pub async fn aggregation_batch_consensus_loop(
     tokio::task::Builder::new()
         .name("aggregation_batch_consensus_loop")
         .spawn_local(async move {
-            let block_genesis_time = match block_config.genesis_block_timestamp {
-                Some(genesis_time) => system_time_to_millis(genesis_time),
-                None => current_unix_time(),
-            };
-
             let block_height_tracker = SlotTimeTracker::new(
                 "aggregation_batch_consensus_loop".to_string(),
                 Duration::from_millis(block_config.block_generation_period),
-                block_genesis_time,
+                block_config.genesis_block_timestamp_ms.unwrap_or_else(current_unix_time),
             );
 
             let timeout_period_blocks = block_config.aggregation_consensus_discard_period_blocks;

apps/sequencer/src/block_creator.rs
@@ -12,7 +12,7 @@ use blocksense_feed_registry::feed_registration_cmds::{
 use blocksense_feed_registry::registry::SlotTimeTracker;
 use blocksense_feed_registry::types::Repeatability;
 use blocksense_registry::config::FeedConfig;
-use blocksense_utils::time::{current_unix_time, system_time_to_millis};
+use blocksense_utils::time::current_unix_time;
 use rdkafka::producer::FutureRecord;
 use rdkafka::util::Timeout;
 use serde_json::json;
@@ -54,15 +54,10 @@ pub async fn block_creator_loop(
             }
             info!("block_generation_period set to {}", block_generation_period);
 
-            let block_genesis_time = match block_config.genesis_block_timestamp {
-                Some(genesis_time) => system_time_to_millis(genesis_time),
-                None => current_unix_time(),
-            };
-
             let block_generation_time_tracker = SlotTimeTracker::new(
                 "block_creator_loop".to_string(),
                 Duration::from_millis(block_generation_period),
-                block_genesis_time,
+                block_config.genesis_block_timestamp_ms.unwrap_or_else(current_unix_time),
             );
 
             // Updates that overflowed the capacity of a block
@@ -355,7 +350,7 @@ mod tests {
         let block_config = BlockConfig {
             max_feed_updates_to_batch: 3,
             block_generation_period: 100,
-            genesis_block_timestamp: None,
+            genesis_block_timestamp_ms: None,
             aggregation_consensus_discard_period_blocks: 100,
         };
 